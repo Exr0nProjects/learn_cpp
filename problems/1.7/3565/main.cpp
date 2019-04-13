f4f7da66373418cc74568c1bcbe8a7630e2077d2
Exr0n <spotyie@gmail.com>
2019-04-13 12:07:25 -0700

    problem #3565 test 0

--- problems/1.7/3565/main.cpp
+++ problems/1.7/3565/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Problem #3565
- *
+ * count numbers
  */
 
 #include <iostream>
@@ -9,10 +9,50 @@
 
 using namespace std;
 
+int place (int * count_arr; int * index_arr; int amnt; int num)
+{
+  for (int i=0; i<amnt; ++i)
+  {
+    if (index_arr[i] == num)
+    {
+      ++ count_arr[i];
+      return 0;
+    }
+    else if (index_arr[i] == -1) // reached the end
+    {
+      index_arr[i] = num;
+      count_arr[i] = 1;
+      return 0;
+    }
+  }
+  return 1; // something went wrong
+}
 
 int main ()
 {
+  int amnt, temp;
+  cin >> num;
 
+  int * count_arr = new int [amnt] {}; // up to 20 different numbers
+  int * index_arr = new int [amnt] {-1}; // same as above
+
+  for (int i=0; i<num; ++i)
+  {
+    cin >> temp;
+    place(count_arr, index_arr, amnt, temp);
+  }
+
+  int max=0, num=0;
+  for (int i=0; i<amnt; ++i)
+  {
+    if (count_arr[i] > max)
+    {
+      max = count_arr[i];
+      num = index_arr[i];
+    }
+  }
+
+  cout << num;
 
   return 0;
 }
