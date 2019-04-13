016bc69f952746e232c4b31cdecd3a23e63d6a65
Exr0n <spotyie@gmail.com>
2019-04-13 11:43:00 -0700

    3562 test 0

--- problems/1.7/3562/main.cpp
+++ problems/1.7/3562/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Problem #3562
- *
+ * delete number from array
  */
 
 #include <iostream>
@@ -9,10 +9,39 @@
 
 using namespace std;
 
+int coutPop (int * array, int size, int find)
+{
+  int * end_array = new int [size];
+  bool contains = false;
+
+  for (int i=0; i<size; ++i)
+  {
+    if (array[i] != find || contains)
+    {
+      cout << array[i] << " ";
+    }
+    else
+    {
+      contains = true;
+    }
+  }
+
+  cout << endl;
+
+  return 0;
+}
 
 int main ()
 {
+  int size, find;
+  cin >> size;
 
+  int * arr = new int[size];
+  for (int i=0; i<size; ++i) cin >> arr[i];
+
+  cin >> find;
+
+  coutPop(arr, size, find);
 
   return 0;
 }
