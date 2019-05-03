9cd852c0801ac0e856e2e50a2255767fac919cfb
Exr0n <spotyie@gmail.com>
2019-05-02 17:34:38 -0700

    3574 for some reason this isnt working

--- problems/1.7/3574/main.cpp
+++ problems/1.7/3574/main.cpp
@@ -13,14 +13,18 @@ using namespace std;
 int main ()
 {
   string line;
+  bool gone = false;
   while (getline(cin, line))
   {
     for (int i=0; i<3; ++i)
     {
-      cout << line << endl;
-      getline(cin, line);
+      if (gone) cout << endl;
+      else gone = true;
+      cout << line;
+      if (!getline(cin, line)) break;
     }
   }
+  //cout << line;
 
   return 0;
 }
