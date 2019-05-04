ef5c9e5d60aa4e3a3da6c65f03bd33d63111c06e
Exr0n <spotyie@gmail.com>
2019-05-04 05:34:50 -0700

    p3574 different version that also works

--- problems/1.7/3574/main.cpp
+++ problems/1.7/3574/main.cpp
@@ -28,3 +28,32 @@ int main ()
 
   return 0;
 }
+
+/*
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+int main ()
+{
+  string line, line1, line2;
+  bool gone = false;
+  while (getline(cin, line))
+  {
+    getline(cin, line1);
+    getline(cin, line2);
+
+    if (gone) cout << endl;
+    else gone = true;
+
+    cout << line << endl << line1 << endl << line2;
+    getline(cin, line); // remove trailing newline in input
+  }
+  //cout << line;
+
+  return 0;
+}
+*/
