aac66e3b522231d5bebadb0749a84e8e3f59f5b1
Exr0n <spotyie@gmail.com>
2019-06-11 21:33:56 -0700

    debugging tips; finish 1178

--- problems/1.8/1178/main.cpp
+++ problems/1.8/1178/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Problem #1178
- *
+ * empty rooms
  */
 
 #include <iostream>
@@ -12,7 +12,15 @@ using namespace std;
 
 int main ()
 {
+  int tot, ocu, max;
+  cin >> tot >> ocu;
 
-
+  if (ocu == 0 || ocu == tot) cout << "0 0";
+  else
+  {
+    if (ocu <= tot/3) max = 2*ocu;
+    else max = tot-ocu;
+    cout << "1 " << max;
+  }
   return 0;
 }
