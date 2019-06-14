f7382b8f91bcc951e0bc98a8216be7174952a526
Exr0n <spotyie@gmail.com>
2019-06-14 12:00:49 -0700

    contest 1152 over

--- problems/contests/1152/1/main.cpp
+++ problems/contests/1152/1/main.cpp
@@ -1,7 +1,7 @@
 /*
  * Post-array test (sub): Problem #1
  * 14 June 2019
- * 
+ * spiral matrix
  */
 
 #include <iostream>
@@ -11,10 +11,60 @@
 
 using namespace std;
 
-
 int main ()
 {
+  int matrix [20][20];
+  bool flop = false;
+  int size, slen, py=0, px=-1, dir=1, num=0;
+  cin >> size;
+  slen = size;
 
+  for (int b=1; b<2*size; ++b)
+  {
+    //cout << dir << " " << slen << ":" << endl;
+    for (int s=0; s<slen; ++s)
+    {
+      //cout << "  " << dir << " " << py << " " << px << endl;
+      switch (dir)
+      {
+        case 0: -- py;
+          break;
+        case 1:
+          ++ px;
+          break;
+        case 2:
+          ++ py;
+          break;
+        case 3:
+          -- px;
+          break;
+      }
+      matrix[py][px] = ++num;
+    }
+    dir = (dir + 1) % 4;
+    flop = ! flop;
+    if (flop) --slen;
+  }
+
+  for (int i=0; i<size; ++i)
+  {
+    for (int j=0; j<size; ++j)
+    {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
 
   return 0;
 }
+
+/*
+ 1 2 3 4 5
+ 16 17 18 19 6
+ 15 24 25 20 7
+ 14 23 22 21 8
+ 13 12 11 10 9
+
+
+
+ */
--- problems/contests/1152/6/main.cpp
+++ problems/contests/1152/6/main.cpp
@@ -1,7 +1,7 @@
 /*
  * Post-array test (sub): Problem #6
  * 14 June 2019
- *
+ * line block
  */
 
 #include <iostream>
@@ -9,12 +9,43 @@
 #include <cmath>
 #include <vector>
 
+#define PI 3.14159265
+
 using namespace std;
 
 
 int main ()
 {
+  int max, pos, rad=-PI, trad, dist=40, fin=0;
+  cin >> max, pos;
+  int line [5010];
+  for (int i=0; i<max; ++i) cin >> line[i];
 
+  for (int i=pos-1; i>=0; --i)
+  {
+    trad = asin( (line[i]-line[pos]) / (pos-i)*dist );
+    if (trad >= rad)
+    {
+      ++ fin;
+      rad = trad;
+    }
+    else
+      break;
+  }
+
+  for (int i=pos+1; i<max; ++i)
+  {
+    trad = asin( (line[i]-line[pos]) / (i-pos)*dist );
+    if (trad >= rad)
+    {
+      ++ fin;
+      rad = trad;
+    }
+    else
+      break;
+  }
+
+  cout << fin;
 
   return 0;
 }
