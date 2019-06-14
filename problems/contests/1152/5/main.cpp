5169a23696e53d2f9946f994a35693fb3204ec79
Exr0n <spotyie@gmail.com>
2019-06-14 09:57:09 -0700

    create base for contest folder 1152

---
+++ problems/contests/1152/5/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Post-array test (sub): Problem #5
+ * 14 June 2019
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+int main ()
+{
+
+
+  return 0;
+}
