3ceb86f790c1c1917ca41109b3cdff2f427a8420
Exr0n <spotyie@gmail.com>
2019-07-12 21:08:53 +0800

    script: creating new problem folder: 2.11 7981

---
+++ problems/2.11/7981/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem #7981
+ * 
+ */
+
+#include <bits/stdc++.h> // includes "everything"
+#define ll long long
+#define dl double
+
+using namespace std;
+
+
+int main ()
+{
+
+
+  return 0;
+}
