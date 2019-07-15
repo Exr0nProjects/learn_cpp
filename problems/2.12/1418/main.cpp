23452e249ed242552439cc013d8d6c9a848db02c
Exr0n <spotyie@gmail.com>
2019-07-15 18:38:56 +0800

    script: creating new problem folder: 2.12 1418

---
+++ problems/2.12/1418/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem #1418
+ * 
+ */
+
+#include <bits/stdc++.h> // includes "everything"
+#define ll long long
+#define dl double
+
+using namespace std;
+
+
+int main ()
+{
+
+
+  return 0;
+}
