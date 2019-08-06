bcbb474b59dbaac45493cdd174ae72ea5a89cd14
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-06 20:07:06 +0800

    script: AC xjoi.net/x8359

---
+++ problems/xjoi.net/x8359/main.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem #8359
+ * Tue 06 Aug 2019 @ 20:04 CST)
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int a, b, c, d;
+  scanf("%d%d%d%d", &a, &b, &c, &d);
+  
+  printf("%d", abs(a-c) + abs(b-d));
+
+  return 0;
+}
