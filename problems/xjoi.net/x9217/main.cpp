76867d2b1b5a3642aa6d14d46fda277947c62726
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-06 19:56:41 +0800

    script: AC xjoi.net/x9217

---
+++ problems/xjoi.net/x9217/main.cpp
@@ -0,0 +1,48 @@
+/*
+ * Problem #9217
+ * Tue 06 Aug 2019 @ 19:52 CST)
+ * chicken leg
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int num, c;
+  scanf("%d%d", &c, &num);
+  num /= c;
+  
+  printf("%d", num + num/3);
+
+  return 0;
+}
