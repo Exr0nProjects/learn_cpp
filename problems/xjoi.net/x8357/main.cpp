e48d3289ec772e4409998c61c7827f3f50bbe5ec
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-06 19:41:37 +0800

    script: AC xjoi.net/x8357

---
+++ problems/xjoi.net/x8357/main.cpp
@@ -0,0 +1,49 @@
+/*
+ * Problem #8357
+ * Tue 06 Aug 2019 @ 19:39 CST)
+ * div, mod
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int num;
+  scanf("%d", &num);
+  int ans = 0;
+  if (num % 5) ++ ans;
+  ans += num/5;
+  printf("%d", ans);
+
+  return 0;
+}
