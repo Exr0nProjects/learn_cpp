74fda23bccec101a6305e8f7fb7c23dbe6c2dfab
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-06 22:23:00 +0800

    script: AC xjoi.net/x9211

---
+++ problems/xjoi.net/x9211/main.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem #9211
+ * Tue 06 Aug 2019 @ 21:59 CST)
+ * pythagorean
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int a, b;
+  scanf("%d%d", &a, &b);
+  
+  printf("%d", (int) sqrt(a*a+b*b));
+  
+  return 0;
+}
