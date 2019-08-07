566db0d8721044adc64cf60670e33352e16c03ed
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-07 11:12:02 +0800

    script: AC xjoi.net/x9310

---
+++ problems/xjoi.net/x9310/main.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem #9310
+ * Wed 07 Aug 2019 @ 11:09 CST)
+ * even
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int num; scanf("%d",  &num);
+  
+  printf("This is an integer.");
+  if (! (num&1)) printf(" It's even.");
+
+  return 0;
+}
