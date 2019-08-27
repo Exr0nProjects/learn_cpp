63612250923a7c702130646939f9a66386d62bfc
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-27 07:40:39 -0700

    WA 7974

---
+++ problems/xjoi.net/7974/main.cpp
@@ -0,0 +1,56 @@
+/*
+ ID: spoytie2
+ TASK: XXXX
+ LANG: C++14
+ */
+
+/*
+ * Problem #7974
+ * Tue 27 Aug 2019 @ 07:33 PDT)
+ *
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+const int dfc (const int r, const int c=1)
+{
+  if (r == 1) return c;
+  return c*dfc(r-1, c+1) % 998244353;
+}
+int main ()
+{
+  int num;
+  scanf("%d", &num);
+  printf("%d", dfc(num)%998244353);
+  return 0;
+}
