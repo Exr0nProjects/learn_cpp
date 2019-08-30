5c921c686d5d64528a5befc367a088bf69b35b53
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-30 06:55:20 -0700

    AC test/c (script)

---
+++ problems/test/c/main.cpp
@@ -0,0 +1,50 @@
+/*
+ID: spoytie2
+TASK: XXXX
+LANG: C++14                 
+*/
+
+/*
+ * Problem c (test/c)
+ * Created Fri 30 Aug 2019 @ 06:55 (PDT)
+ * Accepted [!meta:end!]
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+
+  return 0;
+}
