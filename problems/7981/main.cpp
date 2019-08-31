5bf4bbf6595a0d2ce7dd7af34b5bfe2e869fe030
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-30 17:12:35 -0700

    PLM 7981

---
+++ problems/7981/main.cpp
@@ -0,0 +1,53 @@
+/*
+ID: spoytie2
+TASK: XXXX
+LANG: C++14                 
+*/
+
+/*
+ * Problem 7981 (7981)
+ * Created Fri 30 Aug 2019 @ 17:10 (PDT)
+ * Accepted [!meta:end!]
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int num;
+  scanf("%d", &num);
+  printf("%d", pow(2, num) -1);
+
+  return 0;
+}
