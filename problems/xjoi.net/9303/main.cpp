51023de97b721b878490ee8e99accbbdb5c4099b
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-09-01 15:54:13 -0700

    PLM 9303

---
+++ problems/xjoi.net/9303/main.cpp
@@ -0,0 +1,60 @@
+/*
+ID: spoytie2
+TASK: XXXX
+LANG: C++14                 
+*/
+
+/*
+ * Problem 9303 (xjoi.net/9303)
+ * Created Sun 01 Sep 2019 @ 15:46 (PDT)
+ * Accepted [!meta:end!]
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int num;
+  scanf("%d", &num);
+  
+  int d = 0;
+  for (int i=num%10; num != 0; num /= 10)
+  {
+    d += pow(3, i);
+  }
+
+  if (d == num) printf("YES");
+  else printf("NO");
+  return 0;
+}
