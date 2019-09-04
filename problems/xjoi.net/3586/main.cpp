c09f458bc4de4b5649a4815d13e918b7b6ffda55
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-09-03 20:00:44 -0700

    WIP 3586 t+27m

--- problems/xjoi.net/3586/main.cpp
+++ problems/xjoi.net/3586/main.cpp
@@ -1,14 +1,14 @@
 /*
-ID: spoytie2
-TASK: XXXX
-LANG: C++14                 
-*/
+ ID: spoytie2
+ TASK: XXXX
+ LANG: C++14
+ */
 
 /*
  * Problem 3586 (xjoi.net/3586)
  * Created Tue 03 Sep 2019 @ 19:01 (PDT)
  * Accepted [!meta:end!]
- * 
+ *
  */
 
 #include <bits/stdc++.h>
@@ -44,21 +44,24 @@ using namespace std;
 const int MAXSZ = 210; // todo
 struct Block
 {
-  int i, j, v[3];
+  int i, j, v[4] = { };
   bool is_kentucky_fried_chicken_resturaunt = false;
 } matrix[MAXSZ][MAXSZ];
 
 int n, m;
 
-void bfs(const Block& cur, const int t)
+void bfs(Block cur, const int t)
 {
+  matrix[cur.i][cur.j].v[t] = 1;
   queue<Block> q;
-  for (q.push(cur); !q.empty(); q.pop())
+  for (q.push(matrix[cur.i][cur.j]); !q.empty(); q.pop())
   {
-    if (cur.i-1 > 0   && matrix[cur.i-1][cur.j] == 0) { matrix[cur.i-1][cur.j].v[t] = matrix[cur.i][cur.j].v[t]+1; q.push(matrix[cur.i-1][cur.j]); }
-    if (cur.j-1 > 0   && matrix[cur.i][cur.j-1] == 0) { matrix[cur.i][cur.j-1].v[t] = matrix[cur.i][cur.j].v[t]+1; q.push(matrix[cur.i][cur.j-1]); }
-    if (cur.i+1 < n-1 && matrix[cur.i+1][cur.j] == 0) { matrix[cur.i+1][cur.j].v[t] = matrix[cur.i][cur.j].v[t]+1; q.push(matrix[cur.i+1][cur.j]); }
-    if (cur.j+1 < m-1 && matrix[cur.i][cur.j+1] == 0) { matrix[cur.i][cur.j+1].v[t] = matrix[cur.i][cur.j].v[t]+1; q.push(matrix[cur.i][cur.j+1]); }
+    cur = q.front();
+    //printf("(%d, %d)\n", cur.i, cur.j); for (int i=0; i<n; ++i) { printf("  "); for (int j=0; j<m; ++j) printf("%3d", matrix[i][j].v[t]); printf("\n"); } printf("\n");
+    if (cur.i-1 >= 0 && matrix[cur.i-1][cur.j].v[t] == 0) { matrix[cur.i-1][cur.j].v[t] = cur.v[t]+1; q.push(matrix[cur.i-1][cur.j]); }
+    if (cur.j-1 >= 0 && matrix[cur.i][cur.j-1].v[t] == 0) { matrix[cur.i][cur.j-1].v[t] = cur.v[t]+1; q.push(matrix[cur.i][cur.j-1]); }
+    if (cur.i+1 <  n && matrix[cur.i+1][cur.j].v[t] == 0) { matrix[cur.i+1][cur.j].v[t] = cur.v[t]+1; q.push(matrix[cur.i+1][cur.j]); }
+    if (cur.j+1 <  m && matrix[cur.i][cur.j+1].v[t] == 0) { matrix[cur.i][cur.j+1].v[t] = cur.v[t]+1; q.push(matrix[cur.i][cur.j+1]); }
   }
 }
 
@@ -68,35 +71,57 @@ int main ()
   scanf("%d%d", &n, &m);
   for (int i=0; i<n; ++i)
   {
+    scanf("\n");
     for (int j=0; j<m; ++j)
     {
-      matrix[i][j];
       matrix[i][j].i = i;
       matrix[i][j].j = j;
-
+      
       char t;
-      scanf("%d", &t);
-      if (t == '#') matrix[i][j].v1 = matrix[i][j].v2 = -1;
+      scanf("%c", &t);
+      if (t == '#') matrix[i][j].v[1] = matrix[i][j].v[2] = -1;
       if (t == 'F') matrix[i][j].is_kentucky_fried_chicken_resturaunt = true;
       if (t == '@') { sai = i; saj = j; }
       if (t == '&') { sbi = i; sbj = j; }
     }
   }
-
-  matrix[sai][saj].v1 = matrix[sbi][sbj].v2 = 1;
-  bfs(matrix[sai][saj]);
-  bfs(matrix[sbi][sbj]);
-
+  
+  bfs(matrix[sai][saj], 1);
+  bfs(matrix[sbi][sbj], 2);
+  
   int mmin = 1000000000;
   for (int i=0; i<n; ++i) for (int j=0; j<m; ++j)
   {
     if (matrix[i][j].is_kentucky_fried_chicken_resturaunt
-     && matrix[i][j].v1 > 0 && matrix[i][j].v2 > 0)
-      mmin = min(mmin, matrix[i][j].v1 + matrix[i][j].v2);
+        && matrix[i][j].v[1] > 0 && matrix[i][j].v[2] > 0)
+      mmin = min(mmin, matrix[i][j].v[1] + matrix[i][j].v[2]);
   }
-
+  
   if (mmin == 1000000000) printf("-1");
-  else printf("%d", mmin);
-
+  else printf("%d", mmin-2);
+  
   return 0;
 }
+
+/*
+
+4 4
+@.#F
+....
+.#..
+F..&
+
+ 
+3 3
+@.&
+###
+FFF
+
+2 8
+@......F
+&..#....
+ 
+1 5
+@K#.&
+
+*/
