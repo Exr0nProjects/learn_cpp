8aa4f7c7a5562199523495dc8feac1eac6af4db8
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-09-06 07:19:15 -0700

    AC xjoi.net/3585 (script)

--- problems/xjoi.net/x3585/main.cpp
+++ problems/xjoi.net/x3585/main.cpp
@@ -1,14 +1,14 @@
 /*
-ID: spoytie2
-TASK: XXXX
-LANG: C++14                 
-*/
+ ID: spoytie2
+ TASK: XXXX
+ LANG: C++14
+ */
 
 /*
  * Problem 3585 (xjoi.net/3585)
  * Created Fri 06 Sep 2019 @ 06:36 (PDT)
- * Accepted [!meta:end!]
- * 
+ * Accepted Fri 06 Sep 2019 @ 07:19 (PDT)
+ *
  */
 
 #include <bits/stdc++.h>
@@ -45,9 +45,12 @@ const int MAXSZ = 210; // todo
 struct Block
 {
   int i, j, val, add;
-  bool operator< (const Block& o)
-  { return this->val < o.val; }
+  bool operator< (const Block&) const;
 } mat[MAXSZ][MAXSZ];
+
+bool Block::operator< (const Block& rhs) const
+{ return this->val > rhs.val; }
+
 int a, b;
 
 void bfs (const int ci, const int cj)
@@ -56,11 +59,13 @@ void bfs (const int ci, const int cj)
   priority_queue<Block> pq;
   for (pq.push(mat[ci][cj]); !pq.empty(); pq.pop())
   {
-    c = pq.front();
+    // printf("%d, %d:\n", pq.top().i, pq.top().j); for (int i=0; i<a; ++i) { printf("  "); for (int j=0; j<b; ++j) printf("%3d", mat[i][j].val); printf("\n"); }
+    
+    Block c = pq.top();
     if (c.i > 0   && mat[c.i-1][c.j].val == 0) { mat[c.i-1][c.j].val = c.val+1+mat[c.i-1][c.j].add; pq.push(mat[c.i-1][c.j]); }
     if (c.j > 0   && mat[c.i][c.j-1].val == 0) { mat[c.i][c.j-1].val = c.val+1+mat[c.i][c.j-1].add; pq.push(mat[c.i][c.j-1]); }
     if (c.i < a-1 && mat[c.i+1][c.j].val == 0) { mat[c.i+1][c.j].val = c.val+1+mat[c.i+1][c.j].add; pq.push(mat[c.i+1][c.j]); }
-    if (c.j > b-1 && mat[c.i][c.j+1].val == 0) { mat[c.i][c.j+1].val = c.val+1+mat[c.i][c.j+1].add; pq.push(mat[c.i][c.j+1]); }
+    if (c.j < b-1 && mat[c.i][c.j+1].val == 0) { mat[c.i][c.j+1].val = c.val+1+mat[c.i][c.j+1].add; pq.push(mat[c.i][c.j+1]); }
   }
 }
 
@@ -76,18 +81,32 @@ int main ()
       mat[i][j].i = i;
       mat[i][j].j = j;
       char t;
-      scanf("%d", &t);
+      scanf("%c", &t);
       if (t == '#') mat[i][j].val = -1;
       if (t == 'G') mat[i][j].add = 1;
       if (t == '@') { si = i; sj = j; }
       if (t == 'M') { ei = i; ej = j; }
     }
   }
-
+  
   bfs(si, sj);
-
+  
   if (mat[ei][ej].val == 0) printf("You can't save Mengxin");
-  else printf("%d", mat[ei][ej].val);
+  else printf("%d", mat[ei][ej].val-1);
   
   return 0;
 }
+
+/*
+
+2 2
+M.
+G@
+
+4 4
+@..G
+...G
+....
+GGGM
+
+*/
