31a7259c0d427e4c45fda9becfe4e82a39fda793
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-09-06 19:56:10 -0700

    PLM 3541

---
+++ problems/xjoi.net/3541/main.cpp
@@ -0,0 +1,54 @@
+/*
+ID: spoytie2
+TASK: XXXX
+LANG: C++14                 
+*/
+
+/*
+ * Problem 3541 (xjoi.net/3541)
+ * Created Fri 06 Sep 2019 @ 19:54 (PDT)
+ * Accepted [!meta:end!]
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  int num;
+  scanf("%d", &num);
+  int num = (int) sqrt(num);
+  printf("%d", num);
+
+  return 0;
+}
