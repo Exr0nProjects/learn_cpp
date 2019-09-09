657ec5ab1090569f3a2cc83f34fc374439a9fb81
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-09-08 18:05:52 -0700

    class notes update

---
+++ notes/X-Camp-401/day1.md
@@ -0,0 +1,9 @@
+# homework
+understand floyd warshal. why does it work?
+connect with other concepts. 
+it is a dp problem, why? how does it work? proof?
+
+topological sort + cycles = strongy connected components. research?
+negative edges?
+"don't treat it as a task, rather an exploration"
+should be able to answer imedietly when asked, otherwise you probably don't know it well enough... able to teach other people.
---
+++ problems/xjoi.net/3649/int_array.cpp
@@ -0,0 +1,54 @@
+/*
+ ID: spoytie2
+ TASK: XXXX
+ LANG: C++14
+ */
+
+/*
+ * Problem 3649 (xjoi.net/3649)
+ * Created Sat 08 Sep 2019 @ 14:06 (PDT)
+ * Accepted [!meta:end!]
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 350; // todo
+int digits[350];
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    
+       
+
+    return 0;
+}
