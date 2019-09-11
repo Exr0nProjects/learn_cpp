af306c5523392b7c2377edf2c1984c6bfcd4760a
= <25516241+Exr0n@users.noreply.github.com>
2019-09-10 19:34:55 -0700

    TLE xj8101 t+20m

--- problems/xjoi.net/8101/main.cpp
+++ problems/xjoi.net/8101/main.cpp
@@ -1,14 +1,14 @@
 /*
-ID: spoytie2
-TASK: XXXX
-LANG: C++14                 
-*/
+ ID: spoytie2
+ TASK: XXXX
+ LANG: C++14
+ */
 
 /*
  * Problem 8101 (xjoi.net/8101)
  * Created Tue 10 Sep 2019 @ 18:57 (PDT)
  * Accepted [!meta:end!]
- * 
+ *
  */
 
 #include <bits/stdc++.h>
@@ -43,20 +43,53 @@ using namespace std;
 
 const int MAXSZ = 100010; // todo
 int boxes[MAXSZ];
+int adj[MAXSZ];
 
-int main ()
+int main()
 {
-  int num, mmax, ret=0;
+  int num, mmax, cmax = 1000000000, ret = 0;
   scanf("%d%d", &num, &mmax);
-  for (int i=0; i<num; ++i) scanf("%d", &boxes[i]);
-  for (int i=0; i<num-1; ++i) boxes[i] += boxes[i+1];
-  for (int i=0; i<num-2; ++i)
+  for (int i = 0; i < num; ++i)
+    scanf("%d", &boxes[i]);
+
+  if (num == 1)
   {
-    if (boxes[i] > mmax)
+    printf("%d", boxes[0] - mmax);
+    return 0;
+  }
+
+  for (int i = 0; i < num - 1; ++i)
+    adj[i] = boxes[i] + boxes[i + 1];
+  while (cmax > mmax)
+  {
+    cmax = 0;
+    for (int i = 0; i < num - 1; ++i)
     {
-      ++ret;
-      --boxes[i];
+      cmax = max(cmax, adj[i]);
+      if (adj[i] > mmax)
+      {
+        if (i > 0 && adj[i - 1] > mmax && boxes[i] > 0)
+        {
+          --boxes[i];
+          adj[i - 1] = boxes[i - 1] + boxes[i];
+          ++ret;
+        }
+        else if (i < num - 2 && boxes[i + 1] > 0)
+        {
+          --boxes[i + 1];
+          adj[i + 1] = boxes[i + 1] + boxes[i + 2];
+          ++ret;
+        }
+        else if (boxes[i] > 0)
+        {
+          --boxes[i];
+          adj[i - 1] = boxes[i - 1] + boxes[i];
+          ++ret;
+        }
+        adj[i] = boxes[i] + boxes[i + 1];
+      }
     }
+    //for (int i=0; i<num; ++i) printf("%5d", boxes[i]); printf("\n  "); for (int i=0; i<num-1; ++i) printf("%5d", adj[i]); printf("\n\n");
   }
 
   printf("%d", ret);
