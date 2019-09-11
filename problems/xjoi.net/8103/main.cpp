0cffd8af99820858098a65a2abdfdc4bd57c9ae0
= <25516241+Exr0n@users.noreply.github.com>
2019-09-11 08:37:36 -0700

    WIP 8103 t+5m

---
+++ problems/xjoi.net/8103/main.cpp
@@ -0,0 +1,63 @@
+/*
+ID: spoytie2
+TASK: XXXX
+LANG: C++14                 
+*/
+
+/*
+ * Problem 8103 (xjoi.net/8103)
+ * Created Wed 11 Sep 2019 @ 08:33 (PDT)
+ * Accepted [!meta:end!]
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 5010; // todo
+int apples[MAXSZ];
+
+int main ()
+{
+  int n, s, a, b;
+  scanf("%d%d%d%d", &n, &s, &a, &b);
+  for (int i=0; i<n; ++i) scanf("%d", &apples[i]);
+
+  for (int cmin=-1; cmin < s; ++cmin)
+  {
+    int mindx = -1;
+    for (int i = 0; i < n; ++i)
+    {
+      if ()
+    }
+  }
+
+  return 0;
+}
