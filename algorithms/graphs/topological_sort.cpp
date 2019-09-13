9fc9a5d9f53a256e3f063b5f33f8b56f8366ab3a
= <25516241+Exr0n@users.noreply.github.com>
2019-09-12 21:34:03 -0700

    topolgical sort

---
+++ algorithms/graphs/topological_sort.cpp
@@ -0,0 +1,21 @@
+/*
+ 11 Sep 2019
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXSZ = 110;
+struct Edge
+{
+    int f, t;
+} edges[MAXSZ*2];
+int head[MAXSZ];
+
+
+int main()
+{
+    int p, q;
+    scanf("%d%d", &p, &q);
+    
+}
