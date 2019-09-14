9785d20da132afe6d5d0459e0571c984f0b538bb
= <25516241+Exr0n@users.noreply.github.com>
2019-09-14 15:36:29 -0700

    topo_sort finished

--- algorithms/graphs/topological_sort.cpp
+++ algorithms/graphs/topological_sort.cpp
@@ -8,19 +8,25 @@ using namespace std;
 const int MAXSZ = 110;
 struct Edge
 {
-    int f, t, n, rem;
+    int f, t, n;
 } edges[MAXSZ*2];
 int head[MAXSZ];
+int rem[MAXSZ];
 
 void addEdge (const int a, const int b)
 {
     static int c = 1;
     edges[c].f = a;
     edges[c].t = b;
-    edges[c].rem = edges[head[a]].rem + 1;
     edges[c].n = head[a];
     head[a] = c;
     ++c;
+    ++rem[b];
+}
+
+auto comp (const int& r, const int& l)
+{
+    return rem[r] > rem[l];
 }
 
 int main()
@@ -28,8 +34,7 @@ int main()
     int p, q;
     scanf("%d%d", &p, &q);
     
-    auto comp = [](const int& r, const int& l){return edges[head[r]].rem < edges[head[l]].rem;};
-    priority_queue<int, vector<int>, decltype(comp) > pq;
+    priority_queue<int, vector<int>, function<bool(int,int)> > pq(comp);
     for (int i=0; i<q; ++i)
     {
         int a, b;
@@ -37,12 +42,28 @@ int main()
         addEdge(a, b);
     }
     
-    for (int i=0; i<p; ++i) pq.push(i);
+    for (int i=1; i<=p; ++i) pq.push(i);
     
     for (; !pq.empty(); pq.pop())
     {
+        if (rem[pq.top()])
+        {
+            printf("\nNot a DAG!");
+            break;
+        }
         printf("%d ", pq.top());
+        for (int p=head[pq.top()]; p; p = edges[p].n)
+        {
+            --rem[edges[p].t];
+        }
     }
     
     return 0;
 }
+
+/*
+4 3
+1 3
+3 2
+2 4
+*/
