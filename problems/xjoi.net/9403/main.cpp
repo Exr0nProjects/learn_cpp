691d4a66173831c530de4d61728ca2fcfcd5e038
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-09-16 07:15:25 -0700

    TLE xj9403

--- problems/xjoi.net/9403/main.cpp
+++ problems/xjoi.net/9403/main.cpp
@@ -1,14 +1,14 @@
 /*
-ID: spoytie2
-TASK: XXXX
-LANG: C++14                 
-*/
+ ID: spoytie2
+ TASK: XXXX
+ LANG: C++14
+ */
 
 /*
  * Problem 9403 (xjoi.net/9403)
  * Created Sun 15 Sep 2019 @ 14:59 (PDT)
  * Accepted [!meta:end!]
- * 
+ *
  */
 
 #include <bits/stdc++.h>
@@ -46,15 +46,17 @@ const int MAXSZ = 10010; // todo
 int bricks[MAXSZ];
 int num, siz;
 
-int dfc (cn p=0, cn l=0)
+int dfc (cn p=-1, cn l=1)
 {
   int count = 0;
   if (l >= siz) count = 1;
-  if (p == num) return count;
-
-  for (int i=p; i<num; ++i)
+  if (p == num-1) return count;
+  
+  for (int i=p+1; i<num; ++i)
   {
+    //for (int i=0; i<l; ++i) printf("  "); printf("%d\n", bricks[i]);
     count += dfc(i, l+1);
+    count %= 1000000007;
   }
   return count;
 }
@@ -66,9 +68,17 @@ int main ()
   {
     scanf("%d", &bricks[i]);
   }
-  sort(bricks, bricks+num);
-
-  printf("%d", dfc(num));
-
+  sort(bricks, bricks+num, greater<int>());
+  
+  printf("%d", dfc()-1);
+  
   return 0;
 }
+
+/*
+1 1
+10
+ 
+4 1
+1 2 3 100
+*/
