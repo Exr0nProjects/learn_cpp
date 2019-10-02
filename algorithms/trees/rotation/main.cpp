e7e5b6bd095629d6ae075e9f1ff13a0e32707123
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-10-02 07:06:54 -0700

    final tree rotation

--- algorithms/trees/rotation/main.cpp
+++ algorithms/trees/rotation/main.cpp
@@ -11,77 +11,92 @@ const int MAXSZ = 100;
 struct Node
 {
   int value = NULL;
-  Node* left = NULL;
-  Node* right = NULL;
+  Node *left = NULL;
+  Node *right = NULL;
 };
 
-Node& go_left (Node& base);
-
-Node& go_right (Node& base)
+Node *go_right(Node *base)
 {
-  Node* ret = base.left;
-  base.left = (ret->right);
-  ret->right = &base;
+  printf("rotating node %d right!\n", base->value);
+  Node *ret = base->left;
+  base->left = (ret->right);
+  ret->right = base;
 
-  return *ret;
+  return ret;
 }
 
-Node& go_left (Node& base)
+Node *go_left(Node *base)
 {
+  printf("rotating node %d right!\n", base->value);
+  Node *ret = base->right;
+  base->right = (ret->left);
+  ret->left = base;
 
-  Node* ret = base.right;
-  base.right = (ret->left);
-  ret->left = &base;
-
-  return *ret;
+  return ret;
 }
 
-void input (Node* cur)
+void input(Node *cur)
 {
-  if (cur->value < 0) return;
+  if (cur == NULL || cur->value < 0)
+    return;
   int a, b;
   scanf("%d%d", &a, &b);
   cur->left = new Node();
+  cur->right = new Node();
   cur->left->value = a;
-  cur->left->value = b;
+  cur->right->value = b;
   input(cur->left);
   input(cur->right);
 }
 
 queue<Node *> q;
-void add_print_queue ()
+void add_print_queue()
 {
-  if (! q.empty())
+  if (!q.empty())
   {
-    Node* c = q.front();
+    Node *c = q.front();
     if (c)
     {
       q.push(c->left);
       q.push(c->right);
+      printf("%3d", c->value);
     }
-    printf("%3d", c->value);
     q.pop();
     add_print_queue();
   }
 }
-void print_tree(Node* root)
+void print_tree(Node *root)
 {
-  for (; !q.empty(); q.pop());
+  for (; !q.empty(); q.pop())
+    ;
   q.push(root);
   add_print_queue();
+  printf("\n");
 }
 
-int main ()
+int main()
 {
-  int n;
-  scanf("%d", &n);
+  Node *root = new Node();
+  Node *cur = root;
 
-  Node* root = new Node();
-  Node* cur = root;
-
+  scanf("%d", &cur->value);
   input(root);
-  
 
+  print_tree(root);
+
+  root = go_right(root);
+  print_tree(root);
+
+  root = go_left(root);
+  print_tree(root);
+  root = go_left(root);
+  print_tree(root);
 
   return 0;
 }
+
+/*
+1 -1 -1
+
+1 2 3 -1 -1 -1 4 -1 -1
+*/
