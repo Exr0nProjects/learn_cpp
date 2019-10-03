5fa7efd8c8f30536e7a43cfcd30056130c12842d
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-10-02 20:00:47 -0700

    commit lag

--- algorithms/graphs/scc.cpp
+++ algorithms/graphs/scc.cpp
@@ -14,6 +14,7 @@ struct Edge
 } edges[MAXSZ * 2];
 int head[MAXSZ];
 int dist[MAXSZ];
+int m, n;
 
 void addEdge(cn a, cn b)
 {
@@ -27,33 +28,45 @@ void addEdge(cn a, cn b)
 
 void dfs(cn c, queue<int> q)
 {
+    printf("dfs called, c=%d: ", c);
+    auto p = q;
+    printf("\n");
     q.push(c);
-    int m = MAXSZ;
+    int mn = MAXSZ;
     int mi = 0;
     for (int i = head[c]; i; i = edges[i].n)
     {
-        if (dist[edges[i].t] > 0 && m > dist[edges[i].t])
+        if (dist[edges[i].t] > 0 && mn > dist[edges[i].t])
         {
-            dist[edges[i].t] = dist[c] + 1;
-            m = dist[edges[i].t];
+            mn = dist[edges[i].t];
             mi = edges[i].t;
         }
+        printf("    in %d->%d  ", edges[i].f, edges[i].t);
+        for (int j = 1; j <= m; ++j)
+            printf("%3d", dist[j]);
+        printf("\n");
         if (!dist[edges[i].t])
+        {
+            dist[edges[i].t] = dist[c] + 1;
             dfs(edges[i].t, q);
+        }
     }
     if (mi > 0)
     {
-        for (; !q.empty(); q.pop())
-            printf("%3d", q.front());
+        for (bool f = false; !q.empty(); q.pop())
+        {
+            if (q.front() == mn)
+                f = true;
+            if (f)
+                printf("%3d", q.front());
+        }
         printf("\n");
     }
 }
 
 int main()
 {
-    int m, n;
     scanf("%d%d", &m, &n);
-    printf("\n\n%d %d\n\n\n", m, n);
     for (int i = 1; i <= n; ++i)
     {
         int a, b;
@@ -79,4 +92,16 @@ int main()
 2 3
 3 1
 3 4
+
+8 9
+1 3
+3 2
+2 1
+1 5
+5 7
+7 4
+7 6
+6 4
+4 8
+4 5
  */
