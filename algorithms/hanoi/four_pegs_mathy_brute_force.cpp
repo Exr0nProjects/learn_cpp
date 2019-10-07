8aaef470623559901b95ae450436497109b5c232
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-10-07 16:40:05 -0700

    hanoi work

--- algorithms/hanoi/four_pegs_mathy_brute_force.cpp
+++ algorithms/hanoi/four_pegs_mathy_brute_force.cpp
@@ -6,38 +6,54 @@ f4(n) = min[f4(k)*2+f3(n-k) for k in [0:n]]
 
 #include <bits/stdc++.h>
 #define cn const int
+#define ull unsigned long long
 using namespace std;
-cn MAXSZ = 1000000;
-int best3[MAXSZ];
-int bestk[MAXSZ];
-int best4[MAXSZ];
+cn MAXSZ = 50;
+ull best3[MAXSZ];
+ull bestk[MAXSZ];
+ull best4[MAXSZ];
+
+inline void out(cn s, cn d) { printf("%c->%c", s + 65, d + 65); }
+
+void print3(cn n, cn s = 0, cn d = 2, int aux[] = {})
+{
+    if (n > 1)
+        print3(n - 1, s, (s + d - 1) % 3, aux);
+    printf("%c->%c", cnv(s), cnv(d));
+    if (n > 1)
+        print3(n - 1, )
+}
+
+void print4(cn n, cn s = 0, cn d = 3, int aux[] = {})
+{
+}
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; ++i)
+    ull n;
+    scanf("%lld", &n);
+    for (ull i = 0; i <= n; ++i)
     {
         best3[i] = (1 << i) - 1;
-        printf("%3d", best3[i]);
-        best4[i] = MAXSZ;
+        best4[i] = -1;
     }
     printf("\n");
 
     best4[0] = 0;
     best4[1] = 1;
 
-    for (int i = 1; i < n; ++i)
+    for (ull i = 1; i <= n; ++i)
     {
-        for (int k = 1; k < i; ++k)
+        for (ull k = 1; k < i; ++k)
         {
-            printf("%d %d => %d\n", i, k, best4[k] + best3[n - k]);
-            if (best4[k] * 2 + best3[n - k] < best4[i])
+            //d*/printf("%d-%d=%d => (%d %d) %d\n", i, k, i-k, best4[k], best3[i-k], best4[k]*2 + best3[i - k]);
+            if (best4[k] * 2 + best3[i - k] < best4[i])
             {
                 bestk[i] = k;
-                best4[i] = best4[k] + best3[n - k];
+                best4[i] = best4[k] * 2 + best3[i - k];
             }
         }
-        printf("%d: %d, %d\n\n", i, bestk[i], best4[i]);
+        //printf("%lld: %lld\n", i, best4[i]);
     }
+    print_steps((int)n);
 }
