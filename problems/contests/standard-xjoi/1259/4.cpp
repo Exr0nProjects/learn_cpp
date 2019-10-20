ec31a5befbfb0dbd06259f4fe4ffcd24e7824ad3
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-10-20 10:13:10 -0700

    broken 4 + bits of 6

--- problems/contests/standard-xjoi/1259/4.cpp
+++ problems/contests/standard-xjoi/1259/4.cpp
@@ -5,7 +5,7 @@ cn MAXSZ = 110;
 struct Edge
 {
     int f, t, n; // n can represent next or weight
-} edges[MAXSZ*2];
+} edges[MAXSZ * 2];
 int head[MAXSZ];
 int bigb[MAXSZ];
 int cnt = 1;
@@ -14,6 +14,7 @@ int n;
 
 void addEdge(cn a, cn b)
 {
+    printf("  adding %d, %d\n", a, b);
     edges[cnt].f = a;
     edges[cnt].t = b;
     edges[cnt].n = head[a];
@@ -21,44 +22,50 @@ void addEdge(cn a, cn b)
     ++cnt;
 }
 
-int find(cn c) // todo: problem area
+int find(cn c, cn p = 0) // todo: problem area
 {
-    int m = 0;
-    for (int e=head[c]; e; e=edges[e].n)
+    //    int m = 0;
+    //    for (int e=head[c]; e; e=edges[e].n)
+    //    {
+    //        if (m == 0) m = find(edges[e].t);
+    //        else m = min(m, find(edges[e].t));
+    //    }
+    //    if (m < bigb[c])
+    //    {
+    //        bigb[c] = m;
+    //    }
+    if (bigb[c] != c && bigb[c] != p)
     {
-        if (m == 0) m = find(edges[e].t);
-        else m = min(m, find(edges[e].t));
+        bigb[c] = find(bigb[c], c);
     }
-    if (m < bigb[c])
-    {
-        bigb[c] = m;
-    }
+    return bigb[c];
 }
 
-int main ()
+int main()
 {
     scanf("%d", &n);
-    for (int i=1; i<=n; ++i)
+    for (int i = 1; i <= n; ++i)
     { // init bigb
         bigb[i] = i;
     }
 
-    priority_queue<Edge*, vector<Edge*>, function<bool(Edge*, Edge*)> > pq([](const Edge* l, const Edge* r){ return l->n > r->n; });
-    for (int i=1; i<=n; ++i)
+    priority_queue<Edge *, vector<Edge *>, function<bool(Edge *, Edge *)>> pq([](const Edge *l, const Edge *r) { return l->n > r->n; });
+    for (int i = 1; i <= n; ++i)
     {
-        for (int j=1; j<=n; ++j)
+        for (int j = 1; j <= n; ++j)
         {
+            if (i == j)
+                continue;
             int w;
             scanf("%d", &w);
             if (w == 0)
             {
                 addEdge(i, j);
-                find(i);
-                find(j);
+                bigb[find(max(i, j))] = min(i, j);
             }
             else
             {
-                Edge* p = new Edge();
+                Edge *p = new Edge();
                 p->f = i;
                 p->t = j;
                 p->n = w;
@@ -66,14 +73,21 @@ int main ()
             }
         }
     }
-    int retc=0, retw=0;
-    for (; cnt < n*2-1; pq.pop())
+    for (int i = 1; i <= n; ++i)
+        printf("%3d", i);
+    printf("\n");
+    for (int i = 1; i <= n; ++i)
+        printf("%3d", find(i));
+    printf("\n\n");
+    int retc = 0, retw = 0;
+    printf("%d %d\n", cnt, n);
+    for (; cnt < n * 2 - 2; pq.pop())
     {
-        if (find(pq.top()->f) != find(pq.top()->t))
+        if (pq.top()->n != 0 && find(pq.top()->f) != find(pq.top()->t))
         {
+            printf("adding %d-%d: %d\n", pq.top()->f, pq.top()->t, pq.top()->n);
             addEdge(pq.top()->f, pq.top()->t);
-            find(pq.top()->f);
-            find(pq.top()->t);
+            bigb[find(max(pq.top()->f, pq.top()->t))] = min(pq.top()->f, pq.top()->t);
             ++retc;
             retw += pq.top()->n;
         }
---
+++ problems/contests/standard-xjoi/1259/6.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define cn const int;
+using namespace std;
+
+cn MAXSZ = 1010;
+struct Edge
+{
+    int f, t, w, n;
+} edges[MAXSZ*MAXSZ];
+int s, t, m, n, cnt=1; // m = nodes, n = edges
+
+void addEdge(cn a, cn b, cn w)
+{
+    edges[cnt].f = a;
+    edges[cnt].t = b;
+    edges[cnt].w = w;
+    edges[cnt].n = head[a];
+    head[a] = cnt;
+    ++cnt;
+}
+
+int main ()
+{
+    scanf("%d%d%d%d", &s, &t, &m, &n);
+    printf("%d %d: %d %d\n", s, t, m, n);
+    for (int i=1; i<=n; ++i)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a ,&b, &c);
+        addEdge(a, b, c);
+        addEdge(b, a, c);
+    }
+}
