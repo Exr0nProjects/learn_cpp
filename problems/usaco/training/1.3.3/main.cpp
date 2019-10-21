04f41fd48d52ceee5fcbc93b849b2a7e6afeca3d
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-10-21 07:49:52 -0700

    fix script oopsies

---
+++ problems/usaco/training/1.3.3/main.cpp
@@ -0,0 +1,52 @@
+/*
+ID: spoytie2
+TASK: XXXX
+LANG: C++14                 
+*/
+
+/*
+ * Problem 1.3.3 (usaco/training/1.3.3)
+ * Created Mon 21 Oct 2019 @ 07:47 (PDT)
+ * Accepted [!meta:end!]
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define cn const int
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+
+const int MAXSZ = 1; // todo
+
+int main ()
+{
+  const string path = 
+
+  return 0;
+}
