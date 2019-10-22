da6201eb37ff6290dce6a0e5d68de3908cbd5c08
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-10-22 06:38:14 -0700

    first debug #4 done, AC

--- problems/contests/standard-xjoi/1259/4.cpp
+++ problems/contests/standard-xjoi/1259/4.cpp
@@ -7,36 +7,46 @@ using namespace std;
 cn MAXSZ = 1010;
 typedef pair<int, int> edge;
 vector<pair<int, edge>> G; // graph
-int parent[MAXSZ]; // disjoint set
-int retn=0, retc=0;
+int parent[MAXSZ];         // disjoint set
+int retn = 0, retc = 0;
 
-void AddWeightedEdge(int u, int v, int w) {
+void AddWeightedEdge(int u, int v, int w)
+{
     G.push_back(make_pair(w, edge(u, v)));
 }
 
-int find_set(int i) {
+int find_set(int i)
+{
     if (i == parent[i])
         return i;
     else
         return find_set(parent[i]);
 }
 
-void union_set(int u, int v) {
+void union_set(int u, int v)
+{
     parent[u] = parent[v];
     // above is from pasted code, but don't we need what is below?
     //parent[find_set(u)] = parent[v];
 }
 
-void kruskal() {
+void kruskal()
+{
     int i, uRep, vRep;
     sort(G.begin(), G.end()); // increasing weight
-    for (i = 0; i < G.size(); i++) {
+    for (i = 0; i < G.size(); i++)
+    {
         uRep = find_set(G[i].second.first);
         vRep = find_set(G[i].second.second);
-        if (uRep != vRep) {
-            retc += G[i].first;
-            retn ++;
+        if (uRep != vRep)
+        {
+            //d*/     printf("adding %d-%d: %d\n", G[i].second.first, G[i].second.second, G[i].first);
             union_set(uRep, vRep);
+            if (G[i].first > 0)
+            {
+                retc += G[i].first;
+                retn++;
+            }
         }
     }
 }
@@ -47,19 +57,21 @@ int main()
     scanf("%d", &n);
 
     // disjoint set primer
-    for (int i=1; i<=n; ++i)
+    for (int i = 1; i <= n; ++i)
     {
         parent[i] = i;
     }
 
-    for (int i=1; i<=n; ++i)
+    for (int i = 1; i <= n; ++i)
     {
-        for (int j=1; j<=n; ++j)
+        for (int j = 1; j <= n; ++j)
         {
-            if (i == j) continue;
             int a;
             scanf("%d", &a);
-            AddWeightedEdge(a, i, j);
+            if (i == j)
+                continue;
+            //d*/ printf("  input: %d,%d : %d\n", i, j, a);
+            AddWeightedEdge(i, j, a);
         }
     }
     kruskal();
