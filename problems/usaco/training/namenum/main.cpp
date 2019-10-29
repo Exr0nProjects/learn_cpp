8917ca117819664831e937bfa9d0774a9ca7117d
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-10-28 17:17:40 -0700

    checking trie input

--- problems/usaco/training/namenum/main.cpp
+++ problems/usaco/training/namenum/main.cpp
@@ -1,14 +1,14 @@
 /*
 ID: spoytie2
 TASK: namenum
-LANG: C++14                 
+LANG: C++14
 */
 
 /*
  * Problem [!meta:pid!] (usaco/training/namenum)
  * Created Sun 27 Oct 2019 @ 17:28 (PDT)
  * Accepted [!meta:end!]
- * 
+ *
  */
 
 #include <bits/stdc++.h>
@@ -42,28 +42,42 @@ LANG: C++14
 
 using namespace std;
 
-const int MAXSZ = 1; // todo
+const int MAXSZ = 30; // todo
 long long n;
 struct TrieNode
 {
-  bool leaf=false;
-  TrieNode* c[30] = {}; // each capital letter
+  char v;
+  bool leaf = false;
+  TrieNode *c[MAXSZ] = {}; // each capital letter
+  TrieNode() { v = NULL; }
+  TrieNode(const char _v) { v = _v; }
 } root;
+
+void printTrie(TrieNode *c = &root, cn l = 0)
+{
+  for (int i = 0; i < l; ++i)
+    printf("| ");
+  printf("%c", c->v);
+  for (int i = 0; i < MAXSZ; ++i)
+  {
+    if (c->c[i])
+      printTrie(c->c[i], l + 1);
+  }
+}
 
 void dfs(string n)
 {
-  
 }
 
-int main ()
+int main()
 {
   auto fin = fopen("namenum.in", "r");
   auto fout = fopen("namenum.out", "w+");
 
   auto dict = fopen("dict.txt", "r"); // todo: space seperated? assuming newline
   char c;
-  TrieNode* p = &root;
-  for (fscanf(fin, "%c", &c); c; fscanf(fin, "%c", &c))
+  TrieNode *p = &root;
+  for (fscanf(dict, "%c", &c); c; fscanf(fin, "%c", &c))
   {
     if (c == '\n')
     {
@@ -71,12 +85,19 @@ int main ()
       p = &root;
       continue;
     }
-    if (! p->c[c-'A']) p->c[c-'A'] = new TrieNode();
-    p = p->c[c-'A'];
+    if (!p->c[c - 'A'])
+    {
+      p->c[c - 'A'] = new TrieNode(c);
+    }
+    p = p->c[c - 'A'];
   }
+
+  printTrie();
 
   char t[20];
   fscanf(fin, "%s", t);
+
+  fprintf(fout, "%s", t);
 
   dfs(t);
 
