94c5c62ea1433b6402b15f358b312e794f4c37ca
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-11-24 15:07:06 -0800

    WIP xjoi-1013

--- problems/xjoi.net/1013/main.cpp
+++ problems/xjoi.net/1013/main.cpp
@@ -8,40 +8,52 @@
 
 using namespace std;
 const int MAXSZ = 20;
-int n;
+int n, best = -1, mcount = 0;
 int cells[MAXSZ][MAXSZ];
 int dist[MAXSZ][MAXSZ];
 
-queue<pair<int, int> > q;
+void noop(){}
 
-int main ()
+void mydfs(const int i, const int j)
+{
+ //d*/ printf("dfs: %d %d\n", i, j);
+  if (i == j && j == n-1)
+  {
+    mcount += 1;
+    if (best == -1) best = dist[i][j];
+    else best = min(best, dist[i][j]);
+    return;
+  }
+  pair<int, int> pos[] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+  for (pair<int, int> p : pos)
+  {
+    if (i + p.first < 0 || i + p.second >= n
+     || j + p.second < 0 || j + p.second >= n // y out of range
+     || cells[i + p.first][j + p.second] >= 1 // not a valid spot
+     || dist[i + p.first][j + p.second] > 0) // already visited
+      continue;
+    dist[i+p.first][j+p.second] = dist[i][j] + 1;
+    mydfs(i+p.first, j+p.second);
+    dist[i+p.first][j+p.second] = 0;
+  }
+}
+
+int main()
 {
   scanf("%d", &n);
   for (int i=0; i<n; ++i)
   {
     for (int j=0; j<n; ++j)
     {
-      scanf("%d", cells[i][j]);
+      scanf("%d", &cells[i][j]);
     }
   }
-
+  
   dist[0][0] = 1;
-  for (q.push(pair<int, int>{0, 0}); !q.empty(); q.pop())
-  {
-    pair<int, int> pos[] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-    for (pair<int,int> p : pos)
-    {
-      if (q.top().first + p.first < 0 || q.top().first + p.first >= n
-       || q.top().second + p.secoond < 0 || q.top().second + p.second >= n
-       || cells[q.top().first+p.first][q.top().second+p.second] >= 1)
-        continue;
-      dist[q.top().first+p.first][q.top().second+p.second] = dist[q.top().first][q.top().second] + 1;
-      q.push(pair<int, int>{q.top().first + p.first, q.top().second + p.second});
-    }
-  }
+  mydfs(0, 0);
 
-  printf("%d", cells[n-1][n-1]);
-
+  cout << mcount << endl << best;
+  
   return 0;
 }
 
