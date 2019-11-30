97aa51035b57803774af391df6ceb6864dc70980
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-11-30 09:42:56 -0800

    AC script_test (script)

---
+++ problems/xscript_test/main_script_test.cpp
@@ -0,0 +1,59 @@
+/*
+ID: spoytie2
+TASK: script_test
+LANG: C++14                 
+*/
+
+/*
+ * Problem script_test (script_test)
+ * Created Sat 30 Nov 2019 @ 09:42 (PST)
+ * Accepted Sat 30 Nov 2019 @ 09:42 (PST)
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define cn const int
+#define ca const auto &
+#define UN unsigned
+#define ST static
+#define CN const
+#define LL long long
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+auto fin = fopen("script_test.in", "r");
+auto fout = nullptr;
+
+const int MAXSZ = -1; // todo
+
+
+
+int main ()
+{
+  if (fin) { fout = fopen("script_test.out", "w+"); } else { fin = stdin; fout = stdout; }
+
+  printf("idk what ur doing cuz this is only a script test!");
+
+  return 0;
+}
