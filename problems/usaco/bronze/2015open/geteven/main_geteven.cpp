4ecc31fab64a27bbdc16b362a7bf38968389ce74
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-12-13 19:44:51 -0800

    WIP 2015 open bronze 2 - geteven

---
+++ problems/usaco/bronze/2015open/geteven/main_geteven.cpp
@@ -0,0 +1,68 @@
+/*
+ID: spoytie2
+TASK: geteven
+LANG: C++14
+*/
+
+/*
+ * Problem geteven (usaco/bronze/2015open/geteven)
+ * Created Fri 13 Dec 2019 @ 19:32 (PST)
+ * Accepted [!meta:end!]
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define cn const int
+#define ca const auto &
+#define ll long long
+#define cl const long long
+#define UN unsigned
+#define ST static
+#define CN const
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+FILE* fin = fopen("geteven.in", "r");
+FILE* fout = nullptr;
+
+const int MAXSZ = 30; // todo
+int n;
+
+int conv(char c)
+{
+  switch (c)
+  {
+    
+  }
+}
+
+int main ()
+{
+  if (fin) { fout = fopen("geteven.out", "w+"); } else { fin = stdin; fout = stdout; }
+  
+  fscanf(fin, "%d", &s);
+
+
+  return 0;
+}
--- problems/usaco/bronze/2015open/timing_log.md
+++ problems/usaco/bronze/2015open/timing_log.md
@@ -2,4 +2,7 @@
 
 | time | note |
 18:50 | start
-17:05 | finished analysing original problem (have ideas for 1, 2 ,4), now starting work on #1
+19:05 | finished analysing original problem (have ideas for 1, 2 ,4), now starting work on #1
+19:27 | first version moocrypt doesn't work for sample test case, now trying #2 
+19:32 | starting writing nieve brute force for #2 bessie gets even
+19:43 | halfway through writing, just realized that #2 has much larger limits than I originally thought (20 per var instad of 20 max), now working on dfs-ing #4
