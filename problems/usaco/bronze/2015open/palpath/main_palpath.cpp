9ff7021425b4cba2f6769f788736d5eda24a8bd5
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-12-13 20:01:07 -0800

    PLM usaco 2015 open bronze #4 palpath

---
+++ problems/usaco/bronze/2015open/palpath/main_palpath.cpp
@@ -0,0 +1,98 @@
+/*
+ID: spoytie2
+TASK: palpath
+LANG: C++14
+*/
+
+/*
+ * Problem palpath (usaco/bronze/2015open/palpath)
+ * Created Fri 13 Dec 2019 @ 19:45 (PST)
+ * Accepted [!meta:end!]
+ *
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define cn const int
+#define ca const auto &
+#define ll long long
+#define cl const long long
+#define UN unsigned
+#define ST static
+#define CN const
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+FILE* fin = fopen("palpath.in", "r");
+FILE* fout = nullptr;
+
+const int MAXSZ = 30; // todo
+int n;
+char farm[MAXSZ][MAXSZ];
+set<string> pals;
+
+void walk(cl i, cl j, const string& p="")
+{
+  if (!farm[i][j]) // out of bounds
+    return;
+  if (i == n && j == n) // at the end
+  {
+    printf((p + "\n").c_str());
+    // check if its a palendrome
+    for (int i=1; i<p.length(); ++i)
+    {
+      if (p[i] != p[p.length()-i]) return;
+    }
+    printf(("palendrome: " + p + "\n").c_str());
+    pals.insert(p);
+    return;
+  }
+  walk(i+1, j, p+string{farm[i][j]});
+  walk(i, j+1, p+string{farm[i][j]});
+}
+
+int main ()
+{
+  if (fin) { fout = fopen("palpath.out", "w+"); } else { fin = stdin; fout = stdout; }
+
+  fscanf(fin, "%d", &n);
+  for (int i=0; i<=n+1; ++i) for (int j=0; j<=n+1; ++j) farm[i][j] = '\0';
+  for (int i=1; i<=n; ++i)
+  {
+    fscanf(fin, "\n");
+    for (int j=1; j<=n; ++j)
+    {
+      fscanf(fin, "%c", &farm[i][j]);
+    }
+  }
+  if (farm[1][1] != farm[n][n]) // dfs only checks for palendromes in the center (since you must start at beginning and end at end)
+  {
+    printf("0\n");
+    return 0;
+  }
+  walk(1, 1);
+
+  fprintf(fout, "%d\n", pals.size());
+
+  return 0;
+}
