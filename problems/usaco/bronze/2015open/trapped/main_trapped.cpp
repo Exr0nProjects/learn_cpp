3d3dda502259d3313cd9eff31ca013931d4b1b9b
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-12-15 09:27:36 -0800

    PLM trapped (2015 open bronze)

---
+++ problems/usaco/bronze/2015open/trapped/main_trapped.cpp
@@ -0,0 +1,114 @@
+/*
+ID: spoytie2
+TASK: trapped
+LANG: C++14
+*/
+
+/*
+ * Problem trapped (usaco/bronze/2015open/trapped)
+ * Created Sun 15 Dec 2019 @ 08:59 (PST)
+ * Accepted [!meta:end!]
+ *
+ */
+
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define cn const int
+#define ca const auto &
+#define ll long long
+#define cl const long long
+#define UN unsigned
+#define ST static
+#define CN const
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+FILE *fin = fopen("trapped.in", "r");
+FILE *fout = nullptr;
+
+const int MAXSZ = 4050; // todo
+struct Bale
+{
+  int pos, siz;
+  bool operator<(const Bale &o) const { return pos < o.pos; }
+} bales[MAXSZ];
+ll n;
+
+bool escape(ll l, ll r)
+{
+  if (l <= 0 || r > n)
+  {
+    printf("escaped! l: %d, r: %d, n: %d\n", l, r, n);
+    return true;
+  }
+  cl dist = bales[r].pos - bales[l].pos;
+  if (dist > bales[r].siz || dist > bales[l].siz)
+  {
+    printf("breakthru! dist=%d, %d-%d\n", dist, l, r);
+    if (dist > bales[r].siz)
+      ++r;
+    if (dist > bales[l].siz)
+      --l;
+    printf("-> %d-%d\n", l, r);
+    return escape(l, r);
+  }
+  return false;
+}
+
+int main()
+{
+  if (fin)
+  {
+    fout = fopen("trapped.out", "w+");
+  }
+  else
+  {
+    fin = stdin;
+    fout = stdout;
+  }
+  fscanf(fin, "%lld", &n);
+  printf("n=%d\n", n);
+  for (int i = 1; i <= n; ++i)
+  {
+    int s, p;
+    fscanf(fin, "%d%d", &s, &p);
+    bales[i].siz = s;
+    bales[i].pos = p;
+  }
+
+  ll ret = 0;
+  for (ll i = 1; i < n; ++i)
+  {
+    printf("\nbale %d: %d @ %d\n", i, bales[i].siz, bales[i].pos);
+
+    printf("%d-%d:\n", i, i + 1);
+    if (!escape(i, i + 1))
+    {
+      ret += bales[i + 1].pos - bales[i].pos;
+    }
+  }
+
+  fprintf(fout, "%lld\n", ret);
+
+  return 0;
+}
