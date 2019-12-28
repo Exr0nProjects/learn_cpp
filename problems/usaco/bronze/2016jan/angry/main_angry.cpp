71069df45fed60c203262dbfeeaf3b3eb8760220
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-12-28 10:35:16 -0800

    PLM u-b16j2 (angry) but it should be bfs not dfs

---
+++ problems/usaco/bronze/2016jan/angry/main_angry.cpp
@@ -0,0 +1,116 @@
+/*
+ID: spoytie2
+TASK: angry
+LANG: C++14
+*/
+
+/*
+ * Problem angry (usaco/bronze/2016jan/angry)
+ * Created Sat 28 Dec 2019 @ 10:15 (PST)
+ * Accepted [!meta:end!]
+ *
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+#include <random>
+#include <set>
+#include <map>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#define cn const int
+#define ca const auto &
+#define ll long long
+#define cl const long long
+#define UN unsigned
+#define ST static
+#define CN const
+#define DL double
+#define LD long double
+#define INF 1047483647
+#define PI 3.14159265358979323846264338
+
+using namespace std;
+FILE *fin = fopen("angry.in", "r");
+FILE *fout = nullptr;
+
+const int MAXSZ = 110; // todo
+int bales[MAXSZ];
+bool vis[MAXSZ];
+int N;
+
+ll simulate(cn i, cn s)
+{
+  for (int j = 0; j < s; ++j)
+    printf("  ");
+  printf("simulate called (%d, %d)\n", bales[i], s);
+  ll ret = 1;
+  for (int p = i - 1; p >= 0 && bales[i] - s <= bales[p]; --p) // left
+  {
+    if (vis[p])
+      continue;
+    vis[p] = true;
+    ret += simulate(p, s + 1);
+  }
+  for (int p = i + 1; p < N && bales[i] + s >= bales[p]; ++p) // right
+  {
+    if (vis[p])
+      continue;
+    vis[p] = true;
+    ret += simulate(p, s + 1);
+  }
+  return ret;
+}
+
+int main()
+{
+  if (fin)
+  {
+    fout = fopen("angry.out", "w+");
+  }
+  else
+  {
+    fin = stdin;
+    fout = stdout;
+  }
+
+  fscanf(fin, "%d", &N);
+  for (int i = 0; i < N; ++i)
+  {
+    fscanf(fin, "%d", &bales[i]);
+  }
+
+  stable_sort(bales, bales + N);
+
+  ll ret = 0;
+  for (int i = 0; i < N; ++i)
+  {
+    for (int j = 0; j < N; ++j)
+      vis[j] = false;
+    vis[i] = true;
+    ret = max(ret, simulate(i, 1));
+  }
+  fprintf(fout, "%lld\n", ret);
+
+  return 0;
+}
+
+/*
+6
+3
+4
+5
+6
+8
+13
+ */
--- problems/usaco/bronze/2016jan/timing_log.md
+++ problems/usaco/bronze/2016jan/timing_log.md
@@ -3,3 +3,4 @@
 9:30 | start | reading problems
 9:44 | have fuzzy idea for #2 and #3 | working on #3
 10:12 | 7/10 #3, missing cases 2, 3, 4. Probably edge cases... | analysing #2 
+10:15 | finish analysing #2 bash | writing #2 brute force
