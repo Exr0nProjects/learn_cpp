e8220e16053bf9d00af9374d1e3faa2d18d106ac
Exr0n <spotyie@gmail.com>
2020-01-17 21:41:16 -0800

    Visit input data is corrupted?? + test new template

--- problems/usaco/silver/2019dec/milkvisits/new_milkvisits.cpp
+++ problems/usaco/silver/2019dec/milkvisits/new_milkvisits.cpp
@@ -1,181 +1,145 @@
 /*
 ID: spoytie2
-TASK: milkvisits
+TASK: [!meta:pid!]
 LANG: C++14
 */
 
 /*
- * Problem milkvisits (usaco/silver/2019dec/milkvisits)
- * Created Fri 17 Jan 2020 @ 19:20 (PST)
- * Accepted [!meta:end!]
+ * Problem [!meta:pid!] [!meta:srcpath!]
+ * Create time: [!meta:beg!]
+ * Accept time: [!meta:end!]
  *
  */
 
 #include <iostream>
 #include <cstdio>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
 #include <utility>
 #include <exception>
 #include <algorithm>
 #include <functional>
-#include <math.h>
-#include <random>
-#include <set>
-#include <map>
-#include <tuple>
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <queue>
-#include <stack>
 
 #define cn const int
-#define ca const auto &
 #define ll long long
 #define cl const long long
-#define UN unsigned
-#define ST static
-#define CN const
-#define DL double
-#define LD long double
-#define INF 1047483647
-#define PI 3.14159265358979323846264338
+#define ca const auto &
+
+#define vi vector<int>
+#define ii pair<int, int>
+#define vii vector<ii>
+
+#define MP make_pair
+#define PB push_back
+#define F first
+#define B second
+#define INF 0x7FFFFFFF
+
+// for macro overloading, see https://stackoverflow.com/questions/11761703/overloading-macro-on-number-of-arguments
+// this set is designed for one indexed collections
+#define FOR_(i,b,e) for (int i=(b); i<(e); ++i)
+#define FOR(i,e) FOR_(i,1,(e)+1)
+#define FORR_(i,b,e) for (int i=(e); i>=(b); --i)
+#define FORR(i,e) FORR_(i,1,e)
+#define SORT(a,n) std::sort((a)+1, (a)+(n)+1)
+#define TRAV(a,x) for (auto& a: x)
+#define SORTV(v) std::sort((v).begin()+1, (v).end())
+
+void setIO();
+
+typedef struct {
+  int f, t, w, n;
+} Edge;
+#define TRAVE(s,e) for (int e=head[s]; e; e=edges[e].n)
+
+const int MX=100010;
+#define __USING_EDGELIST
+void addEdge(cn a, cn b, cn w=1);
+Edge edges[MX*MX];
+int ect=1, head[MX];
 
 using namespace std;
-FILE* fin = fopen("milkvisits.in", "r");
-FILE* fout = nullptr;
+int N, V;
 
+char type[MX];
+int group[MX];
 
-const int MAXSZ = 50010; // todo
-struct Edge
-{
-  int f, t, n;
-};
-Edge edges[2500000010];
-int ect = 1, head[MAXSZ];
-void addEdge(cl a, cl b)
-{
-  edges[ect].f = a;
-  edges[ect].t = b;
-  edges[ect].n = head[a];
-  head[a] = ect;
-  ++ect;
-}
-
-char type[MAXSZ];
-int group[MAXSZ];
-
-void disjointDfs(cn s, cn g)
+void disjointDFS(cn s, cn g)
 {
   if (group[s]) return;
   group[s] = g;
-  for (int e=head[s]; e; e=edges[e].n)
+  TRAVE(s, e)
   {
-//    printf("%d->%d ? %d\n", s, edges[e].t, type[edges[e].t] == type[s]);
     if (type[edges[e].t] == type[s])
     {
-      disjointDfs(edges[e].t, g);
+      disjointDFS(edges[e].t, g);
     }
   }
 }
 
-int N, V;
-
-int main ()
+int main()
 {
   setIO();
-  fscanf(fin, "%d%d\n", &N, &V);
-  for (int i=1; i<=N; ++i)
-  {
-    fscanf(fin, "%c", &type[i]);
-  }
-  for (int i=1; i<N; ++i)
+  scanf("%d%d", &N, &V);
+  scanf("%s", type+1);
+  FOR(i, N)
   {
     int a, b;
-    fscanf(fin, "%d%d", &a, &b);
+    scanf("%d%d", &a, &b);
     addEdge(a, b);
     addEdge(b, a);
   }
   
-//  for (int i=1; i<=N; ++i)
-//  {
-//    printf("%2d ->", i);
-//    for (int e=head[i]; e; e=edges[e].n)
-//    {
-//      printf("%3d", edges[e].t);
-//    }
-//    printf("\n");
-//  }
-
-  // groupings
-  for (int i=1; i<=N; ++i)
+  FOR(i, N)
   {
-    disjointDfs(i, i);
+    disjointDFS(i, i);
   }
   
 //  for (int i=1; i<=N; ++i) printf("%2d (%c): %3d\n", i, type[i], group[i]);
-
-  for (int i=0; i<V; ++i)
+  
+  for (int i=1; i<=V; ++i)
   {
     int a, b;
     char c;
-    fscanf(fin, "%d%d %c", &a, &b, &c);
-    if (group[a] == group[b]) fprintf(fout, "%d", type[a] == c);
-    else fprintf(fout, "1");
+//    scanf("%d%d%c", &a, &b, &c);
+    cin >> a >> b >> c;
+    printf("%d : %d (%c)\n", a, b, c);
+    if (group[a] == group[b]) printf("%d", type[a] == c);
+    else printf("1");
   }
-
-  fprintf(fout, "\n");
 
   return 0;
 }
 
-/*
-3 2
-HGH
-1 2
-1 3
-1 3 G
-1 3 H
-=> 01
- 
-3 2
-HGH
-1 2
-1 3
-1 2 G
-1 2 H
-=> 11
+// boilerplate functions
+void setIO()
+{
+  ios_base::sync_with_stdio(0); cin.tie(0); // fast cin/cout
+  if (fopen("[!meta:pid!].in", "r") != nullptr)
+  {
+    freopen("[!meta:pid!].in", "r", stdin);
+    freopen("[!meta:pid!].out", "w", stdout);
+  }
+}
 
-// larger dataset
-10 2
-HHHHHHHHHH
-1 2
-2 3
-3 4
-5 4
-6 5
-9 8
-7 8
-6 7
-9 10
-3 4 H
-1 10 G
-=> 10
-
-// circle
-7 7
-GHHHGGG
-1 2
-1 3
-1 4
-1 5
-1 6
-1 7
-1 2 H
-1 3 H
-1 4 H
-1 5 H
-1 6 H
-1 7 H
-7 7 G
- => 1110001
- */
+#ifdef __USING_EDGELIST
+void addEdge(cn a, cn b, cn w)
+{
+  edges[ect].f = a;
+  edges[ect].t = b;
+  edges[ect].w = w;
+  edges[ect].n = head[a];
+  head[a] = ect++;
+}
+#endif
