7be0990f92fd9f22b69998cc5e78f39ae1ed0377
Exr0n <spotyie@gmail.com>
2020-01-18 19:49:50 -0800

    prepare for silver2020jan contest tomorrow!

--- problems/create.sh
+++ problems/create.sh
@@ -7,7 +7,7 @@ dateformat="+%a %d %b %Y @ %R (%Z)";
 mkdir -p $1
 
 [[ -s "$1/main_$(basename $1).cpp" ]] ||\
-  cat ./.template_state/template.cpp\
+  cat ./.template_state/defines_test.cpp\
     | sed "s/\[\!meta\:pid\!\]/$(basename $1)/g"\
     | sed "s/\[\!meta\:srcpath\!\]$/(${1//\//\\/})/g"\
     | sed "s/\[\!meta\:beg\!\]$/$(date "$dateformat")/g"\
---
+++ problems/usaco/silver/2020jan/timing_log.md
@@ -0,0 +1,21 @@
+# Contest usaco/silver/2020jan - played Sun 19 Jan 202 (PST)
+
+## Contest Timeline
+
+| Time | Note | Next |
+|----|----|----|
+
+
+## Purify Notes
+
+### Problem 1:
+
+-
+
+### Problem 2:
+
+-
+
+### Problem 3:
+
+-
