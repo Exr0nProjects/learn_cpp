7de19b79d1c84ec0763f2e9d71f1b13862032df0
Exr0n <spotyie@gmail.com>
2020-01-26 15:55:42 -0800

    remove comments from testing template

--- problems/.template_state/defines_test.cpp
+++ problems/.template_state/defines_test.cpp
@@ -88,38 +88,3 @@ void setIO(const string &name)
     freopen((name+".out").c_str(), "w+", stdout);
   }
 }
-
-/*
-Benjamin Qi
-#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
-#define F0R(i,a) FOR(i,0,a)
-#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
-#define R0F(i,a) ROF(i,0,a)
-#define trav(a,x) for (auto& a: x)
- 
-#define pb push_back
-*/
-
-/*
-thecodingwizard
-
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
-#define SORT(vec) sort(vec.begin(), vec.end())
-
-#define INF 1000000000
-#define LL_INF 4500000000000000000
-#define LSOne(S) (S & (-S))
-#define EPS 1e-9
-#define A first
-#define B second
-#define mp make_pair
-#define pb push_back
-#define PI acos(-1.0)
-#define ll long long
-typedef pair<int, int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
-*/
