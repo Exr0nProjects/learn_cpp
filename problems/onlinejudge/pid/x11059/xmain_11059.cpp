31f80411df8de3e972311276da15efe34e2ce1d2
Exr0n <spotyie@gmail.com>
2020-01-26 16:21:46 -0800

    AC onlinejudge/pid/11059 (script)

--- problems/onlinejudge/pid/x11059/xmain_11059.cpp
+++ problems/onlinejudge/pid/x11059/xmain_11059.cpp
@@ -7,7 +7,7 @@ LANG: C++14
 /*
  * Problem 11059 (onlinejudge/pid/11059)
  * Create time: Sun 26 Jan 2020 @ 14:22 (PST)
- * Accept time: [!meta:end!]
+ * Accept time: Sun 26 Jan 2020 @ 16:21 (PST)
  * 
  * This is wrong answer somehow??
  */
@@ -77,12 +77,9 @@ int seq[MX];
 
 int main()
 {
-  setIO();
 
   for (int cs = 1; cin >> N; ++cs)
   {
-    if (cs > 1)
-      printf("\n\n");
 
     FOR(i, N)
     scanf("%d", &seq[i]);
@@ -97,7 +94,7 @@ int main()
         prod *= seq[j];
       }
     }
-    printf("Case #%d: The maximum product is %lld.", cs, ret);
+    printf("Case #%d: The maximum product is %lld.\n\n", cs, ret);
   }
 
   return 0;
@@ -114,38 +111,3 @@ void setIO(const string &name)
     freopen((name + ".out").c_str(), "w+", stdout);
   }
 }
-
-/*
-Benjamin Qi
-#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
-#define F0R(i,a) FOR(i,0,a)
-#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
-#define R0F(i,a) ROF(i,0,a)
-#define trav(a,x) for (auto& a: x)
- 
-#define pb push_back
-*/
-
-/*
-thecodingwizard
-
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
-#define SORT(vec) sort(vec.begin(), vec.end())
-
-#define INF 1000000000
-#define LL_INF 4500000000000000000
-#define LSOne(S) (S & (-S))
-#define EPS 1e-9
-#define A first
-#define B second
-#define mp make_pair
-#define pb push_back
-#define PI acos(-1.0)
-#define ll long long
-typedef pair<int, int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
-*/
