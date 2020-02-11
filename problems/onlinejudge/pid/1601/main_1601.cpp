bd6ca74de2db84e7605d6da720dc0e6d03628841
Exr0n <spotyie@gmail.com>
2020-02-10 19:52:44 -0800

    seems to work, very slow?

--- problems/onlinejudge/pid/1601/main_1601.cpp
+++ problems/onlinejudge/pid/1601/main_1601.cpp
@@ -58,19 +58,29 @@ LANG: C++14
 void setIO(const std::string &name = "1601");
 
 using namespace std;
-const int MX = 16;    
-const int MX_CELLS = 200;         // 3/4 * grid size
-const int dx = {0, 1, 0, -1, 0}; // four directions + noop
-const int dy = {1, 0, -1, 0, 0};
+const int MX = 16;
+const int MX_CELLS = 200;          // 3/4 * grid size
+const int dx[] = {0, 1, 0, -1, 0}; // four directions + noop
+const int dy[] = {1, 0, -1, 0, 0};
 int W, H, N;
 
 // cell: empty positions on the input board
-typedef cell int; // ! one indexed
-int x[MX_CELLS], y[MX_CELLS]; // map cell_id to <w, h>
-cell maze[MX][MX]; // map <w, h> to cell_id
-cell start[3], dest[3], cell_id=0; // starting and dest cells of each ghost
-void setup()
+typedef char cell;                   // ! one indexed
+int x[MX_CELLS], y[MX_CELLS];        // map cell_id to <w, h>
+cell maze[MX][MX];                   // map <w, h> to cell_id
+cell start[3], dest[3], cell_id = 0; // starting and dest cells of each ghost
+bool setup()
 {
+  // reset globals
+  FOR(i, MX_CELLS)
+  x[i] = y[i] = 0;
+  FOR(i, MX)
+  FOR(j, MX) maze[i][j] = 0;
+  FOR(i, 3)
+  start[i] = dest[i] = 0;
+  cell_id = 0;
+
+  // input
   scanf("%d%d%d", &W, &H, &N);
   FOR(h, H)
   {
@@ -85,11 +95,14 @@ void setup()
         x[cell_id] = w;
         y[cell_id] = h;
         maze[w][h] = cell_id;
-        if (isLower(c)) s[c-'a'] = cell_id;
-        if (isUpper(c)) s[c-'A'] = cell_id;
+        if (islower(c))
+          start[c - 'a'] = cell_id;
+        if (isupper(c))
+          dest[c - 'A'] = cell_id;
       }
     }
   }
+  return W != 0 || H != 0 || N != 0;
 }
 
 // state: combo of the locations of three ghosts
@@ -101,10 +114,11 @@ struct state
   state(cell a, cell b, cell c) : a(a), b(b), c(c) {}
   state(const state &src, cn da, cn db, cn dc)
   {
-    a = maze[x[src.a]+dx[da]][y[src.a]+dy[da]];
-    b = maze[x[src.b]+dx[db]][y[src.b]+dy[db]];
-    c = maze[x[src.c]+dx[dc]][y[src.c]+dy[dc]];
+    a = maze[x[src.a] + dx[da]][y[src.a] + dy[da]];
+    b = maze[x[src.b] + dx[db]][y[src.b] + dy[db]];
+    c = maze[x[src.c] + dx[dc]][y[src.c] + dy[dc]];
   }
+  bool operator<(const state &other) const { return (a << 16 | b << 8 | c) < (other.a << 16 | other.b << 8 | other.c); }
 };
 
 vector<state> neighbors(state src)
@@ -115,15 +129,14 @@ vector<state> neighbors(state src)
   };
   vector<state> ret;
   FOR(da, 5)
-  FOR(db, 5) FOR(dc, 5)
+  FOR(db, 5)
+  FOR(dc, 5)
   {
     state next(src, da, db, dc);
     // in a wall
-    if (illegalPos(next.ax, next.ay) || illegalPos(next.bx, next.by) || illegalPos(next.cx, next.cy))
+    if (illegalPos(next.a) || illegalPos(next.b) || illegalPos(next.c))
       continue;
-    if (illegalMove(src.a next.a, src.b, next.b)
-     || illegalMove(src.b, next.b, src.c, next.c)
-     || illegalMove(src.c, next.c, src.a, next.a))
+    if (illegalMove(src.a, next.a, src.b, next.b) || illegalMove(src.b, next.b, src.c, next.c) || illegalMove(src.c, next.c, src.a, next.a))
       continue;
     ret.push_back(next);
   }
@@ -135,54 +148,129 @@ inline bool win(state s)
   return s.a == dest[0] && s.b == dest[1] && s.c == dest[2];
 }
 
-int bfs(state start)
+int bfs(state src)
 {
-  static queue<state> q;
-  static map<state, int> dist;
-  dist[start] = 0;
-  for (q.push(start); !q.empty(); q.pop())
+  queue<state> q;
+  map<state, int> dist;
+  dist[src] = 1;
+  for (q.push(src); !q.empty(); q.pop())
   {
+    //    FOR(i, dist[q.front()]) printf("  "); printf("%d %d %d", q.front().a, q.front().b, q.front().c); printf("\n");
     if (win(q.front()))
-      return dist[q.front()];
+      return dist[q.front()] - 1;
 
     TRAV(n, neighbors(q.front()))
     {
-      if (dist.count(q.front())) continue; // dejavu: already been here before
-      dist[n] = dist[q.front()]+1;
+      //      printf(". %d %d %d: %d\n", n.a, n.b, n.c, dist[n]);
+      if (dist[n] > 0)
+        continue; // dejavu: already been here before
+      dist[n] = dist[q.front()] + 1;
       q.push(n);
     }
   }
   return -1;
 }
 
+void debug()
+{
+  return;
+  printf("\n######################################\n    ");
+  FOR(w, W)
+  printf("%3d", w);
+  FOR(h, H)
+  {
+    printf("\n%3d:", h);
+    FOR(w, W)
+    {
+      if (maze[w][h])
+        printf("%3d", maze[w][h]);
+      else
+        printf("  .");
+    }
+  }
+  printf("\nid: ");
+  FOR_(i, 1, cell_id + 1)
+  printf("%3d", i);
+  printf("\nx:  ");
+  FOR_(i, 1, cell_id + 1)
+  printf("%3d", x[i]);
+  printf("\ny:  ");
+  FOR_(i, 1, cell_id + 1)
+  printf("%3d", y[i]);
+
+  printf("\n");
+}
+
 int main()
 {
   setIO();
-  setup();
-
-  // put additional ghosts in the corner to fill all three
-  if (N <= 1)
+  while (setup())
   {
-    ++cell_id;
-    maze[0][0] = cell_id;
-    x[cell_id] = y[cell_id] = 0;
-    start[2] = dest[2] = cell_id;
-  }
-  if (N <= 2)
-  {
-    ++cell_id;
-    maze[W-1][0] = cell_id;
-    x[cell_id] = W-1;
-    y[cell_id] = 0;
-    start[2] = dest[2] = cell_id;
-  }
+    // put additional ghosts in the corner to fill all three
+    if (N <= 1)
+    {
+      ++cell_id;
+      maze[0][0] = cell_id;
+      x[cell_id] = y[cell_id] = 0;
+      start[2] = dest[2] = cell_id;
+    }
+    if (N <= 2)
+    {
+      ++cell_id;
+      maze[W - 1][0] = cell_id;
+      x[cell_id] = W - 1;
+      y[cell_id] = 0;
+      start[2] = dest[2] = cell_id;
+    }
 
-  // go!
-  state start(s[0], s[1], s[2]);
-  printf("%d\n", bfs(start));
+    debug();
+
+    // go!
+    state src{start[0], start[1], start[2]};
+
+    printf("%d %d %d -> %d %d %d\n", start[0], start[1], start[2], dest[0], dest[1], dest[2]);
+
+    //  find neighbors works...
+    //  TRAV(s, neighbors(src))
+    //  { printf("a %d, b %d, c %d\n", s.a, s.b, s.c); }
+
+    printf("%d\n", bfs(src));
+  }
 
   return 0;
 }
+
+/*
+ 5 5 2
+ #####
+ #A#B#
+ #   #
+ #b#a#
+ #####
+ 16 4 3
+ ################
+ ## ########## ##
+ #    ABCcba    #
+ ################
+ 16 16 3
+ ################
+ ### ##    #   ##
+ ##  #  ##   # c#
+ #  ## ########b#
+ # ##  # #   #  #
+ #  # ##   # # ##
+ ##  a#  # # #  #
+ ### ## #### ## #
+ ##   #   #  #  #
+ #  ##### # ## ##
+ ####   #B# #   #
+ ##  C#   #   ###
+ #  # # ####### #
+ # ######  A##  #
+ #        #    ##
+ ################
+ 0 0 0
+ */
 
 void setIO(const string &name)
 {
