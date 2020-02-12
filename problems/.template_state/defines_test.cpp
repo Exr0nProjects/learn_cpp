e9d972659c941777c0ce040c0c3d228cd33ddc3c
Exr0n <spotyie@gmail.com>
2020-02-11 20:23:34 -0800

    four space indent template

--- problems/.template_state/defines_test.cpp
+++ problems/.template_state/defines_test.cpp
@@ -46,37 +46,34 @@ LANG: C++14
 #define S second
 #define INF 0x7FFFFFFF
 
-#define FOR_(i,b,e) for (int i=(b); i<(e); ++i)
-#define FOR(i,e) FOR_(i,0,(e))
-#define FORR_(i,b,e) for (int i=(e)-1; i>=(b); --i)
-#define FORR(i,e) FORR_(i,0,e)
-#define SORT(a,n) std::sort((a), (a)+(n))
-#define TRAV(a,x) for (auto& a: x)
+#define FOR_(i, b, e) for (int i = (b); i < (e); ++i)
+#define FOR(i, e) FOR_(i, 0, (e))
+#define FORR_(i, b, e) for (int i = (e)-1; i >= (b); --i)
+#define FORR(i, e) FORR_(i, 0, e)
+#define SORT(a, n) std::sort((a), (a) + (n))
+#define TRAV(a, x) for (auto &a : x)
 #define TRAVE(e, s) for (int e = head[s]; e; e = edges[e].n)
 #define SORTV(v) std::sort((v).begin(), (v).end())
 
-void setIO(const std::string &name="[!meta:pid!]");
+void setIO(const std::string &name = "[!meta:pid!]");
 
 using namespace std;
-const int MX=-1;
-
-
+const int MX = -1;
 
 int main()
 {
-  setIO();
+    setIO();
 
-  return 0;
+    return 0;
 }
-
-
 
 void setIO(const string &name)
 {
-  ios_base::sync_with_stdio(0); cin.tie(0); // fast cin/cout
-  if (fopen((name+".in").c_str(), "r") != nullptr)
-  {
-    freopen((name+".in").c_str(), "r", stdin);
-    freopen((name+".out").c_str(), "w+", stdout);
-  }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); // fast cin/cout
+    if (fopen((name + ".in").c_str(), "r") != nullptr)
+    {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w+", stdout);
+    }
 }
