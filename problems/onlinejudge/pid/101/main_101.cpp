cf8a92775f4d79c825cf692f502b444ec1d62ced
Exr0n <spotyie@gmail.com>
2020-02-14 18:58:12 -0800

    oj101 still RE

--- problems/onlinejudge/pid/101/main_101.cpp
+++ problems/onlinejudge/pid/101/main_101.cpp
@@ -69,111 +69,111 @@ int N, src, dst;
 
 int read(int &op)
 {
-  char buf[80];
-  scanf("\n%s", buf);
-  if (buf[0] == 'q')
-  {
-    return 0;
-  }
+    char buf[80];
+    scanf("\n%s", buf);
+    if (buf[0] == 'q')
+    {
+        return 0;
+    }
 
-  op = 0;
-  if (buf[0] == 'm')
-    op |= 2;
-  scanf("%d%s", &src, buf);
-  if (buf[1] == 'n')
-    op |= 1;
-  scanf("%d", &dst);
-  return 1;
+    op = 0;
+    if (buf[0] == 'm')
+        op |= 2;
+    scanf("%d%s", &src, buf);
+    if (buf[1] == 'n')
+        op |= 1;
+    scanf("%d", &dst);
+    return 1;
 }
 
 void output()
 {
-  FOR(i, N)
-  {
-    printf("%d:", i);
-    //    for(; !world[i].empty(); world[i].pop_front())
-    //    {
-    //      printf(" %d", world[i].front());
-    //    }
-    FOR(j, world[i].size())
+    FOR(i, N)
     {
-      printf(" %d", world[i].front());
-      world[i].push_back(world[i].front());
-      world[i].pop_front();
+        printf("%d:", i);
+        //    for(; !world[i].empty(); world[i].pop_front())
+        //    {
+        //      printf(" %d", world[i].front());
+        //    }
+        FOR(j, world[i].size())
+        {
+            printf(" %d", world[i].front());
+            world[i].push_back(world[i].front());
+            world[i].pop_front();
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 }
 
 int main()
 {
-  setIO();
-  scanf("%d", &N);
-  FOR(i, N)
-  world[i].push_back(i);
-  iota(loc, loc + N, 0);
+    setIO();
+    scanf("%d", &N);
+    FOR(i, N)
+    world[i].push_back(i);
+    iota(loc, loc + N, 0);
 
-  //  output();
+    //  output();
 
-  int operation;
-  while (read(operation))
-  {
-    //    printf("(%d) %d -> %d\n", operation, src, dst);
-    if (loc[src] == loc[dst])
-      continue;
-
-    auto &s_pile = world[loc[src]];
-    auto &d_pile = world[loc[dst]];
-
-    if (operation & 2) // move type
+    int operation;
+    while (read(operation))
     {
-      for (; s_pile.back() != src; s_pile.pop_back())
-      {
-        loc[s_pile.back()] = s_pile.back();
-        world[s_pile.back()].push_back(s_pile.back());
-      }
-    }
-    if (operation & 1) // onto type
-    {
-      for (; d_pile.back() != dst; d_pile.pop_back())
-      {
-        loc[d_pile.back()] = d_pile.back();
-        world[d_pile.back()].push_back(d_pile.back());
-      }
-    }
-    // do the final move
-    bool found = 0;
-    for (auto it = s_pile.begin(); it != s_pile.end() && !s_pile.empty();)
-    {
-      //      printf("checking %d found? %d\n", *it, found);
-      if (*it == src)
-        found = true;
-      if (found)
-      {
-        loc[*it] = dst;
-        d_pile.push_back(*it);
-        it = s_pile.erase(it);
-      }
-      else
-      {
-        ++it;
-      }
-    }
-    //    output();
-  }
+        //    printf("(%d) %d -> %d\n", operation, src, dst);
+        if (loc[src] == loc[dst])
+            continue;
 
-  output();
+        auto &s_pile = world[loc[src]];
+        auto &d_pile = world[loc[dst]];
 
-  return 0;
+        if (operation & 2) // move type
+        {
+            for (; s_pile.back() != src; s_pile.pop_back())
+            {
+                loc[s_pile.back()] = s_pile.back();
+                world[s_pile.back()].push_back(s_pile.back());
+            }
+        }
+        if (operation & 1) // onto type
+        {
+            for (; d_pile.back() != dst; d_pile.pop_back())
+            {
+                loc[d_pile.back()] = d_pile.back();
+                world[d_pile.back()].push_back(d_pile.back());
+            }
+        }
+        // do the final move
+        bool found = 0;
+        for (auto it = s_pile.begin(); it != s_pile.end() && !s_pile.empty();)
+        {
+            //      printf("checking %d found? %d\n", *it, found);
+            if (*it == src)
+                found = true;
+            if (found)
+            {
+                loc[*it] = dst;
+                d_pile.push_back(*it);
+                it = s_pile.erase(it);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+        //    output();
+    }
+
+    output();
+
+    return 0;
 }
 
 void setIO(const string &name)
 {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0); // fast cin/cout
-  if (fopen((name + ".in").c_str(), "r") != nullptr)
-  {
-    freopen((name + ".in").c_str(), "r", stdin);
-    freopen((name + ".out").c_str(), "w+", stdout);
-  }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); // fast cin/cout
+    if (fopen((name + ".in").c_str(), "r") != nullptr)
+    {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w+", stdout);
+    }
 }
