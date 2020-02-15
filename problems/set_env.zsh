f783510f33c5ac76576c2e83955626aa45d636f0
Exr0n <spotyie@gmail.com>
2020-02-15 08:09:43 -0800

    run laptop config in set env

--- problems/set_env.zsh
+++ problems/set_env.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 # set command line env for competitive programming
+source $ZSH_CUSTOM/../dontrun_custom/laptop_config.zsh
 
 ENVROOT=$(pwd)
 alias crt='cd $ENVROOT && source ./create.sh'
