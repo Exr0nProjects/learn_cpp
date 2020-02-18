69c6161a9bc26a13930e6a1946cbe75463dba250
Exr0n <spotyie@gmail.com>
2020-02-17 19:05:01 -0800

    WIP STL5 oj12096 (website down)

--- problems/onlinejudge/pid/12096/main_12096.cpp
+++ problems/onlinejudge/pid/12096/main_12096.cpp
@@ -59,10 +59,58 @@ void setIO(const std::string &name = "12096");
 
 using namespace std;
 const int MX = -1;
+int K, N;
+
+typedef int id;
+typedef set<id> Set;
+vector<Set> db;
+map<Set, id> set_id;
+
+int encode(Set s)
+{
+    if (!set_id.count(s))
+    {
+        set_id[s] = db.size();
+        db.push_back(s);
+    }
+    return set_id[s];
+}
+
+// yoinked from answer
+#define ALL(x) x.begin(), x.end()
+#define INS(x) inserter(x,x.end())
 
 int main()
 {
     setIO();
+    scanf("%d", &K);
+    FOR(kase, K)
+    {
+        stack<int> comp;
+        scanf("%d", &N);
+        FOR(i, N)
+        {
+            char cmd;
+            scanf("\n%c%*s", &cmd);
+            if (cmd == 'P') comp.push(encode(Set{}));
+            else if (cmd == 'D') comp.push(comp.top()); // FIX: needs to be else if to propogate the else statement functionality so it doesn't double pop on cmd='P'. found after spending 47 mins debugging vim clipboard integration and 3 minutes in xcode rip
+            else
+            {
+                Set s1 = db[comp.top()];
+                comp.pop();
+                Set s2 = db[comp.top()];
+                comp.pop();
+                Set res;
+                if (cmd == 'U') set_union(ALL(s1), ALL(s2), INS(res)); // apparently inserter creates an output_iterator
+                if (cmd == 'I') set_intersection(ALL(s1), ALL(s2), INS(res));
+                if (cmd == 'A') { res = s2; res.insert(encode(s1)); }
+                comp.push(encode(res));
+            }
+            printf("%d\n", db[comp.top()].size());
+        }
+
+        printf("***\n");
+    }
 
     return 0;
 }
