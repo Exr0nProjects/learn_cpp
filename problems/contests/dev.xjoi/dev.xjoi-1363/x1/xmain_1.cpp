23fd874da9e6e0e0948f5673fa893089469f4f9f
Exr0n <spotyie@gmail.com>
2020-02-19 09:07:04 -0800

    AC contests/dev.xjoi/dev.xjoi-1363/1 (script)

---
+++ problems/contests/dev.xjoi/dev.xjoi-1363/x1/xmain_1.cpp
@@ -0,0 +1,94 @@
+/*
+ID: spoytie2
+TASK: 1
+LANG: C++14
+*/
+
+/*
+ * Problem 1 ([!meta:srcpath!])
+ * Create time: Wed 19 Feb 2020 @ 09:02 (PST)
+ * Accept time: Wed 19 Feb 2020 @ 09:07 (PST)
+ *
+ */
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <list>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define cn const int
+#define ll long long
+#define cl const long long
+#define ca const auto &
+
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define INF 0x7FFFFFFF
+
+#define FOR_(i, b, e) for (int i = (b); i < (e); ++i)
+#define FOR(i, e) FOR_(i, 0, (e))
+#define FORR_(i, b, e) for (int i = (e)-1; i >= (b); --i)
+#define FORR(i, e) FORR_(i, 0, e)
+#define SORT(a, n) std::sort((a), (a) + (n))
+#define TRAV(a, x) for (auto &a : x)
+#define TRAVE(e, s) for (int e = head[s]; e; e = edges[e].n)
+#define SORTV(v) std::sort((v).begin(), (v).end())
+
+void setIO(const std::string &name = "1");
+
+using namespace std;
+const int MX = 1000010;
+int N, V;
+vi nums;
+
+int main()
+{
+    //setIO();
+    scanf("%d%d", &N, &V);
+    FOR(i, N)
+    {
+        int n;
+        scanf("%d", &n);
+        nums.push_back(n);
+    }
+
+    auto found = lower_bound(nums.begin(), nums.end(), V);
+    if (found != nums.end() && *found == V) printf("%d\n", found-nums.begin()+1);
+    else printf("0\n");
+
+    return 0;
+}
+
+void setIO(const string &name)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); // fast cin/cout
+    if (fopen((name + ".in").c_str(), "r") != nullptr)
+    {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w+", stdout);
+    }
+}
