42b7a4f1c16df795f053d91be78ebf00587d7cf0
Exr0n <spotyie@gmail.com>
2020-03-08 14:15:46 -0700

    download the problems from hpi2020novice

---
+++ problems/contests/hpi2020/download_problems.zsh
@@ -0,0 +1,4 @@
+for l in {A..L}; do                                                                                                                                                          13:55:18
+wget --load-cookies cookies.txt "https://codeforces.com/group/VLcY3ROcNO/contest/270787/problem/$l/" -O "${l:l}/prob.pdf"
+done
+
