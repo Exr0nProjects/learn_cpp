a543f0f061925ed7a479f3324a4a435952739b3f
Exr0n <spotyie@gmail.com>
2020-03-09 18:35:09 -0700

    wip set_env crt also open the file

--- problems/set_env.zsh
+++ problems/set_env.zsh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 
-# set command line env for competitive programming
-source $ZSH_CUSTOM/../dontrun_custom/laptop_config.zsh
-
 ENVROOT=$(pwd)
+# alias crt='cd $ENVROOT && source ./create.sh && vim main*'
 alias crt='cd $ENVROOT && source ./create.sh'
 alias ans='g++ answer.cpp -o answer && ./answer && cat *.out'
 
