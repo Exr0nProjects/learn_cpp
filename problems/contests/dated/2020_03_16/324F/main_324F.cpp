8c7679f5d070c8bcd68fc68c6e2655cfdaefd586
Exr0n <spotyie@gmail.com>
2020-03-16 15:19:53 -0700

    its so scuffed

--- problems/contests/dated/2020_03_16/324F/main_324F.cpp
+++ problems/contests/dated/2020_03_16/324F/main_324F.cpp
@@ -60,47 +60,69 @@ void setIO(const std::string &name = "324F");
 
 using namespace std;
 const int MX = 211;
-ll N, A, B, total_area=0, height[MX], contact[MX]; // contact[i] = contact between board i and board i-1
+ll N, A, B, height[MX];
+ll contact[MX]; // contact[i] = contact between board i and board i-1
+ll total[MX]; // prefix sum over area
 pair<ll, bool> tab[MX][MX]; // (pair<unattractiveness, whether this board is red>)tab[i][j] rep best looks by board i having used j red paint
+
+pair<ll, bool> best(int i, int r)
+{
+    if (tab[i][r].F) return tab[i][r];
+    pair<ll, bool> ret = {(ll)1<<60, true};
+    if (i < 1 || r < 0) return ret;
+
+    if (r <= A && total[i] - r <= B) ret = min(ret,
+            make_pair(best(i-1, r).F + contact[i]*best(i-1, r).S, false ));
+
+    if (r-height[i] <= A && total[i] - r+height[i] <= B) ret = min(ret,
+            make_pair(best(i-height[i], r).F + contact[i]*best(i-1, r).S, false ));
+
+    tab[i][r] = ret;
+    return ret;
+}
 
 int main()
 {
     FOR(i, MX) FOR(j, MX) tab[i][j].F = 1<<30;
     // setIO();
     scanf("%lld%lld%lld", &N, &A, &B);
-    FOR(i, N) scanf("%lld", &height[i]);
+    FOR(i, N) { scanf("%lld", &height[i]); total[i] = total[i-1] + height[i]; }
     FOR(i, N) contact[i+1] = min(height[i+1], height[i]);
 
-    tab[0][0] = {0, 0};
-    FOR_(i, 1, N+1)
-    {
-        total_area += height[i-1];
-        FOR_(j, 0, total_area)
-        {
-            // tab[i+1][j  ] = { tab[i][j].F + contact[i]*( tab[i][j].S), 0};
-            // tab[i+1][j+height[i]] = { tab[i][j].F + contact[i]*(!tab[i][j].S), 1};
-            if (j <= A && total_area-j <= B)
-                tab[i][j] = min(tab[i][j],
-                        make_pair( tab[i-1][j  ].F
-                                 + contact[i]*(bool)(tab[i-1][j  ].S), false ));
+    // tab[0][0] = {0, 0};
+    // FOR_(i, 1, N+1)
+    // {
+    //     FOR_(j, 0, total_area)
+    //     {
+    //         // tab[i+1][j  ] = { tab[i][j].F + contact[i]*( tab[i][j].S), 0};
+    //         // tab[i+1][j+height[i]] = { tab[i][j].F + contact[i]*(!tab[i][j].S), 1};
+    //         if (j <= A && total_area-j <= B)
+    //             tab[i][j] = min(tab[i][j],
+    //                     make_pair( tab[i-1][j  ].F
+    //                              + contact[i]*(bool)(tab[i-1][j  ].S), false ));
 
-            if (j-1 <= A && total_area-j+1 <= B)
-                tab[i][j] = min(tab[i][j],
-                        make_pair( tab[i-1][j-height[i]].F
-                                 + contact[i]*!(bool)(tab[i-1][j-height[i]].S), true ));
-        }
-    }
+    //         if (j-1 <= A && total_area-j+1 <= B)
+    //             tab[i][j] = min(tab[i][j],
+    //                     make_pair( tab[i-1][j-height[i]].F
+    //                              + contact[i]*!(bool)(tab[i-1][j-height[i]].S), true ));
+    //     }
+    // }
 
-    FOR(i, N+5) { FOR(j, total_area+10) if (tab[i][j].F < 1<<29) printf("%3lld", tab[i][j].F); else printf("  ."); printf("\n"); }
+    // FOR(i, N+5) { FOR(j, total_area+10) if (tab[i][j].F < 1<<29) printf("%3lld", tab[i][j].F); else printf("  ."); printf("\n"); }
 
-    ll ret=(ll)1<<60;
-    FOR(j, total_area+1)
-        if (j <= A && total_area - j <= B)
-        {
-            printf("j: %d, tot-j: %d\n", j, total_area-j);
-            ret = min(ret, tab[N][j].F);
-        }
-    printf("%lld\n", ret);
+    // ll ret=(ll)1<<60;
+    // FOR(j, total_area+1)
+    //     if (j <= A && total[N-1] - j <= B)
+    //     {
+    //         printf("j: %d, tot-j: %d\n", j, total[N-1]-j);
+    //         ret = min(ret, tab[N][j].F);
+    //     }
+
+    pair<ll, bool> ret = {(ll) 1<<60, true};
+    FOR(i, N) ret = min(ret, best(N, total[i]));
+    printf("%lld\n", ret.F);
+
+    FOR(i, N+5) { FOR(j, total[N-1]+10) if (tab[i][j].F < 1<<29) printf("%3lld", tab[i][j].F); else printf("  ."); printf("\n"); }
 
     return 0;
 }
