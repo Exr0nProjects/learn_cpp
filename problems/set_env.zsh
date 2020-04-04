5c42ac7f299188a5782700b8ac2c84efa55c20a5
Exr0n <spotyie@gmail.com>
2020-04-04 12:23:33 -0700

    set_env exports IDEMODE env var

--- problems/set_env.zsh
+++ problems/set_env.zsh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
 ENVROOT=$(pwd)
-# alias crt='cd $ENVROOT && source ./create.sh && vim main*'
+tmux setenv IDEMODE 0
+export IDEMODE=0
 alias crt='cd $ENVROOT && source ./create.sh'
 alias ans='g++ answer.cpp -o answer && ./answer && cat *.out'
 
