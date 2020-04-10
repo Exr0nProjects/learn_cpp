3ac35a62fb4231c45f3fe088af46d7a63a9b1e99
Exr0n <spotyie@gmail.com>
2020-04-09 17:55:21 -0700

    check alias to compare with accepted code
    write a `gen.py`, compile a `./answer` binary, and run `check`

--- problems/set_env.zsh
+++ problems/set_env.zsh
@@ -5,4 +5,5 @@ tmux setenv IDEMODE 0
 export IDEMODE=0
 alias crt='cd $ENVROOT && source ./create.sh'
 alias ans='g++ answer.cpp -o answer && ./answer && cat *.out'
+alias check='py gen.py | tee >(./auto > a.diff) >(./answer > b.diff) && [[ ! -z "$(diff --brief *.diff)" ]] && v -d *.diff'
 
