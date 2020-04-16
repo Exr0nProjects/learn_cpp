854afb52cac4b3f3a412b200599c0d7b93db75ff
Exr0n <spotyie@gmail.com>
2020-04-15 17:03:09 -0700

    AC xjoi.net/3657re1 (script)

---
+++ problems/xjoi.net/x3657re1/main_3657re1.cpp
@@ -0,0 +1,90 @@
+/*
+TASK: 3657re1
+LANG: C++14
+*/
+
+/*
+ * Problem 3657re1 (xjoi.net/3657re1)
+ * Create time: Wed 15 Apr 2020 @ 16:58 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <iterator>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+#define ca const auto &
+
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define g(t, i) get<i>(t)
+#define mt make_tuple
+
+#define FOR_(i, b, e) for (long long i = (b); i < (e); ++i)
+#define FOR(i, e) FOR_(i, 0, (e))
+#define FORR_(i, b, e) for (long long i = (e)-1; i >= (b); --i)
+#define FORR(i, e) FORR_(i, 0, e)
+#define TRAV(a, x) for (auto &a : x)
+
+void setIO(const std::string &name = "3657re1");
+
+using namespace std;
+const int MX = 101;
+int N, tri[MX][MX];
+
+int main()
+{
+    scanf("%d", &N);
+    FOR(i, N)
+	FOR(j, i+1)
+	    scanf("%d", &tri[i][j]);
+
+    FORR(i, N)
+	FOR(j, i+1)
+	    tri[i][j] += max(tri[i+1][j], tri[i+1][j+1]);
+    
+    printf("%d\n", tri[0][0]);
+
+    return 0;
+}
+
+void setIO(const string &name)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); // fast cin/cout
+    if (fopen((name + ".in").c_str(), "r") != nullptr)
+    {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w+", stdout);
+    }
+}
