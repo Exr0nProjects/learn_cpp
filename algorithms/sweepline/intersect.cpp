9fa2dc5c873801b461fd1634ef9fcb715575405f
Exr0n <spotyie@gmail.com>
2020-04-16 11:20:49 -0700

    segfaulttttt

--- algorithms/sweepline/intersect.cpp
+++ algorithms/sweepline/intersect.cpp
@@ -15,7 +15,7 @@ typedef pair<dl, dl> Point; // <x, y>
 typedef pair<Point, Point> Seg; // <p1, p2>; assert(p1 <= p2)
 int N;
 
-typedef pair<pair<dl, int>, pair<int, int> > Event; // <<pos x, type {1 = start line, 0 = intersect/swap, -1 = end line}>, <line_id, other_id/unused>>
+typedef pair<pair<dl, int>, pair<int, int> > Event; // <<pos x, type {0 = start line, 1 = intersect/swap, 2 = end line}>, <line_id, other_id/unused>>
 
 deque<Seg> lines;
 priority_queue<Event, deque<Event>, greater<Event> > events;
@@ -38,7 +38,7 @@ bool setcmp(int lhs, int rhs)
 
 pair<bool, Point> intersect(Seg s1, Seg s2)
 {
-    printf("intersect ((%lf %lf) (%lf %lf)) and ((%lf %lf) (%lf %lf))\n", s1.first.first, s1.first.second, s1.second.first, s1.second.second, s2.first.first, s2.first.second, s2.second.first, s2.second.second);
+    // printf("intersect ((%lf %lf) (%lf %lf)) and ((%lf %lf) (%lf %lf))\n", s1.first.first, s1.first.second, s1.second.first, s1.second.second, s2.first.first, s2.first.second, s2.second.first, s2.second.second);
     // https://www.desmos.com/calculator/txz1ndtoot
     // segments have left point first
     if (s1.first > s1.second) swap(s1.first, s1.second);
@@ -51,14 +51,14 @@ pair<bool, Point> intersect(Seg s1, Seg s2)
     dl m1 = (s1.first.second - s1.second.second) / (s1.first.first - s1.second.first);
     dl m2 = (s2.first.second - s2.second.second) / (s2.first.first - s2.second.first);
 
-    printf("m1 %lf, m2 %lf\n", m1, m2);
+    // printf("m1 %lf, m2 %lf\n", m1, m2);
 
     assert(abs(m2-m1) > 0.0000000001); // disallow parallel lines: slope difference > one billionth
 
     dl intersect_x = (s2.first.second - s1.first.second + m1 * s1.first.first - m2 * s2.first.first) / (m1 - m2);
     dl intersect_y = m1*(intersect_x - s1.first.first) + s1.first.second;
 
-    printf("intersect x %lf y %lf\n", intersect_x, intersect_y);
+    // printf("intersect x %lf y %lf\n", intersect_x, intersect_y);
 
     if (s1.first.first <= intersect_x && intersect_x <= s1.second.first
      && s2.first.first <= intersect_x && intersect_x <= s2.second.first)
@@ -104,13 +104,17 @@ void checkNeighboors(const set<int, function<bool(int, int)> >::iterator &it)
 {
     auto intersectPrev = intersect(lines[*it], lines[*prev(it)]);
     if (intersectPrev.first && intersectPrev.second.first > sweepx)
-	events.push(mp(mp(intersectPrev.second.first, 0), mp(*it, *prev(it))));
+	events.push(mp(mp(intersectPrev.second.first, 1), mp(*it, *prev(it))));
 
     auto intersectNext = intersect(lines[*it], lines[*next(it)]);
     if (intersectNext.first && intersectNext.second.first > sweepx)
-	events.push(mp(mp(intersectNext.second.first, 0), mp(*it, *next(it))));
+	events.push(mp(mp(intersectNext.second.first, 1), mp(*it, *next(it))));
 }
 
+void printEvent(const Event &ev)
+{
+    printf("    %d event at %lf, payload %d %d\n", ev.first.second, ev.first.first, ev.second.first, ev.second.second);
+}
 
 int main()
 {
@@ -118,16 +122,11 @@ int main()
      * testing plan:
      * 1. [x] test intersection
      * 2. [ ] test swapping of stuff in the set
+		2
+		0 0 5 0
+		2 2 2 -2
      * 3. [ ] test events---do events show up in the right place?
      */
-
-    while (true)
-    {
-	int a, b, c, d, e, f, g, h;
-	scanf("%d%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g, &h);
-	auto loc = intersect(mp(mp(a, b), mp(c, d)), mp(mp(e, f), mp(g, h)));
-	printf("%d: (%lf %lf)\n", loc.first, loc.second.first, loc.second.second);
-    }
 
     printf("Please, no parallel or concurrent lines!\n");
     scanf("%d", &N);
@@ -140,25 +139,27 @@ int main()
 	    swap(x1, x2);
 	    swap(y1, y2);
 	}
-	events.emplace(make_pair(x1, 1), mp(i, 0));
-	events.emplace(make_pair(x2, -1), mp(i, 0));
+	events.emplace(make_pair(x1, 0), mp(i, 0));
+	events.emplace(make_pair(x2, 2), mp(i, 0));
 	lines.emplace_back(Point(x1, y1), Point(x2, y2));
     }
     
     // # of events = 2*N + # of intersections
     while (!events.empty())
     {
+	printEvent(events.top());
+
 	Event ev = events.top();
 	events.pop();
 
 	sweepx = ev.first.first;
 	
-	if (ev.first.second == 1)	// start of a line
+	if (ev.first.second == 0)	// start of a line
 	{
 	    auto it = active.insert(ev.second.first).first;
 	    checkNeighboors(it);
 	}
-	else if (ev.first.second == 0)	// intersection
+	else if (ev.first.second == 1)	// intersection
 	{
 	    set<int, function<bool(int, int)> >::iterator left = active.find(ev.second.first);
 	    set<int, function<bool(int, int)> >::iterator right = active.find(ev.second.second);
@@ -168,7 +169,7 @@ int main()
 	    checkNeighboors(left);
 	    checkNeighboors(right);
 	}
-	else if (ev.first.second == 1) // end of line
+	else if (ev.first.second == 2) // end of line
 	{
 	    active.erase(ev.second.first);
 	}
