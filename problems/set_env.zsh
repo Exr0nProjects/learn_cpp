451122bdcc5f4c20994d3183caa6aad2f8b7b38b
Exr0n <spotyie@gmail.com>
2020-04-17 13:10:54 -0700

    ans alias: compile with std=c++11

--- problems/set_env.zsh
+++ problems/set_env.zsh
@@ -6,6 +6,6 @@ tmux setenv ENVROOT $ENVROOT
 tmux setenv IDEMODE 0
 export IDEMODE=0
 alias crt='cd $ENVROOT && source ./create.sh'
-alias ans='g++ answer*.cpp -o answer && ./answer && cat *.out'
+alias ans='g++ -std=c++11 answer*.cpp -o answer && ./answer && cat *.out'
 alias check='py gen.py | tee >(./auto > a.diff) >(./answer > b.diff) && [[ ! -z "$(diff --brief *.diff)" ]] && v -d *.diff'
 
