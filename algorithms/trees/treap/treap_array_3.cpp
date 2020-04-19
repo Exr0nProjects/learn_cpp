f3084d6760d9cbc88df8c153b56fa3167a75212a
Exr0n <spotyie@gmail.com>
2020-04-19 16:22:57 -0700

    UNTESTED treap array 3

---
+++ algorithms/trees/treap/treap_array_3.cpp
@@ -0,0 +1,81 @@
+// UNTESTED
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int MX = 10111;
+int parent[MX], left[MX], right[MX], weight[MX], value[MX];
+
+int rotate_right(int node)
+{
+    assert(left[node]);
+    int then = left[node];
+    // update center node
+    parent[right[then]] = node;
+    left[node] = right[then];
+    // update then
+    if (node == left[parent[node]])
+	left[parent[node]] = then;
+    else
+	right[parent[node]] = then;
+    parent[then] = parent[node];
+    // update left child
+    right[then] = node;
+    parent[node] = then;
+    
+}
+
+int rotate_left(int node)
+{
+    assert(left[node]);
+    int then = left[node];
+    // center
+    parent[right[then]] = node;
+    left[node] = right[then];
+    // then
+    if (left[parent[node]] == node)
+	left[parent[node]] = then;
+    else
+	right[parent[node]] = then;
+    parent[then] = parent[node];
+    // root
+    parent[node] = then;
+    right[then] = node;
+
+    return then;
+}
+
+void heapify_up(int node)
+{
+    if (!parent[node])
+	return;
+    if (weight[parent[node]] < weight[node])
+	if (left[parent[node]] == node)
+	    rotate_left(parent[node])
+	else
+	    rotate_right(parent[node])
+    heapify_up(node);
+}
+
+void heapify_down(int node)
+{
+    if (!left[node] && !right[node])
+	return;
+    int then=0;
+    if (left[node] && right[node])
+	if (weight[left[node]] < weight[right[node]])
+	    then = 1; 	// right
+	else
+	    then = -1;	// left
+    else if (left[node] && weight[node] < weight[left[node]])
+	then = -1;
+    else if (right[node] && weight[node] < weight[right[node]])
+	then = 1;
+
+    if (then > 0)
+	rotate_left(node);
+    else if (then < 0)
+	rotate_right(node);
+    heapify_down(node);
+}
