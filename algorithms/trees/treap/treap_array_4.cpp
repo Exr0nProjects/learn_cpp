dab5ef97f42749a65d5892762cb6cf917a1b73e2
Exr0n <spotyie@gmail.com>
2020-04-27 19:10:14 -0700

    plm treap array 4

--- algorithms/trees/treap/treap_array_4.cpp
+++ algorithms/trees/treap/treap_array_4.cpp
@@ -1,8 +1,9 @@
 // treap array rewrite, hopefully clean (27 Apr 2020)
 // does not reuse memory after deletion... use an alloc pq for that
 
-#include <iostream>	// io
-#include <cassert> 	// assert
+#include <algorithm>	// swap
+#include <iostream>		// io
+#include <cassert> 		// assert
 
 const int MX = 100111;
 
@@ -21,14 +22,14 @@ inline int step(int cur, int val)
 //   /   \	    --->      /   \
 //  A     B	   	right    B     C
 
-int rot(int cur, bool dir)
+int rot(int &cur, bool dir)
 {
 	assert(child[cur][dir]);						// don't rotate an empty node into the tree
 	// TODO: how to update pointer from parent?   
 	int thn = child[cur][dir];						// then, aka new 'cur'
 	child[cur][dir] = child[thn][1-dir];			// update pointer to center child
 	child[thn][dir] = cur;							// update pointer to cur
-	//cur = thn;									// update references to cur, since other code expects to be in same position on the tree
+	swap(cur, thn);									// update references, since other code expects to be in same position on the tree
 	return thn;										// return new 'cur'
 }
 
@@ -40,7 +41,7 @@ int parent(int val, int cur=root)
 	return cur;										// else, this is the "would be parent"
 }
 
-int insert(int val, int cur=root)
+int insert(int val, int &cur=root)
 {	// recursively insert a node, then heapify up on the way back
 	if (value[cur] == val)							// if already exists
 		return cur;									// return
@@ -67,17 +68,20 @@ int has(int val)
 
 int erase(int val)
 {
-	int pre, cur = has(val);						// find the node
-	if (!cur) return;								// if it doesn't exist, then we're done!
+	int big, &pre = parent(val);					// get would be parent
+	int &cur = child[pre][cmp(value[pre], val)];	// and child if it exists
+	if (!cur) return;								// if child doesn't exist, we're done!
 	while (child[cur][0] && child[cur][1])			// both children exist
 	{
-		int l = weight[child[cur][0]];				// get weight of left
-		int r = weight[child[cur][1]];				// get weight of right
-		rot(cur, l > r);							// rotate away from bigger (so bigger ends on top)
+		big = weight[child[cur][0]]					// direction is whether
+			< weight[child[cur][1]];				// weight left < weight right
+		pre = cur;									// remember parent of final cur
+		cur = rot(cur, 1-big);						// rotate away from bigger (so bigger ends on top), reassign cur to its new loc
 	}
-	child[
+	child[pre][big]= child[cur][0] | child[cur][1];	// only one child, just bitwise or to get its pos
 	// delete the node
-	child[cur][0] = child[cur
 	value[cur] = 0;
 	weight[cur] = 0;
+	child[cur][0] = child[cur][1] = 0;
+}
 
