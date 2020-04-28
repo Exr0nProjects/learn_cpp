bb8d15bea95fbb5569e982e6afba0be9be5dcc54
Exr0n <spotyie@gmail.com>
2020-04-27 17:06:54 -0700

    pause treap array 4, work on lopsided heap

--- algorithms/trees/treap/treap_array_4.cpp
+++ algorithms/trees/treap/treap_array_4.cpp
@@ -1,4 +1,5 @@
 // treap array rewrite, hopefully clean (27 Apr 2020)
+// does not reuse memory after deletion... use an alloc pq for that
 
 #include <iostream>	// io
 #include <cassert> 	// assert
@@ -22,60 +23,47 @@ inline int step(int cur, int val)
 
 int rot(int &cur, bool dir)
 {
-    assert(child[cur][dir]);
-    // TODO: how to update pointer from parent?   
-    int thn = child[cur][dir];
-    child[cur][dir] = child[thn][1-dir];
-    child[thn][dir] = cur;
-    cur = thn;
-}
-
-int insert(int val, bool bad)
-{
-    if (root)						// if the tree exists
-    {	// TODO: rewrite this block
-	int cur=root;
-	while ( child[cur][cmp(value[cur], val)] )	// while the next traversal exists
-	    cur=child[cur][cmp(value[cur], val)];	// go there
-	// construct node
-	child[cur][cmp(value[cur], val)] = alloc;	// set the child of the parent
-    }
-    value[alloc] = val;					// set value of new node
-    weight[alloc] = rand()%(1<<30);			// random weight of new node
+	assert(child[cur][dir]);						// don't rotate an empty node into the tree
+	// TODO: how to update pointer from parent?   
+	int thn = child[cur][dir];						// then, aka new 'cur'
+	child[cur][dir] = child[thn][1-dir];			// update pointer to center child
+	child[thn][dir] = cur;							// update pointer to cur
+	cur = thn;										// update references to cur, since other code expects to be in same position on the tree
+	return thn;										// return new 'cur'
 }
 
 int insert(int val, int cur=root)
-{
-    if (value[cur] == val)
-	return cur;
-    const int dir = cmp(value[cur], val);
-    if (child[cur][dir])
-    {
-	int it = insert(val, child[cur][dir]);
-	if (weight[cur] < weight[child[cur][dir]])
-	    rot(cur, 1-dir);
-	return it;
-    }
-    // insert here
-    value[alloc] = val;
-    weight[alloc] = rand() % (MX*1000); 		// 0.1% chance of weight collision
-    return alloc++;
+{	// recursively insert a node, then heapify up on the way back
+	if (value[cur] == val)							// if already exists
+		return cur;									// return
+	const int dir = cmp(value[cur], val);			// direction to branch at this node
+	if (child[cur][dir])							// if that direction isn't a leaf
+	{
+		int it = insert(val, child[cur][dir]);		// recursively insert
+		if (weight[cur] < weight[child[cur][dir]])	// if heap property broken
+			rot(cur, 1-dir);						// fix it by rotating branch with new node to top
+		return it;									// return insertion position
+	}
+	// insert here
+	value[alloc] = val;								// set value
+	weight[alloc] = rand() % (MX*1000); 			// 0.1% chance of weight collision
+	return alloc++;									// return insertion position, increase size counter
 }
 
 
 int has(int val)
 {
-    int cur=root;
-    while (cur)
-    {
-	if (value[cur] == val) return cur;
-	cur=child[cur][cmp(value[cur], val)];
-    }
-    return false;
+	int cur = root;									// start at the root
+	while (cur)										// while at a valid node
+	{
+		if (value[cur] == val) return cur;			// if we found it, return position
+		cur = child[cur][cmp(value[cur], val)];		// else, traverse the tree in the direction based on value
+	}
+	return false;									// no longer at valid node, val must not exist
 }
 
 int erase(int val)
 {
-    int it = has(val);
-    if (!it) return;
-
+	int cur = has(val);								// find the node
+	if (!cur) return;								// if it doesn't exist, then we're done!
+	if (child[cur][0] && child[cur][1])				// both children exist
