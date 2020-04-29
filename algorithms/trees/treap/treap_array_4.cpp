e0b4de4237670c843b42ae8c42e631f9a5c10b12
Exr0n <spotyie@gmail.com>
2020-04-28 18:15:28 -0700

    debugging..

--- algorithms/trees/treap/treap_array_4.cpp
+++ algorithms/trees/treap/treap_array_4.cpp
@@ -5,7 +5,7 @@
 #include <iostream>		// io
 #include <cassert> 		// assert
 
-const int MX = 100111;
+const int MX = 111;
 
 int value[MX], weight[MX], child[MX][2], root=0, alloc=1;
 
@@ -17,17 +17,19 @@ int rot(int &cur, bool dir)
 {
 //	        R	           R	
 //	        |	           |	
-//	       cur	  left    thn	
+//	       cur	    1     thn	
 //	      /   \   <---   /   \
 //	    thn    C        A	  cur	
 //	   /   \	  --->      /   \
-//	  A     B	  right    B     C
+//	  A     B	   0       B     C
 //
-	assert(child[cur][dir]);						// don't rotate an empty node into the tree
+	printf("rotating %d %d\n", cur, dir);
+	if (!child[cur][dir]) printf("ASSERTION FAILED!\n");
+	if (!child[cur][dir]) return cur;				// don't rotate an empty node into the tree
 	// TODO: how to update pointer from parent?   
 	int thn = child[cur][dir];						// then, aka new 'cur'
 	child[cur][dir] = child[thn][1-dir];			// update pointer to center child
-	child[thn][dir] = cur;							// update pointer to cur
+	child[thn][1-dir] = cur;						// update pointer to cur
 	std::swap(cur, thn);							// update references, since other code expects to be in same position on the tree
 	return thn;										// return new 'cur'
 }
@@ -44,21 +46,30 @@ int insert(int val, int &cur=root)
 {	// recursively insert a node, then heapify up on the way back
 	if (value[cur] == val)							// if already exists
 		return cur;									// return
-	const int dir = cmp(value[cur], val);			// direction to branch at this node
+	printf("inserting %d from %d\n", val, cur);
 	if (step(cur, val))								// if that direction isn't a leaf
 	{	// TODO: write with parent, maybe?
-		int it = insert(val, child[cur][dir]);		// recursively insert
-		if (weight[cur] < weight[child[cur][dir]])	// if heap property broken
-			rot(cur, 1-dir);						// fix it by rotating branch with new node to top
+		int it = insert(val, step(cur, val));		// recursively insert
+		if (weight[cur] < weight[step(cur, val)])	// if heap property broken
+			rot(cur, cmp(value[cur], val));			// fix it by rotating branch with new node to top
 		return it;									// return insertion position
 	}
 	// insert here
 	value[alloc] = val;								// set value
 	weight[alloc] = rand() % (MX*1000); 			// 0.1% chance of weight collision
+	printf("weight: %d\n", weight[alloc]);
+	step(cur, val) = alloc;							// set pointer from parent to child
+	if (!root) root = alloc;						// set root if this is the first node
 	return alloc++;									// return insertion position, increase size counter
 }
 
-int has(int val) { return step(parent(val), val); }	// return whether child exists on wouldbe parent
+int has(int val)
+{ 
+	return step(parent(val), val) || !(				// either wouldbe parent has correct child
+		cmp(value[root], val) ||					// or value is root (reflexive compare)
+		cmp(val, value[root])
+	);
+}
 
 void erase(int val)
 {
@@ -82,9 +93,9 @@ void erase(int val)
 void dump(int cur=root, int lay=1)
 {
 	if (!cur) return;
-	for (int i=0; i<lay; ++i) printf("|   "); printf("(%d | %d) at %d\n", value[cur], weight[cur], cur);
-	dump(child[cur][0], lay+1);
 	dump(child[cur][1], lay+1);
+	for (int i=1; i<lay; ++i) printf("    "); printf("|--- %d - %d at %d\n", value[cur], weight[cur], cur);
+	dump(child[cur][0], lay+1);
 }
 
 int main()
@@ -110,7 +121,7 @@ int main()
 		else if (c == 'q')
 		{
 			scanf("%d", &d);
-			printf("found %d at %d\n", d, has(d));
+			printf("%d\n", (bool)has(d));
 		}
 		else if (c == 'x')
 		{
