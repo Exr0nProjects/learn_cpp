81ed3171df46ff3be0f3c37fe027ade5f2886e77
Exr0n <spotyie@gmail.com>
2020-05-10 11:52:05 -0700

    scuffed but working traversal

--- algorithms/trees/treap/treap_full_1.cpp
+++ algorithms/trees/treap/treap_full_1.cpp
@@ -1,6 +1,9 @@
 // 10 May 2020
 
 #include <iostream>
+#include <utility>
+
+using namespace std;
 
 struct Node
 {
@@ -12,8 +15,24 @@ struct Node
 
 inline int size(Node *cur)
 {
-	return cur->c[0] ? cur->c[0]->size : 0
-		+  cur->c[1] ? cur->c[1]->size : 0
+	return (cur->c[0] ? cur->c[0]->size : 0)
+		+  (cur->c[1] ? cur->c[1]->size : 0);
+}
+
+pair<Node*, Node*> flatten(Node* cur) // pointers to left and right extremes of subtree
+{
+	if (!cur) return make_pair(nullptr, nullptr);
+	pair<Node*, Node*> left, right;
+	left  = cur->c[0] ? flatten(cur->c[0]) : pair<Node*, Node*>(cur, nullptr);
+	right = cur->c[1] ? flatten(cur->c[1]) : pair<Node*, Node*>(nullptr, cur);
+
+	cur->prev = left.second;
+	if (left.second) left.second->next = cur;
+
+	cur->next = right.first;
+	if (right.first) right.first->prev = cur;
+	
+	return make_pair(left.first, right.second);
 }
 
 void rotate(Node *&cur, bool dir)
@@ -72,6 +91,16 @@ int main()
 		char c=0;
 		while (c < 'a' || c > 'z') scanf("%c", &c);
 		if (c == 'x') break;
+		if (c == 't')
+		{
+			pair<Node*, Node*> bounds = flatten(root);
+			printf("got bounds %d thru %d\n", bounds.first->d, bounds.second->d);
+			for (Node* it = bounds.first; it!=bounds.second; it = it->next)
+				printf("%d ", it->d);
+			if (bounds.second)
+				printf("%d\n", bounds.second->d);
+			continue;
+		}
 
 		int d;
 		scanf("%d", &d);
