3496c79e21de3cadc43ae7832091543b99b5c490
Exr0n <spotyie@gmail.com>
2020-05-15 00:24:34 -0700

    copy-write treap again

---
+++ algorithms/trees/treap/treap_prev_next_construct.cpp
@@ -0,0 +1,55 @@
+// 15 May 2020
+// og code written in 6 min
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int d, w;
+	Node *c[2] = {};
+	Node(int d): d(d), w(rand()%10000) {}
+} *root;
+
+void rotate(Node *&cur, bool dir)
+{
+	if (!cur || !cur->c[dir]) return;
+	Node *thn = cur->c[dir];
+	cur->c[dir] = thn->c[1-dir];
+	thn->c[1-dir] = cur;
+	cur = thn;
+}
+
+Node *&insert(Node *&cur, int d)
+{
+	if (cur->d == d) return cur;
+	if (!cur) return cur = new Node(d);
+	Node *&stp = cur->c[cur->d < d];
+	Node *&ins = insert(stp, d);
+	if (cur->w < stp->w)
+		rotate(cur, cur->d < d);
+	return ins;
+}
+
+Node *&locate(Node *&cur, int d)
+{
+	if (!cur || cur->d == d) return cur;
+	return locate(cur->c[cur->d < d], d);
+}
+	
+void remove(Node *&cur)
+{
+	if (cur->c[0] && cur->c[1])
+	{
+		const bool dir = cur->c[0]->w < cur->c[1]->w;
+		rotate(cur, dir);
+		remove(cur->c[1-dir]);
+	}
+	else
+	{
+		Node *thn = cur;
+		cur = cur->c[0] ? cur->c[0] : cur->c[1];
+		delete thn;
+	}
+}
+
