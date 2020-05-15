2d173b659baae016d56821022348edea41868689
Exr0n <spotyie@gmail.com>
2020-05-14 20:15:17 -0700

    segtree seems to work, input is one indexed tho..?

--- algorithms/trees/segment_tree/segment_tree_0.cpp
+++ algorithms/trees/segment_tree/segment_tree_0.cpp
@@ -1,4 +1,5 @@
 // 14 May 2020
+// query and update are inc l inc r, input is 1 indexed for some reason..?
 
 #include <iostream>
 #include <numeric>
@@ -22,36 +23,35 @@ void build()
 	lays = ceil(log2(N));
 	for (int i=0; i<N; ++i)
 		scanf("%d", &segfault[i+ (1<<lays)]);
-	for (int i=lays; i>0; --i)
+	for (int i=(1<<lays)-1; i>0; --i) // FIX: i=(1<<lays)-1 not i=lays; -1 cuz else overwrite first of last layer
 		segfault[i] = min(segfault[2*i], segfault[2*i+1]);
-
-	dump();
 }
 
-int query(int lq, int rq, int idx=1, int lb=0, int rb=1<<lays, int acc=0)
+int query(int lq, int rq, int idx=1, int lb=1, int rb=1<<lays, int acc=0)
 {
-	if (rq < lb || lq > rb)
+	//for (int i=0; i<log2(idx); ++i) printf("|   "); printf("lq %d rq %d lb %d rb %d\n", lq, rq, lb, rb);
+	if (rq < lb || rb < lq)
 		return 1<<30;
 
 	acc += addv[idx];
-	if (lq <= lb && rq >= rb)
+	if (lq <= lb && rb <= rq)
 		return segfault[idx] + acc;
 
 	const int lc = 2*idx;
 	const int rc = 2*idx +1;
 	const int mid = lb + (rb-lb)/2; // (lb+rb)/2 w/o overflow
 	return min(
-			query(lq, rq, lc, lb, mid),
-			query(lq, rq, rc, mid+1, rb));
+			query(lq, rq, lc, lb, mid, acc),
+			query(lq, rq, rc, mid+1, rb, acc));
 }
 
-int update(int lq, int rq, int val, int idx=1, int lb=0, int rb=1<<lays)
+int update(int lq, int rq, int val, int idx=1, int lb=1, int rb=1<<lays)
 {
 	if (rq < lb || rb < lq) return segfault[idx];
 	if (lq <= lb && rb <= rq)
 	{
 		addv[idx] += val; // FIX: add to addv not minv
-		return segfault[idx];
+		return segfault[idx] + addv[idx]; // FIX: return acutual not original
 	}
 
 	const int lc = 2*idx;
