91fda67ae375ad82e80f2a8d0f31a71b0759b852
Exr0n <spotyie@gmail.com>
2020-05-19 17:42:21 -0700

    2 treaps are scuffed

--- problems/contests/algo/treap/2_statistical_work/main_2_statistical_work.cpp
+++ problems/contests/algo/treap/2_statistical_work/main_2_statistical_work.cpp
@@ -51,14 +51,14 @@ LANG: C++14
 
 using namespace std;
 const int MX = 500111;
-int N, M, head[MX], tail[MX], min_gap=1<<30, min_sort_gap=1<<30;
+int N, M, head[MX], tail[MX], min_sort_gap=1<<30;
 
 struct Node
 {
 	int d, w;
 	Node *c[2]={}, *n[2]={};
 	Node(int d): d(d), w(rand()) {}
-} *root = nullptr;
+} *num_root = nullptr, *gap_root = nullptr, *pre_min_gap = nullptr;
 void rotate(Node *&cur, bool dir)
 {
 	if (!cur || !cur->c[dir]) return;
@@ -67,13 +67,13 @@ void rotate(Node *&cur, bool dir)
 	thn->c[1-dir] = cur;
 	cur = thn;
 }
-Node *&treap_insert(Node *&cur, int d)
+Node *&tinsert(Node *&cur, int d)
 {
 	if (!cur) return cur = new Node(d);
 	if (cur->d == d) return cur;
 	const bool dir = cur->d < d;
 	Node *&stp = cur->c[dir];
-	Node *&ins = treap_insert(stp, dir);
+	Node *&ins = tinsert(stp, dir);
 	if (stp == ins && !ins->n[1-dir])
 	{
 		ins->n[dir] = cur->n[dir];
@@ -81,33 +81,59 @@ Node *&treap_insert(Node *&cur, int d)
 		cur->n[dir] = ins;
 		if (ins->n[dir])
 			ins->n[dir]->n[1-dir] = cur;
-		min_sort_gap = min(min_sort_gap, min(
-					d - ins->n[0]->d,
-					ins->n[1]->d - d));
 	}
 	if (cur->w < stp->w)
 		rotate(cur, dir);
 	return ins;
 }
+Node *&tlocate(Node *&cur, int d)
+{
+	if (!cur || cur->d == d) return cur;
+	return tlocate(cur->c[cur->d < d], d);
+}
+void tremove(Node *cur)
+{
+	if (!cur) return;
+	if (cur->c[0] && cur->c[1])
+	{
+		const bool dir = cur->c[0]->w < cur->c[1]->w;
+		rotate(cur, dir);
+		tremove(cur->c[1-dir]);
+	}
+	else
+	{
+		Node *thn = cur;
+		if (thn->n[0]) thn->n[0]->n[1] = thn->n[1];
+		if (thn->n[1]) thn->n[1]->n[0] = thn->n[0];
+		cur = cur->c[0] ? cur->c[0] : cur->c[1];
+		delete thn;
+	}
+}
+void tremove(Node *&root, int d)
+{ tremove(tlocate(root, d)); }
 
 void insert(int p, int d)
 {
-	min_gap = min(min_gap, (int)abs(tail[p]-d));
+	if (p+1 < N) tremove(gap_root, abs(tail[p] - head[p+1]));
+	tinsert(gap_root, abs(tail[p] - d));
 	tail[p] = d;
-	min_gap = min(min_gap, (int)abs(head[p+1]-d));
+	if (p+1 < N) tinsert(gap_root, abs(head[p+1] - d));
 
-	treap_insert(root, d);
-	printf("after insert: min_sort_gap = %3d, min_gap = %3d\n", min_sort_gap, min_gap);
+	Node *ins = tinsert(num_root, d);
+	min_sort_gap = min(min_sort_gap, min(d - ins->n[0]->d, ins->n[1]->d - d));
+	printf("after insert: min_sort_gap = %3d, min_gap = %3d\n", min_sort_gap, pre_min_gap->n[1]->d);
 }
 
 int main()
 {
 	scanf("%d%d", &N, &M);
+	pre_min_gap = tinsert(gap_root, -1);
 	for (int i=0; i<N; ++i)
 	{
 		scanf("%d", &head[i]);
 		tail[i] = head[i];
-		treap_insert(root, head[i]);
+		if (i) tinsert(gap_root, abs(head[i]-head[i-1]));
+		tinsert(num_root, head[i]);
 	}
 	printf("okay\n");
 	for (int m=0; m<M; ++m)
@@ -125,7 +151,7 @@ int main()
 			if (buf[3] == 'S')
 				printf("%d\n", min_sort_gap);
 			else
-				printf("%d\n", min_gap);
+				printf("%d\n", pre_min_gap->n[1]->d);
 		}
 	}
 
