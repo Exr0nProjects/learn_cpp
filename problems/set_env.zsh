6b94334c846aae1663933d706f4c2dd9682d9adf
Exr0n <spotyie@gmail.com>
2020-05-30 10:33:08 -0700

    better check alias

--- problems/set_env.zsh
+++ problems/set_env.zsh
@@ -7,6 +7,6 @@ tmux setenv IDEMODE 0
 export IDEMODE=0
 alias crt='cd $ENVROOT && source ./create.sh'
 alias ans='g++ -std=c++11 answer*.cpp -o answer && ./answer && cat *.out'
-alias check='py gen.py | tee >(./auto > a.diff) >(./answer > b.diff) && [[ ! -z "$(diff --brief *.diff)" ]] && v -d *.diff'
+alias check='for (( i=1; ; i++ )); do py gen.py > test.in && ./auto < test.in > test.diff && ./answer < test.in > correct.diff && echo -en "\rtest case $i" && [[ -z "$(diff --brief *.diff)" ]] || break; done'
 alias clean='setopt +o nomatch && rm -f auto *.in *.out answer *.diff'
 
