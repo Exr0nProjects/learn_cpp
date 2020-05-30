ef61135c0fa6e3e1bdc7b8c86bea2fca6eb4e8a6
Exr0n <spotyie@gmail.com>
2020-05-30 09:54:00 -0700

    bidx tree seems to work

--- algorithms/trees/bindex_tree/bidx_tree_0.cpp
+++ algorithms/trees/bindex_tree/bidx_tree_0.cpp
@@ -1,14 +1,24 @@
 // 30 May 2020
 
+#include <iostream>
+#define ll long long
 const int MX = 10000;
+
 ll N, bidx[MX];
+void update(ll i, ll v);
 void build()
 {
 	scanf("%lld", &N);
 	memset(bidx, 0, sizeof bidx);
-	for (int i=1; i<=N; ++i)		// FIX: one indexed
-		scanf("%lld", &bidx[i]);
-	// TODO: ^^^^^^^^^^^^^^^^^^^ that without another array
+	//for (int i=1; i<=N; ++i)		// FIX: one indexed
+	//    scanf("%lld", &bidx[i]);
+	//// TODO: ^^^^^^^^^^^^^^^^^^^ that without another array
+	for (int i=1; i<=N; ++i)
+	{
+		int t;
+		scanf("%d", &t);
+		update(i, t);
+	}
 }
 ll prefix(ll idx)
 {
@@ -31,5 +41,27 @@ void update(ll i, ll v)
 {
 	for (; i<=N; i+=i&-i)	// FIX: i<=N not just i, since i goes up
 		bidx[i] += v;
+}
+void dump()
+{
+	for (int i=1; i<=N; ++i)
+		printf("%3lld", bidx[i]);
+	printf("\n");
+	// TODO: print the tree structure
+}
+
+int main()
+{
+	build();
+	while (1)
+	{
+		char c = ' '; while (c < 'a' || c > 'z') scanf("%c", &c);
+		int a, b; scanf("%d%d", &a, &b);
+		if (c == 'q')
+			printf("%lld\n", query(a, b));
+		if (c == 'u')
+			update(a, b);
+		dump();
+	}
 }
 
