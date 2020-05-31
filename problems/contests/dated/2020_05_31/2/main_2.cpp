249b50123e22901e7a86bce53ce768c5e94bb779
Exr0n <spotyie@gmail.com>
2020-05-31 15:53:42 -0700

    diff this segtree with #3, found typo, shud work now

--- problems/contests/dated/2020_05_31/2/main_2.cpp
+++ problems/contests/dated/2020_05_31/2/main_2.cpp
@@ -64,17 +64,9 @@ void build()
 	for (int i=(1<<D)-1; i>0; --i)
 		segt[i] = max(segt[i*2], segt[i*2+1]);
 }
-int query(int ql, int qr, int k=1, int tl=1, int tr=1<<D, int acc=0)
-{
-	if (qr < tl || tr < ql) return 0;
-	if (ql <= tl && tr <= qr) return segt[k] + acc;
-	acc += addv[k];
-	int mid = tl + (tr-tl)/2;
-	return min(query(ql, qr, k*2, tl, mid, acc), query(ql, qr, k*2+1, mid+1, tr, acc));
-}
 void update(int ql, int qr, int v=1, int k=1, int tl=1, int tr=1<<D)
 {
-	if (ql < tl || tr < ql) return;
+	if (qr < tl || tr < ql) return;
 	if (ql <= tl && tr <= qr)
 	{
 		segt[k] += v;
@@ -86,6 +78,14 @@ void update(int ql, int qr, int v=1, int k=1, int tl=1, int tr=1<<D)
 	update(ql, qr, v, k*2+1, mid+1, tr);
 	segt[k] = max(segt[k*2], segt[k*2+1]);
 }
+int query(int ql, int qr, int k=1, int tl=1, int tr=1<<D, int acc=0)
+{
+	if (qr < tl || tr < ql) return 0;
+	if (ql <= tl && tr <= qr) return segt[k] + acc;
+	acc += addv[k];
+	int mid = tl + (tr-tl)/2;
+	return max(query(ql, qr, k*2, tl, mid, acc), query(ql, qr, k*2+1, mid+1, tr, acc));
+}
 
 bool cmp(const pair<int, int> &lhs, const pair<int, int> &rhs);
 
@@ -94,15 +94,18 @@ int main()
 	scanf("%d", &N);
 	for (int i=0; i<N; ++i)
 		scanf("%d", &capa[i]);
+	build();
 
-	while (true)
-	{
-		char c=' '; if (c<'a'||c>'z') scanf("%c", &c);
-		int a, b; scanf("%d%d", &a, &b);
-		if (c == 'q') printf("%d\n", query(a, b));
-		if (c == 'u') update(a, b);
-		dump();
-	}
+	//dump();
+	//while (true)
+	//{
+	//    char c=' '; while (c<'a'||c>'z') scanf("%c", &c);
+	//    int a, b; scanf("%d%d", &a, &b);
+	//    if (c == 'q') printf("%d\n", query(a, b));
+	//    if (c == 'u') update(a, b);
+	//    printf("finished processing command %c\n", c);
+	//    dump();
+	//}
 
 	return 0;
 }
