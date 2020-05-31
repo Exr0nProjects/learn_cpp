14e86735e74df9a4482e9d9eef787d53d07d7b9a
Exr0n <spotyie@gmail.com>
2020-05-31 14:51:18 -0700

    updating segtree has some issues

---
+++ problems/contests/dated/2020_05_31/3/main_3.cpp
@@ -0,0 +1,104 @@
+/*
+ * Problem 3 (contests/dated/2020_05_31/3)
+ * Create time: Sun 31 May 2020 @ 14:37 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <iterator>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const int MX = 60111;
+
+int N, S, R, D, segt[2*MX], addv[2*MX];
+void dump()
+{
+	for (int i=1; i<2<<D; ++i)
+	{
+		if (__builtin_popcount(i) == 1) printf("\n");
+		printf("%3d", segt[i]);
+	}
+	printf("\n");
+}
+void build()
+{
+	D = log2(N)+1;
+	memset(segt, 0, sizeof segt);
+	memset(addv, 0, sizeof addv);
+}
+int update(int ql, int qr, int v, int k=1, int tl=1, int tr=1<<D)
+{
+	printf("update %d..%d +%d   @%d (%d %d)\n", ql, qr, v, k, tl, tr);
+	if (qr < tl || tr < ql) return segt[k];
+	if (ql <= tl && tr <= qr)
+	{
+		addv[k] += v;
+		segt[k] += v;
+		return segt[k];
+	}
+	int mid = tl + (tr-tl)/2;
+	int lhs = update(ql, qr, v, k*2, tl, mid);
+	int rhs = update(ql, qr, v, k*2+1, mid+1, tr);
+	segt[k] = max(lhs, rhs);
+	return segt[k];
+}
+int query(int ql, int qr, int k=1, int tl=1, int tr=1<<D, int acc=0)
+{
+	if (qr < tl || tr < ql) return 0;
+	if (ql <= tl && tr <= qr) return segt[k] + acc;
+	acc += addv[k];
+	int mid = tl + (tr-tl)/2;
+	return max(query(ql, qr, k*2, tl, mid, acc), query(ql, qr, k*2+1, mid+1, tr, acc));
+}
+
+int main()
+{
+	scanf("%d", &N);
+	build();
+	while (true)
+	{
+		char c=' '; while (c<'a'||c>'z') scanf("%c", &c);
+		int a, b; scanf("%d%d", &a, &b);
+		if (c == 'q') printf("%d\n", query(a, b));
+		if (c == 'u')
+		{
+			int v; scanf("%d", &v);
+			update(a, b, v);
+		}
+		dump();
+	}
+
+	return 0;
+}
+
---
+++ problems/contests/dated/2020_05_31/timing_log.md
@@ -0,0 +1,34 @@
+# Contest contests/dated/2020_05_31 - played Sun 31 May 2020 @ 14:15 (PDT)
+
+## Contest Timeline
+
+| Time | Note | Next |
+|----|----|----|
+14:15 | start | reading problems
+14:22 | have plans for 1, 3, need to analyse 2 | coding #1
+14:37 | AC #1 | coding #3
+
+## Contest Reflection
+
+### Knowledge
+What knowledge did you use in this test, and which part you didn't do well knowledge-wise?
+
+[Link to the contest](http://115.236.49.52:83/contest/1399)
+
+#### Problem 1:
+
+-
+
+#### Problem 2:
+
+-
+
+#### Problem 3:
+
+-
+
+### Strategy
+How did you spend your time during the test, and whether there's any issue about your strategy?
+
+### Action item
+How would you change your homework, your mastery of knowledge, or class time etc in order to do better next time?
