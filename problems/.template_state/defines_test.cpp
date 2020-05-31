1ad5a967ace06ff0d032b3923f73cf296dfe6625
Exr0n <spotyie@gmail.com>
2020-05-31 14:13:59 -0700

    slim down template more

--- problems/.template_state/defines_test.cpp
+++ problems/.template_state/defines_test.cpp
@@ -1,9 +1,4 @@
 /*
-TASK: [!meta:pid!]
-LANG: C++14
-*/
-
-/*
  * Problem [!meta:pid!] ([!meta:srcpath!])
  * Create time: [!meta:beg!]
  * Accept time: [!meta:end!]
@@ -43,31 +38,12 @@ LANG: C++14
 #define f first
 #define s second
 
-#define FOR_(i, b, e) for (long long i = (b); i < (e); ++i)
-#define FOR(i, e) FOR_(i, 0, (e))
-#define FORR_(i, b, e) for (long long i = (e)-1; i >= (b); --i)
-#define FORR(i, e) FORR_(i, 0, e)
-#define TRAV(a, x) for (auto &a : x)
-
-void setIO(const std::string &name = "[!meta:pid!]");
-
 using namespace std;
 const int MX = -1;
 
 int main()
 {
-	setIO();
 
 	return 0;
 }
 
-void setIO(const string &name)
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); // fast cin/cout
-	if (fopen((name + ".in").c_str(), "r") != nullptr)
-	{
-		freopen((name + ".in").c_str(), "r", stdin);
-		freopen((name + ".out").c_str(), "w+", stdout);
-	}
-}
