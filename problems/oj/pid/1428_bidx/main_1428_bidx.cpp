396a0115a3ce01aa3b144b3a8081fa65f7d34cf3
Exr0n <spotyie@gmail.com>
2020-06-01 16:51:58 -0700

    parens around (-x) in x&-x

--- problems/oj/pid/1428_bidx/main_1428_bidx.cpp
+++ problems/oj/pid/1428_bidx/main_1428_bidx.cpp
@@ -54,26 +54,34 @@ void setIO(const std::string &name = "1428_bidx");
 using namespace std;
 const ll MX = 100111;
 
-ll T, N, bidx[MX], lef[MX], rig[MX];
+ll T, N, bidx[MX], c[MX], d[MX];
 
 void update(ll i, ll v)
 {
 	if (!i) return;	// FIX: can't update zero
-	for (; i<=MX; i += i&-i)	// FIX: MX not N, since we are doing the counting sort thing
+	for (; i<=MX; i += i&(-i))	// FIX: MX not N, since we are doing the counting sort thing
 		bidx[i] += v;
 }
-ll query(int l, int r)
-{
-	//printf("query %lld %lld, r<l: %lld\n", l, r, r < l);
-	if (r < l) return 0;	// FIX: invalid range detection
+//ll query(int l, int r)
+//{
+//    //printf("query %lld %lld, r<l: %lld\n", l, r, r < l);
+//    if (r < l) return 0;	// FIX: invalid range detection
 
-	ll tot; --l;			// FIX: --l for inclusive inclusive
-	for (; r>l; r-=r&-r)
-		tot += bidx[r];
-	for (; l>r; l-=l&-l)
-		tot -= bidx[l];
+//    ll tot; --l;			// FIX: --l for inclusive inclusive
+//    for (; r>l; r-=r&-r)
+//        tot += bidx[r];
+//    for (; l>r; l-=l&-l)
+//        tot -= bidx[l];
+//    return tot;
+//}
+ll query(int i)
+{
+	ll tot = 0;
+	for (; i; i-=i&(-i))
+		tot += i;
 	return tot;
 }
+
 void dump()
 {
 	return;
@@ -86,8 +94,8 @@ void solve()
 {
 	scanf("%lld", &N);
 	memset(bidx, 0, sizeof bidx);
-	memset(lef, 0, sizeof lef);
-	memset(rig, 0, sizeof rig);
+	memset(c, 0, sizeof c);
+	memset(d, 0, sizeof d);
 	vector<pair<ll, ll> > sorted;
 	//printf("epic"); fflush(stdout);
 	for (ll i=0; i<N; ++i)
@@ -95,8 +103,8 @@ void solve()
 		ll t; scanf("%lld", &t);
 		//printf("t: %lld, t-1: %lld\n", t, t-1);
 
-		lef[i] = query(1, t-1);
-		//printf("lef[%d] = %d\n", i, lef[i]);
+		c[i] = query(t-1);
+		//printf("c[%d] = %d\n", i, c[i]);
 		sorted.eb(t, i);
 
 		//printf("uh"); fflush(stdout);
@@ -105,14 +113,14 @@ void solve()
 	}
 	sort(sorted.begin(), sorted.end());
 	for (ll i=0; i<sorted.size(); ++i)
-		rig[sorted[i].s] = i - lef[sorted[i].s];
+		d[sorted[i].s] = i - c[sorted[i].s];
 
 	//for (int i=0; i<N; ++i)
-	//    printf("%d: left %4lld right %4lld\n", i, lef[i], rig[i], lef[i]*(N-rig[i]-i-1) + (i-lef[i])*rig[i]);
+	//    printf("%d: c %4lld right %4lld\n", i, c[i], d[i], c[i]*(N-d[i]-i-1) + (i-c[i])*d[i]);
 
 	ll tot = 0;
 	for (ll i=0; i<N; ++i)
-		tot += lef[i]*(N-rig[i]-i-1) + (i-lef[i])*rig[i];
+		tot += c[i]*(N-d[i]-i-1) + (i-c[i])*d[i];
 	printf("%lld\n", tot);
 }
 
