b591de5b7f444ae8cf0300bb870a10903854a1b9
Exr0n <spotyie@gmail.com>
2020-06-03 15:57:24 -0700

    idemode true; vertical split on crt

--- problems/set_env.zsh
+++ problems/set_env.zsh
@@ -3,8 +3,8 @@
 export ENVROOT=$(dirname "$(realpath -s "$0")")
 tmux setenv ENVROOT $ENVROOT
 
-tmux setenv IDEMODE 0
-export IDEMODE=0
+#tmux setenv IDEMODE 0
+#export IDEMODE=0
 alias crt='cd $ENVROOT && source ./create.sh'
 alias ans='g++ -std=c++11 answer*.cpp -o answer && ./answer && cat *.out'
 alias check='for (( i=1; ; i++ )); do py gen.py > test.in && ./auto < test.in > test.diff && ./answer < test.in > correct.diff && echo -en "\rtest case $i" && [[ -z "$(diff --brief *.diff)" ]] || break; done'
