58a0f4e4cb6b05f8fd3858a8768e09985a77bace
Exr0n <spotyie@gmail.com>
2020-06-03 15:55:11 -0700

    what if we just restart

--- problems/oj/pid/12563up/main_12563up.cpp
+++ problems/oj/pid/12563up/main_12563up.cpp
@@ -40,7 +40,7 @@
 
 using namespace std;
 const int MX = 51;
-const int MXT = 1000111;
+const int MXT = 20111;	// 50 * 180
 ll kase, N, T, len=0, tab[MX][MXT], dura[MX];
 vector<ll> best;
 
@@ -53,29 +53,32 @@ int main()
 		memset(tab, 0, sizeof tab);
 		best.clear();
 
-		for (int i=0; i<N; ++i)
+		for (int i=1; i<=N; ++i)
 			scanf("%lld", &dura[i]);
 		
 		printf("mkay %lld %lld\n", N, T);
 
-		for (int i=0; i<T; ++i)
-			if (i > dura[0])
-				tab[0][i] = 1;
+		//for (int i=0; i<T; ++i)
+			//if (i > dura[0])
+				//tab[0][i] = 1;
 
-		for (int i=1; i<N; ++i)
+		for (int i=1; i<=N; ++i)
 			for (int j=0; j<T; ++j)
+			{
+				tab[i][j] = tab[i-1][j];
 				if (j > dura[i])
+					tab[i][j] = max(tab[i][j], tab[i-1][j-dura[i]]+1);
+
+				if (tab[i][j] > len)
 				{
-					tab[i][j] = max(tab[i-1][j], tab[i-1][j-dura[i]]+1);
-					if (tab[i][j] > len)
-					{
-						len = tab[i][j];
-						best.clear();
-					}
-					if (tab[i][j] == len)
-						best.pb(j);
+					len = tab[i][j];
+					best.clear();
 				}
-		sort(best.begin(), best.end(), less<ll>{});
+				if (tab[i][j] == len)
+					best.pb(j);
+			}
+		sort(best.begin(), best.end(), greater<ll>{});
+		for (int i=0; i<best.size(); ++i) printf("%3d", best[i]); printf("\n");
 		printf("Case %d: %lld %lld\n", kek+1, len+1, best[0]+678);
 	}
 
