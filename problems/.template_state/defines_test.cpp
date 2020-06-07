a296d78d0d63e827ffc454d32b87905786608686
Exr0n <spotyie@gmail.com>
2020-06-07 12:36:35 -0700

    move cstring to top of template

--- problems/.template_state/defines_test.cpp
+++ problems/.template_state/defines_test.cpp
@@ -6,12 +6,12 @@
  */
 
 #include <iostream>
+#include <cstring>
 #include <sstream>
 #include <cstdio>
 #include <tuple>
 #include <vector>
 #include <string>
-#include <cstring>
 #include <list>
 #include <array>
 #include <queue>
