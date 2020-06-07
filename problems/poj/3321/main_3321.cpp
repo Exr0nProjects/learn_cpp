641a79f268ad3d00db8acf7273931aee2693c0ab
Exr0n <spotyie@gmail.com>
2020-06-07 15:02:43 -0700

    wa poj3321

--- problems/poj/3321/main_3321.cpp
+++ problems/poj/3321/main_3321.cpp
@@ -7,27 +7,8 @@
 
 #include <iostream>
 #include <cstring>
-#include <sstream>
-#include <cstdio>
-#include <tuple>
-#include <vector>
-#include <string>
-#include <list>
-#include <array>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <cmath>
-#include <random>
-#include <chrono>
 #include <utility>
-#include <iterator>
-#include <exception>
-#include <algorithm>
-#include <functional>
+#include <list>
 
 #define ll long long
 #define dl double
