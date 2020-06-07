4ff68791e664fb864aa9ae1e5c749ef50ead2211
Exr0n <spotyie@gmail.com>
2020-06-07 14:59:38 -0700

    poj3321 seems to work

--- problems/poj/3321/main_3321.cpp
+++ problems/poj/3321/main_3321.cpp
@@ -43,22 +43,24 @@ const int MX = 100111;
 int N, Q;
 
 list<int> head[MX];
-int center[MX], lef[MX], rig[MX];	// how the tree maps to the bit
+int lef[MX], rig[MX];	// how the tree maps to the bit
+
+bool apple[MX];
 
 ll bidx[MX];
 
 int idx=1;
 pair<int, int> get_bounds(int cur, int pre=0)
 {
-	printf("    get_bounds %d from %d\n", cur, pre);
+	//printf("    get_bounds %d from %d\n", cur, pre);
 	if (!pre) idx = 1; // simulate static variable
-	center[cur] = idx++;	// FIX: idx++ not cur++
-	printf("moving on %d %d\n", cur, head[cur].size());
+	int pos = idx++;	// FIX: idx++ not cur++
+	//printf("moving on %d %d\n", cur, head[cur].size());
 
 	pair<int, int> ret(0, 0);
 	if (head[cur].size() == 1)
 	{
-		ret = mp(center[cur], center[cur]); // no children
+		ret = mp(pos, pos); // no children
 	}
 	else
 	{
@@ -69,12 +71,28 @@ pair<int, int> get_bounds(int cur, int pre=0)
 				//if (!ret.f) ret.f = bounds.f;
 				ret.s = bounds.s;
 			}
-		ret.f = center[cur];	// include self
+		ret.f = pos;	// include self
 	}
 
 	lef[cur] = ret.f;
 	rig[cur] = ret.s;
 	return ret;
+}
+
+ll query(int l, int r)
+{
+	--l;
+	ll tot = 0;
+	for (; r>l; r-=r&-r)
+		tot += bidx[r];
+	for (; l>r; l-=l&-l)
+		tot -= bidx[l];
+	return tot;
+}
+void update(int i, int v)
+{
+	for (; i<=N; i+=i&-i)
+		bidx[i] += v;
 }
 
 int main()
@@ -87,15 +105,28 @@ int main()
 		head[v].pb(u);
 	}
 
-	get_bounds(1);
-	for (int i=1; i<=N; ++i)
-		printf("fork %3d (%3d): %3d ..%3d\n", i, center[i], lef[i], rig[i]);
+	get_bounds(1);		// figure out how to map the input tree to the bit
+	//for (int i=1; i<=N; ++i) printf("fork %3d (%3d): %3d ..%3d\n", i, center[i], lef[i], rig[i]);
 
-	return 0;
+	// init the tree to full
+	for (int i=1; i<=N; ++i)
+	{
+		update(i, 1);
+		apple[i] = 1;
+	}
 
 	scanf("%d", &Q);
 	for (int i=0; i<Q; ++i)
-	{ /* TODO */ }
+	{
+		char c=0; while (c != 'Q' && c != 'C') scanf("%c", &c);
+		int d; scanf("%d", &d);
+		if (c == 'Q') printf("%d\n", query(lef[d], rig[d]));
+		if (c == 'C')
+		{
+			update(d, 1-2*apple[d]);
+			apple[d] ^= 1;
+		}
+	}
 
 	return 0;
 }
