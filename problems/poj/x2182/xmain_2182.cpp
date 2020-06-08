10d83d1b6a68da82c7134fea6998783f9ccf3fe8
Exr0n <spotyie@gmail.com>
2020-06-07 20:21:58 -0700

    AC poj/2182 (script)

---
+++ problems/poj/x2182/xmain_2182.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem 2182 (poj/2182)
+ * Create time: Sun 07 Jun 2020 @ 13:14 (PDT)
+ * Accept time: Sun 07 Jun 2020 @ 20:21 (PDT)
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const int MX = 8111;
+ll N, arr[MX], ans[MX];
+
+int main()
+{
+	scanf("%lld", &N);
+	for (int i=2; i<=N; ++i)
+		scanf("%d", &arr[i]);
+
+	for (int k=1; k<=N; ++k)
+		for (int i=N; i>0; --i)
+			if (!arr[i])
+			{
+				for (int j=i; j<=N; ++j)
+					--arr[j];	// FIX: arr[j] not arr[i]
+				ans[i] = k;
+				break;
+			}
+
+	for (int i=1; i<=N; ++i)
+		printf("%lld\n", ans[i]);
+
+	return 0;
+}
+
