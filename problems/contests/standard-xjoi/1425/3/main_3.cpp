b8e201543a7d924e8f81ab719d9226a1e57404f5
Exr0n <spotyie@gmail.com>
2020-06-09 18:54:35 -0700

    this is actually too heavy since we kno whos parent
    the whole of depth() was written for a bidirectional edgelist

--- problems/contests/standard-xjoi/1425/3/main_3.cpp
+++ problems/contests/standard-xjoi/1425/3/main_3.cpp
@@ -40,7 +40,26 @@
 
 using namespace std;
 const int MX = 101;
-int N, root=0, leafs=0, childcount[MX];
+int N, root=0, leafs=0;
+
+list<int> head[MX];
+
+int depth(int cur, int pre=0)
+{
+	printf("cur = %d\n", cur);
+	if (pre && head[cur].size() != 3) return -1;
+	int dep = -1;
+	for (int nxt : head[cur])
+		if (nxt != pre)
+		{
+			int got = depth(nxt, cur);
+			if (got < 0) return -1;
+			if (dep >= 0 && dep != got) return -1; // depth mismatch
+			dep = got;
+		}
+	printf("depth %d -> %d\n", cur, dep);
+	return dep+1;
+}
 
 int main()
 {
@@ -60,21 +79,21 @@ int main()
 		}
 		else
 		{
-			++childcount[parent];
+			head[parent].pb(i);
 		}
 	}
 
-	for (int i=1; i<=N; ++i)
-	{
-		if (childcount[i] != 2)
-		{
-			if (childcount[i] == 0)
-				++leafs;
-			else { printf("no\n"); return 0; }
-		}
-	}
+	//for (int i=1; i<=N; ++i)
+	//{
+	//    if (childcount[i] != 2)
+	//    {
+	//        if (childcount[i] == 0)
+	//            ++leafs;
+	//        else { printf("no\n"); return 0; }
+	//    }
+	//}
 
-	if (N+1 == 2*leafs)
+	if (depth(root) >= 0)
 		printf("yes\n");
 	else
 		printf("no\n");
