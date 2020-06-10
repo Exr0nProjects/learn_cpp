20f46993fc090166f98c81a4c29424d2f39e3277
Exr0n <spotyie@gmail.com>
2020-06-09 19:02:32 -0700

    with depth checking i don't need to check self-parents

--- problems/contests/standard-xjoi/1425/3/main_3.cpp
+++ problems/contests/standard-xjoi/1425/3/main_3.cpp
@@ -42,23 +42,19 @@ using namespace std;
 const int MX = 101;
 int N, root=0, leafs=0;
 
-list<int> head[MX];
+vector<int> head[MX];
 
-int depth(int cur, int pre=0)
+int depth(int cur, int pre=0)	// FIX: check each subtree depth individually
 {
-	printf("cur = %d\n", cur);
-	if (pre && head[cur].size() != 3) return -1;
-	int dep = -1;
-	for (int nxt : head[cur])
-		if (nxt != pre)
-		{
-			int got = depth(nxt, cur);
-			if (got < 0) return -1;
-			if (dep >= 0 && dep != got) return -1; // depth mismatch
-			dep = got;
-		}
-	printf("depth %d -> %d\n", cur, dep);
-	return dep+1;
+	if (pre && head[cur].size() == 0) return 0;
+	if (pre && head[cur].size() != 2) return -1;
+
+	// i didn't mean it, but this looks alot like XC2000268AH's code
+	// much credit to them for making me realize that you need to check subtree depth
+	int lef = depth(head[cur][0], cur);
+	int rig = depth(head[cur][1], cur);
+	if (lef != rig) return -1;
+	return lef + 1;
 }
 
 int main()
@@ -71,7 +67,6 @@ int main()
 	{
 		int parent;
 		scanf("%d", &parent);
-		if (parent == i) { printf("no\n"); return 0; }	// FIX: check if is own parent (from XC2000268AH)
 		if (parent < 0)
 		{
 			if (!root) root = i;
@@ -82,16 +77,6 @@ int main()
 			head[parent].pb(i);
 		}
 	}
-
-	//for (int i=1; i<=N; ++i)
-	//{
-	//    if (childcount[i] != 2)
-	//    {
-	//        if (childcount[i] == 0)
-	//            ++leafs;
-	//        else { printf("no\n"); return 0; }
-	//    }
-	//}
 
 	if (depth(root) >= 0)
 		printf("yes\n");
