0bc083d492527ba2199d9221445a298b05eb93fd
Exr0n <spotyie@gmail.com>
2020-06-11 07:37:05 -0700

    plm #1

---
+++ problems/contests/standard-xjoi/1405/1/main_1.cpp
@@ -0,0 +1,77 @@
+/*
+ * Problem 1 (contests/standard-xjoi/1405/1)
+ * Create time: Thu 11 Jun 2020 @ 07:32 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <cstdio>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <iterator>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const int MX = 1000111;
+ll N, M, bidx[MX];
+void update(int i, ll v)
+{
+	for (; i<=N; i+=i&-i)
+		bidx[i] += v;
+}
+ll query(int l, int r)
+{
+	ll tot = 0; --l;
+	for (; r>l; r-=r&-r)
+		tot += bidx[r];
+	for (; l>r; l-=l&-l)
+		tot -= bidx[l];
+	return tot;
+}
+
+int main()
+{
+	scanf("%d%d", &N, &M);
+	for (int i=0; i<N; ++i)
+	{
+		int d; scanf("%d", &d);
+		update(i+1, d);
+	}
+	for (int i=0; i<M; ++i)
+	{
+		int c, a, b;
+		scanf("%d%d%d", &c, &a, &b);
+		if (c == 1) update(a, b);
+		if (c == 2) printf("%lld\n", query(a, b));
+	}
+
+	return 0;
+}
+
---
+++ problems/contests/standard-xjoi/1405/timing_log.md
@@ -0,0 +1,32 @@
+# Contest contents/standard-xjoi/1405 - played Thu 11 Jun 2020 @ 7:14 (PDT)
+
+## Contest Timeline
+
+| Time | Note | Next |
+|----|----|----|
+07:15 | start | reading problems
+07:31 | have plans for 1-3, 4 sketch, dunno 5 | coding #1
+07:36 | submitted #1 | coding #2
+
+## Contest Reflection
+
+### Knowledge
+What knowledge did you use in this test, and which part you didn't do well knowledge-wise?
+
+#### Problem 1:
+
+-
+
+#### Problem 2:
+
+-
+
+#### Problem 3:
+
+-
+
+### Strategy
+How did you spend your time during the test, and whether there's any issue about your strategy?
+
+### Action item
+How would you change your homework, your mastery of knowledge, or class time etc in order to do better next time?
