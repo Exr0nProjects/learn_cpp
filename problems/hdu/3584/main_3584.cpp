10f403c8ad04181c588ae760d273008e3e6ce648
Exr0n <spotyie@gmail.com>
2020-06-10 23:18:38 -0700

    GIT REVERT

--- problems/hdu/3584/main_3584.cpp
+++ problems/hdu/3584/main_3584.cpp
@@ -22,67 +22,65 @@ const int MX = 111;
 
 int N, Q, delt[MX][MX][MX];
 
-void modify(int i, int _j, int _k, int v=1)
+void rupdate(int i, int _j, int _k, int v=1)
 {
 	for (; i<=N; i+=i&-i)
 		for (int j=_j; j<=N; j+=j&-j)
 			for (int k=_k; k<=N; k+=k&-k)
-				//delt[i][j][k] += v;
-				delt[i][j][k] = !delt[i][j][k];
+				delt[i][j][k] += v;
+				//delt[i][j][k] = !delt[i][j][k];
 }
 
-ll query(int i, int _j, int _k)
+int query(int i, int _j, int _k)
 {
-	ll tot = 0;
+	int tot = 0;
 	for (; i; i-=i&-i)
 		for (int j=_j; j; j-=j&-j)
 			for (int k=_k; k; k-=k&-k)
-				if (delt[i][j][k])
-					tot != tot;
+			{
+				//printf("    adding %d %d\n", i, j);
+				tot += delt[i][j][k];
+				//tot ^= delt[i][j][k];
+				//if (delt[i][j][k]) tot = !tot;
+			}
 	return tot;	// FIX: don't forget to return
 }
 
-//void update(int i1, int j1, int k1, int i2, int j2, int k2, int v=1)
-//{
-//
-//    // magenta
-//    rupdate(i2, j2, k2, -v);
-//    // blue
-//    rupdate(i2, j2, k1, v);
-//    rupdate(i2, j1, k2, v);
-//    rupdate(i1, j2, k2, v);
-//    // yellow
-//    rupdate(i2, j1, k1, -v);
-//    rupdate(i1, j2, k1, -v);
-//    rupdate(i1, j1, k2, -v);
-//    // orange
-//    rupdate(i1, j1, k1, v);
-//}
+void update(int i1, int j1, int k1, int i2, int j2, int k2, int v=1)
+{
+	++i2; ++j2; ++k2;
+	// magenta
+	rupdate(i2, j2, k2, -v);
+	// blue
+	rupdate(i2, j2, k1, v);
+	rupdate(i2, j1, k2, v);
+	rupdate(i1, j2, k2, v);
+	// yellow
+	rupdate(i2, j1, k1, -v);
+	rupdate(i1, j2, k1, -v);
+	rupdate(i1, j1, k2, -v);
+	// orange
+	rupdate(i1, j1, k1, v);
+}
 
 int main()
 {
-	scanf("%d%d", &N, &Q);
-	for (int i=0; i<Q; ++i)
+	while (scanf("%d%d", &N, &Q))
 	{
-		int c, x1, y1, z1;
-		scanf("%d%d%d%d", &c, &x1, &y1, &z1);
-		if (c)
+		for (int i=0; i<Q; ++i)
 		{
-			int x2, y2, z2;
-			scanf("%d%d%d", &x2, &y2, &z2);
-			//update(i1, j1, k1, i2, j2, k2);
-			modify(x1, y1, z1);
-			modify(x1, y1, z2 + 1);
-			modify(x1, y2 + 1, z1);
-			modify(x2 + 1, y1, z1);
-			modify(x1, y2 + 1, z2 + 1);
-			modify(x2 + 1, y1, z2 + 1);
-			modify(x2 + 1, y2 + 1, z1);
-			modify(x2 + 1, y2 + 1, z2 + 1);
-		}
-		else
-		{
-			printf("%d\n", query(x1, y1, z1));	// FIX: output ll (long long)?
+			int c, x1, y1, z1;
+			scanf("%d%d%d%d", &c, &x1, &y1, &z1);
+			if (c)
+			{
+				int x2, y2, z2;
+				scanf("%d%d%d", &x2, &y2, &z2);
+				update(x1, y1, z1, x2, y2, z2);
+			}
+			else
+			{
+				printf("%d\n", query(x1, y1, z1)%2);	// FIX: output ll (long long)?
+			}
 		}
 	}
 
