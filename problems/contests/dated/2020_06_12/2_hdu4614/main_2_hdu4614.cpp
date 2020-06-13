6a068b58ff6b5d47adf2cbad3d94dad5f74025da
Exr0n <spotyie@gmail.com>
2020-06-13 13:52:59 -0700

    UT: segt with sett and pushdown for #2

---
+++ problems/contests/dated/2020_06_12/2_hdu4614/main_2_hdu4614.cpp
@@ -0,0 +1,97 @@
+/*
+ * Problem 2_hdu4614 (contests/dated/2020_06_12/2_hdu4614)
+ * Create time: Sat 13 Jun 2020 @ 13:43 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <cstdio>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <iterator>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const int MX = 50111;
+
+int N, D, segt[MX], sett[MX];
+
+void init()
+{
+	scanf("%d", &N);
+	memset(segt, 0, sizeof segt);
+	memset(sett, -1, sizeof sett);
+	D = log2(N) +1;
+	// no need to init segtree since it's all just zero
+}
+
+void push_down(int k, int tl, int tr)
+{
+	if (sett[k] < 0) return;
+	sett[k<<1] = sett[k<<1|1] = sett[k];
+	segt[k<<1] = segt[k<<1|1] = (tr-tl+1)*sett[k];
+	sett[k] = -1;
+}
+void collect(int k, int tl, int tr)
+{
+	if (sett[k] >= 0) segt[k] = (tr-tl+1)*sett[k];
+	else segt[k] = segt[k<<1] + segt[k<<1|1];
+}
+
+int query(int ql, int qr, int k=1, int tl=1, int tr=1<<D)
+{
+	if (qr < tl || tr < ql) return 0;
+	if (ql <= tl && tr <= qr) return segt[k];
+	push_down(k, tl, tr);
+	int mid = tl + (tr-tl)/2;
+	return query(ql, qr, k<<1, tl, mid), query(ql, qr, k<<1|1, mid+1, tr);
+}
+void update(int ql, int qr, int v, int k=1, int tl=1, int tr=1<<D)
+{
+	if (qr < tl || tr < ql) return;
+	if (ql <= tl && tr <= qr)
+	{
+		sett[k] = v;
+		segt[k] = (tr-tl+1) * v;
+		return;
+	}
+	push_down(k, tl, tr);
+	int mid = tl + (tr-tl)/2;
+	update(ql, qr, v, k<<1, tl, mid);
+	update(ql, qr, v, k<<1|1, mid+1, tr);
+	collect(k);
+}
+
+int main()
+{
+
+	return 0;
+}
+
--- problems/contests/dated/2020_06_12/timing_log.md
+++ problems/contests/dated/2020_06_12/timing_log.md
@@ -8,6 +8,9 @@
 12:59 | not a contest, lets just write it | coding #1
 13:22 | wrote #1, CE?? | debugging #1
 13:23 | TLE #1? | debugging
+13:25 | Q log N should be fast enough, wut? | analysing #2
+13:29 | seems doable in Q log^2 N, might as well read others | reading problems
+13:41 | can't do 4, too math. 5 hard to code, dunno 3. | psudocoding #2
 
 ## Contest Reflection
 
@@ -27,6 +30,12 @@ What knowledge did you use in this test, and which part you didn't do well knowl
 -
 
 #### Problem 4: https://vjudge.net/problem/HDU-4578
+
+big bad
+
+#### Problem 5: https://vjudge.net/problem/CodeChef-ADDMUL
+
+lil bad
 
 ### Strategy
 How did you spend your time during the test, and whether there's any issue about your strategy?
