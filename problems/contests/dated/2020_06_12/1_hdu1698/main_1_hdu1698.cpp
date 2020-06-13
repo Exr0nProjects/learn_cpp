e9ed1c8a5369f6c39e3e4081d63e0da3b6545281
Exr0n <spotyie@gmail.com>
2020-06-13 13:15:08 -0700

    UT: og segt with set update and push down

---
+++ problems/contests/dated/2020_06_12/1_hdu1698/main_1_hdu1698.cpp
@@ -0,0 +1,123 @@
+/*
+ * Problem 1_hdu1698 (contests/dated/2020_06_12/1_hdu1698)
+ * Create time: Sat 13 Jun 2020 @ 13:00 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <cstdio>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <iterator>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const int MX = 100111;
+
+int N, D, segt[MX], sett[MX];
+void init_segt()
+{
+	memset(segt, 0, sizeof segt);
+	memset(sett, 0, sizeof sett);
+
+	scanf("%d", &N);
+
+	D = log2(N) +1;
+	for (int i=0; i<N; ++i)
+		scanf("%d", &segt[i+(1<<D)]);
+
+	for (int i=(1<<D)-1; i>0; --i)
+		segt[i] = segt[i*2] + segt[i*2+1];
+}
+
+void push_down(int k, int tl, int tr)
+{
+	if (!sett[k]) return;
+	sett[k*2] = sett[k*2+1] = sett[k];
+	segt[k*2] = segt[k*2+1] = (tr-tl)/2 * sett[k];
+	sett[k] = 0;
+}
+
+void collect(int k, int tl, int tr)
+{
+	segt[k] = segt[k*2] + segt[k*2+1];
+}
+
+int query(int ql, int qr, int k=1, int tl=0, int tr=1<<D)
+{
+	if (qr < tl || tr < ql) return 0;
+	if (ql <= tl && tr <= qr) return segt[k];	// TODO: need to take into account sett?
+	push_down(k, tl, tr);
+	int mid = tl + (tr - tl)/2;
+	return query(ql, qr, k*2, tl, mid) + query(ql, qr, k*2+1, mid+1, tr);
+}
+
+void update(int ql, int qr, int v, int k=1, int tl=0, int tr=1<<D)
+{
+	if (qr < tl || tr < ql) return;
+	if (ql <= tl && tr <= qr)
+	{
+		sett[k] = v;
+		segt[k] = (tr-tl+1)*v;
+		return;
+	}
+	push_down(k, tl, tr);
+	int mid = tl + (tr - tl) /2;
+	update(ql, qr, v, k*2, tl, mid);
+	update(ql, qr, v, k*2+1, mid+1, tr);
+	collect(k, tl, tr);
+}
+
+void dump()
+{
+	for (int i=1; i<=1<<1+D; ++i)
+	{
+		if (__builtin_popcount(i) == 1) printf("\n");
+		printf("%3d %3d,  ", segt[i], sett[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	init_segt();
+	dump();
+	printf("les go\n");
+	while (true)
+	{
+		int l, r, v;
+		scanf("%d%d%d", &l, &r, &v);
+		update(l, r, v);
+		dump();
+	}
+
+	return 0;
+}
+
---
+++ problems/contests/dated/2020_06_12/timing_log.md
@@ -0,0 +1,33 @@
+# Contest contests/dated/2020_06_12 - played Sat 13 Jun 2020 @ 12:53 (PDT)
+
+## Contest Timeline
+
+| Time | Note | Next |
+|----|----|----|
+12:54 | start | reading problems
+12:59 | not a contest, lets just write it | coding #1
+
+## Contest Reflection
+
+### Knowledge
+What knowledge did you use in this test, and which part you didn't do well knowledge-wise?
+
+#### Problem 1: https://vjudge.net/problem/HDU-1698
+
+-
+
+#### Problem 2: https://vjudge.net/problem/HDU-4614
+
+-
+
+#### Problem 3: http://poj.org/problem?id=2777
+
+-
+
+#### Problem 4: https://vjudge.net/problem/HDU-4578
+
+### Strategy
+How did you spend your time during the test, and whether there's any issue about your strategy?
+
+### Action item
+How would you change your homework, your mastery of knowledge, or class time etc in order to do better next time?
