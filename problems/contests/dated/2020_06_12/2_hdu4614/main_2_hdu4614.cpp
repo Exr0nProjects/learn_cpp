39aac52dfefd0e3a5416918de73c63cbeed98815
Exr0n <spotyie@gmail.com>
2020-06-15 09:58:58 -0700

    inline functions; bit shift still TLE

--- problems/contests/dated/2020_06_12/2_hdu4614/main_2_hdu4614.cpp
+++ problems/contests/dated/2020_06_12/2_hdu4614/main_2_hdu4614.cpp
@@ -43,14 +43,14 @@ const int MX = 50111;
 
 int N, M, D, segt[2*MX], sett[2*MX];	// FIX: segt is double sized
 
-void push_down(int k, int tl, int tr)
+inline void push_down(int k, int tl, int tr)
 {
 	if (sett[k] < 0) return;
 	sett[k<<1] = sett[k<<1|1] = sett[k];
-	segt[k<<1] = segt[k<<1|1] = (tr-tl+1)/2*sett[k];	// FIX: typo--range/2 not just range, cuz its a child
+	segt[k<<1] = segt[k<<1|1] = (tr-tl+1>>1)*sett[k];	// FIX: typo--range/2 not just range, cuz its a child
 	sett[k] = -1;
 }
-void collect(int k, int tl, int tr)
+inline void collect(int k, int tl, int tr)
 {
 	if (sett[k] >= 0) segt[k] = (tr-tl+1)*sett[k];
 	else segt[k] = segt[k<<1] + segt[k<<1|1];
@@ -63,7 +63,7 @@ int query(int ql, int qr, int k=1, int tl=1, int tr=1<<D, int lay=0)
 	if (ql <= tl && tr <= qr) return segt[k];
 	//if (lay) printf("    not quite...\n");
 	push_down(k, tl, tr);
-	int mid = tl + (tr-tl)/2;
+	int mid = tl + (tr-tl>>1);
 	return query(ql, qr, k<<1, tl, mid, lay+!!lay)		// FIX: function args--recurse with lay+1 to propogate
 		 + query(ql, qr, k<<1|1, mid+1, tr, lay+!!lay);	// FIX: typo--query() + query(), not query(), query() smah
 
@@ -78,45 +78,33 @@ void update(int ql, int qr, int v, int k=1, int tl=1, int tr=1<<D)
 		return;
 	}
 	push_down(k, tl, tr);
-	int mid = tl + (tr-tl)/2;
+	int mid = tl + (tr-tl>>1);
 	update(ql, qr, v, k<<1, tl, mid);
 	update(ql, qr, v, k<<1|1, mid+1, tr);
 	collect(k, tl, tr);
 }
 
-void dump()
-{
-	return;
-	for (int k=1; k<(1<<1+D); ++k)
-	{
-		if (__builtin_popcount(k) == 1) printf("\n");
-		printf("%3d %3d, ", segt[k], sett[k]);
-	}
-	printf("\n");
-	for (int i=1; i<=N; ++i)
-		printf("%3d      ", query(i, i));
-	printf("\n");
-}
-
-//int bins(int a, int d, int l=1, int r=N)	// FIX: use inc inc binsearch to match segtree
+//void dump()
 //{
-//    //printf("    binary search %d..%d (%d %d)\n", l, r, a, d);
-//    if (l == r) return l;
-//    int mid = l + (r-l)/2;
-//    int gaps = mid-a+1-query(a, mid);
-//    //printf("mid %d, gaps %d\n", mid, gaps);
-//    if (gaps == d) return bins(a, d, l, mid);	// FIX: equ--return bins(..., l, mid) not just mid
-//    else if (gaps < d) return bins(a, d, mid+1, r);
-//    else return bins(a, d, l, mid-1);
+//    return;
+//    for (int k=1; k<(1<<1+D); ++k)
+//    {
+//        if (__builtin_popcount(k) == 1) printf("\n");
+//        printf("%3d %3d, ", segt[k], sett[k]);
+//    }
+//    printf("\n");
+//    for (int i=1; i<=N; ++i)
+//        printf("%3d      ", query(i, i));
+//    printf("\n");
 //}
 
-int bins_iter(int a, int d, int l=1, int r=N)
+inline int bins_iter(int a, int d, int l=1, int r=N)
 {
 	for (int i=0; i<30; ++i)
 	{
 		//printf("    iter   search %d..%d (%d %d)\n", l, r, a, d);
 		if (l == r) break;
-		int mid = l + (r-l)/2;	// FIX: cannot l + (r-l)>>1 because order of ops
+		int mid = l + (r-l>>1);	// FIX: cannot l + (r-l)>>1 because order of ops
 		int gaps = mid-a+1-query(a, mid);
 		if (gaps == d) r = mid;
 		else if (gaps < d) l = mid+1;
