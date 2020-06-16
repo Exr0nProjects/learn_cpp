d439d3306b3afb10ee6322a630adc14776ed29db
Exr0n <spotyie@gmail.com>
2020-06-16 10:19:03 -0700

    modify code for #5 for subset of #4

---
+++ problems/contests/dated/2020_06_12/4_hdu4578/gen.py
@@ -0,0 +1,25 @@
+MXN = 100000
+MXM = 100000
+MXC = 10000
+
+MXT = 1
+MXP = 1
+
+MXN = 10
+MXM = 10
+
+from random import randint
+T = randint(1, MXT)
+for i in range(T):
+    N = randint(1, MXN)
+    M = randint(1, MXM)
+    print(N, M)
+    for j in range(M):
+        l = randint(1, N)
+        r = randint(l, N)
+        if (randint(0, 1)):
+            print(4, l, r, randint(1, MXP))
+        else:
+            print(randint(1, 3), l, r, randint(1, MXC))
+print('0 0')
+
