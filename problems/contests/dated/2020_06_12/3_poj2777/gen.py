0d9945b1d3da1d1bcd590a1b5e2968dd637f525f
Exr0n <spotyie@gmail.com>
2020-06-16 09:09:11 -0700

    yeah idk why its wa

--- problems/contests/dated/2020_06_12/3_poj2777/gen.py
+++ problems/contests/dated/2020_06_12/3_poj2777/gen.py
@@ -2,18 +2,18 @@ MX = 100000
 OPS = 100000
 
 MX = 100000
-OPS = 10000
+OPS = 100000
 
 from random import randint
-L = randint(1, MX)
-O = randint(1, OPS)
+L = randint(MX-100, MX)
+O = randint(OPS-100, OPS)
 print(f'{L} 30 {O}')
 
 for i in range(O):
     l = randint(1, L)
     r = randint(l, L)
-    if randint(0, 1):
-        print(f'C {l} {r} {randint(1, 30)}')
-    else:
+    if randint(0, 8):
         print(f'P {l} {r}')
+    else:
+        print(f'C {l} {r} {randint(1, 30)}')
 
