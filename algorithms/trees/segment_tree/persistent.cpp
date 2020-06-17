8feea4e1b1189285078de98af65dc77e9713f74f
Exr0n <spotyie@gmail.com>
2020-06-17 10:41:42 -0700

    persistent dump() requires bfs

--- algorithms/trees/segment_tree/persistent.cpp
+++ algorithms/trees/segment_tree/persistent.cpp
@@ -1,6 +1,7 @@
 // persistent segtree 1, 17 june 2020
 #include <iostream>
 #include <cstring>
+#include <queue>
 #include <cmath>
 
 #define ll long long
@@ -10,14 +11,20 @@ const ll MX = 100111;
 ll N, D, M, segt[MX<<6], addt[MX<<6];
 ll alloc=1, root[MX], lc[MX<<6], rc[MX<<6];
 
-void dump(ll root=1)
+void dump(ll k)
 {
 	ll d = D+1;
-	for (ll i=1; i<1<<1+D; ++i)
+	queue<int> bfs;
+	bfs.push(k);
+	for (int i=1; i<1<<1+D; ++i)
 	{
 		if (__builtin_popcount(i) == 1) { --d; printf("\n"); }
-		printf("%3d +%-2d(%-2d %2d) ", segt[i], addt[i], lc[i], rc[i]);
-		for (int i=1; i<1<<d; ++i) printf("               ");
+		k = bfs.front(); bfs.pop();
+		bfs.push(lc[k]);
+		bfs.push(rc[k]);
+
+		printf("%2d: %2d +%-2d(%-2d %2d) ", k, segt[k], addt[k], lc[k], rc[k]);
+		for (int i=1; i<1<<d; ++i) printf("                  ");
 	}
 }
 void apply(ll addv, ll k, ll tl, ll tr)
@@ -55,7 +62,7 @@ void update(ll ql, ll qr, ll v, ll &k, ll tl=1, ll tr=1<<D)
 	update(ql, qr, v, rc[k], mid+1, tr);
 	comb(k);
 }
-ll query(ll ql, ll qr, ll k=1, ll tl=1, ll tr=1<<D)
+ll query(ll ql, ll qr, ll k, ll tl=1, ll tr=1<<D)
 {
 	if (qr<tl || tr<ql) return 0;
 	if (ql <= tl && tr <= qr) return segt[k];
@@ -69,24 +76,20 @@ int main()
 	D = log2(N)+1;
 
 	// FIX: init the storage
-	root[1] = alloc++;	// 1 indexed for consistency
+	root[0] = alloc++;	// 1 indexed for consistency
 	for (int i=1; i<1<<D; ++i)
 	{
 		lc[i] = alloc++;
 		rc[i] = alloc++;
 	}
-	ll updates = 1;
 
-	for (int i=0; i<M; ++i)
+	for (int i=1; i<=M; ++i)
 	{
-		dump(); printf("\n\n$ ");
-		int c, l, r; scanf("%d%d%d", &c, &l, &r);
-		if (c == 1)
-		{
-			root[++updates] = root[updates-1];
-			int d; scanf("%d", &d);
-			update(l, r, d, root[updates]);
-		}
-		if (c == 2) printf("%d\n", query(l, r));
+		dump(root[i-1]); printf("\n\nt = %d $ ", i);
+		root[i] = root[i-1];
+		int c, l, r, v;
+		scanf("%d%d%d%d", &c, &l, &r, &v);
+		if (c == 1) update(l, r, v, root[i]);
+		if (c == 2) printf("%d\n", query(l, r, root[v]));
 	}
 }
