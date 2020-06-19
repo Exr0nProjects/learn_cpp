247b8d9bc830a185c2072166d7261d64e819ad19
Exr0n <spotyie@gmail.com>
2020-06-19 14:39:51 -0700

    AC contests/2020summer401/2/2 (script)

--- problems/contests/2020summer401/2/x2/main_2.cpp
+++ problems/contests/2020summer401/2/x2/main_2.cpp
@@ -10,17 +10,10 @@
 #include <cmath>
 
 #define ll long long
-#define dl double
-
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#define f first
-#define s second
 
 using namespace std;
 const ll MX = 100111;
-int N, gonedepth=0;
+int N, gone=0;
 char inp[MX];
 
 struct Node
@@ -34,98 +27,69 @@ struct Node
 	Node(char c, Node *l, Node *r): isnum(0), op(c), L(l), R(r) {}
 } *root = nullptr, *calc=nullptr;
 
-Node *build(ll l, ll r, ll lay=1)
+inline ll pemdas(char c)
 {
-	//for (ll i=0; i<lay; ++i) printf("|   "); printf("building %d..%d: \"", l, r);
-	//for (ll i=l; i<=r; ++i) printf("%c", inp[i]); printf("\"\n");
+	if (c == '+' || c == '-') return 1;
+	if (c == '*' || c == '/') return 2;
+	if (c == '^') return 3;
+	return 0;	// FIX: return something or it will undefined behavior
+}
 
-	//while (inp[l] == '(' && inp[r] == ')') ++l, --r;	// FIX: cannot just assume begin and end w/ parens means whole thing is parens
+Node *build(const ll l, const ll r)
+{
+
 	ll paren = 0;
-	//printf("    nvm  %d..%d\n", l, r);
-	for (ll i=r; i>=l; --i)
-	{
-		if (inp[i] == ')') ++paren;
-		if (inp[i] == '(') --paren;
-		//printf("c %c @ %d paren %d\n", inp[i], i, paren);
-		if (!paren && (inp[i] == '+' || inp[i] == '-'))
-			return new Node(inp[i], build(l, i-1, lay+1), build(i+1, r, lay+1));
-	}
-
-	//paren=0; // TODO: shouldn't be needed
-	for (ll i=r; i>=l; --i)
-	{
-		if (inp[i] == ')') ++paren;
-		if (inp[i] == '(') --paren;
-		if (!paren && (inp[i] == '*' || inp[i] == '/'))
-			return new Node(inp[i], build(l, i-1, lay+1), build(i+1, r, lay+1));
-	}
-
-	for (ll i=r; i>=l; --i)
-	{
-		if (inp[i] == ')') ++paren;
-		if (inp[i] == '(') --paren;
-		//printf("c %c @ %d paren %d\n", inp[i], i, paren);
-		if (!paren && inp[i] == '^') // FIX: logic--higher order of ops means it should be grouped closer which means parsed later
-			return new Node(inp[i], build(l, i-1, lay+1), build(i+1, r, lay+1));
-	}
+	for (ll t=1; t<=3; ++t)
+		for (ll i=r; i>=l; --i)
+		{
+			if (inp[i] == ')') ++paren;
+			if (inp[i] == '(') --paren;
+			if (!paren && pemdas(inp[i]) == t)
+				return new Node(inp[i], build(l, i-1), build(i+1, r));
+		}
 
 	if (inp[l] == '(' && inp[r] == ')')
-		return build(l+1, r-1, lay);	// FIX: deal with full parens wrap by checking if op exists first
+		return build(l+1, r-1);	// FIX: deal with full parens wrap by checking if op exists first
 
 	ll d = 0;
 	for (ll i=l; i<=r; ++i)
 		d = d*10 + inp[i]-'0';
-	//printf("d = %lld\n", d);
 	return new Node(d);
 }
 
-void print(Node *&cur, ll lay=1)
+void print(Node *&cur)
 {
 	if (cur->isnum) printf("%lld", cur->val);
 	else
 	{
-		if (cur->L->isnum && cur->R->isnum && lay > gonedepth)	// FIX: typo after change--check `!hasgone++` not `hasgone++`
-		//if (cur->L->isnum && cur->R->isnum)
-		{
-			//printf("setting calc at op %c\n", cur->op);
-			gonedepth = lay;
+		if (cur->L->isnum && cur->R->isnum && !gone++)	// FIX: typo after change--check `!hasgone++` not `hasgone++`
 			calc = cur;
-		}
-		print(cur->L, lay);
-		printf(" ");
-		print(cur->R, lay);
-		printf(" %c", cur->op);
+		print(cur->L); printf(" ");
+		print(cur->R); printf(" %c", cur->op);
 	}
 }
 
 int main()
 {
-	scanf("%s%n", inp, &N);	// TODO: input hecka sketch, invis characters messes up %n count
-	//printf("got %d chars\n", N);
+	scanf("%s%n", inp, &N);
 	root = build(0, N-1);	// FIX: indexing--build takes inclusive so upper is N-1 not N
 	print(root); printf("\n");
-
-	//printf("starting calculation\n");
-	do
+	while (!calc->isnum)
 	{
-		//if (calc) printf("calc = %d : %d %c\n", calc->isnum, calc->val, calc->op);
-		if (!calc->isnum)
-		{
-			ll val, a=calc->L->val, b=calc->R->val;
-			delete calc->L;
-			delete calc->R;
-			if (calc->op == '+') val = a + b;
-			if (calc->op == '-') val = a - b;
-			if (calc->op == '*') val = a * b;
-			if (calc->op == '/') val = a / b;
-			if (calc->op == '^') val = pow(a, b);
-			calc->isnum = 1;
-			calc->val = val;
-		}
-		gonedepth = 0;
+		ll val, a=calc->L->val, b=calc->R->val;
+		delete calc->L;
+		delete calc->R;
+		if (calc->op == '+') val = a + b;
+		if (calc->op == '-') val = a - b;
+		if (calc->op == '*') val = a * b;
+		if (calc->op == '/') val = a / b;
+		if (calc->op == '^') val = pow(a, b);
+		calc->isnum = 1;
+		calc->val = val;
+
+		gone=0;
 		print(root); printf("\n");
-	//} while (!calc->isnum);
-	} while (!calc->isnum);
+	}
 
 	return 0;
 }
