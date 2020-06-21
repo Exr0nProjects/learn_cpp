d8c7d61a66d6115ea2c337f7388f63e08bdd7d71
Exr0n <spotyie@gmail.com>
2020-06-21 12:50:07 -0700

    whats up with test 5

--- problems/contests/dated/2020_06_19/1_cf840d/main_1_cf840d.cpp
+++ problems/contests/dated/2020_06_19/1_cf840d/main_1_cf840d.cpp
@@ -39,21 +39,21 @@
 #define s second
 
 using namespace std;
-const ll MX = 300111;
-ll N, D, Q, tsum[MX<<5], addt[MX<<5];
+const int MX = 300111;
+int N, D, Q, tsum[MX<<5], addt[MX<<5];
 int alc=1, lc[MX<<5], rc[MX<<5], rt[MX];
 
-void dump(ll k)
+void dump(int k)
 {
-	ll d = D+1;
-	queue<ll> bfs; bfs.push(k);
-	for (ll i=1; i<1<<1+D; ++i)
+	int d = D+1;
+	queue<int> bfs; bfs.push(k);
+	for (int i=1; i<1<<1+D; ++i)
 	{
 		if (__builtin_popcount(i) == 1) { --d; printf("\n"); }
 		k = bfs.front(); bfs.pop();
 		bfs.push(lc[k]); bfs.push(rc[k]);
 		printf("%3d+%-d @%-2d(%-2d %2d)", tsum[k], addt[k], k, lc[k], rc[k]);	// FIX: typo--print k not i
-		for (ll i=1; i<1<<d; ++i) printf("                ");
+		for (int i=1; i<1<<d; ++i) printf("                ");
 	}
 	printf("\n");
 }
@@ -66,7 +66,7 @@ inline void dupe(int &k)
 	rc[alc] = rc[k];
 	k = alc++;
 }
-inline void apply(ll addv, int &k, int tl, int tr)
+inline void apply(int addv, int &k, int tl, int tr)
 {
 	dupe(k);
 	addt[k] = addv;
@@ -76,60 +76,72 @@ inline void push(int &k, int tl, int tr)
 {
 	dupe(k);
 	if (!addt[k]) return;
-	ll mid = tl + (tr-tl>>1);
+	int mid = tl + (tr-tl>>1);
 	apply(addt[k], lc[k], tl, mid);
 	apply(addt[k], rc[k], mid+1, tr);
 	addt[k] = 0;
 }
 
-void update(int q, ll addv, int &k, int tl=1, int tr=1<<D)
+void update(int q, int addv, int &k, int tl=1, int tr=1<<D)
 {
 	if (q < tl || q > tr) return;
 	if (tl == tr) return apply(addv, k, tl, tr);
-	push(k, tl, tr); ll mid = tl + (tr-tl>>1);
+	push(k, tl, tr); int mid = tl + (tr-tl>>1);
 	if (q <= mid) update(q, addv, lc[k], tl, mid);
 	else update(q, addv, rc[k], mid+1, tr);
 	tsum[k] = tsum[lc[k]] + tsum[rc[k]];
 }
-int querykdup(int k1, int k2, ll kdup, int tl=1, int tr=1<<D)
+//int querykdup(int k1, int k2, int kdup, int tl=1, int tr=1<<D)
+//{
+//    //printf("query dup %d: @%d, %d (%d..%d)\n", kdup, k1, k2, tl, tr);
+//    if (tl == tr) return tsum[k2]-tsum[k1] > kdup ? tl : -1;
+//    push(k1, tl, tr); push(k2, tl, tr);
+//    int mid = tl + (tr-tl>>1);
+//    int ret = 1000000; // unsigned so the min overwrites this
+//    //printf("lef %3d   rig %3d     vs kdup = %d\n", tsum[lc[k2]]-tsum[lc[k1]], tsum[rc[k2]]-tsum[rc[k1]], kdup);
+//    if (tsum[lc[k2]]-tsum[lc[k1]] > kdup)
+//        ret = min(ret, querykdup(lc[k1], lc[k2], kdup, tl, mid));
+//    if (tsum[rc[k2]]-tsum[rc[k1]] > kdup)
+//        ret = min(ret, querykdup(rc[k1], rc[k2], kdup, mid+1, tr));
+//    if (ret > 100000) return -1;
+//    return ret;
+//}
+
+unsigned int querykdup(int k1, int k2, int kdup, int tl=1, int tr=1<<D)
 {
-	//printf("query dup %d: @%d, %d (%d..%d)\n", kdup, k1, k2, tl, tr);
-	if (tl == tr) return tsum[k2]-tsum[k1] > kdup ? tl : -1;
+	//printf("query %d %d kdup %d (%d..%d)\n", k1, k2, kdup, tl, tr);
+	if (tsum[k2] - tsum[k1] <= kdup) return -1;
+	if (tl == tr) return tl;
 	push(k1, tl, tr); push(k2, tl, tr);
-	ll mid = tl + (tr-tl>>1);
-	unsigned int ret = -1; // unsigned so the min overwrites this
-	//printf("lef %3d   rig %3d     vs kdup = %d\n", tsum[lc[k2]]-tsum[lc[k1]], tsum[rc[k2]]-tsum[rc[k1]], kdup);
-	if (tsum[lc[k2]]-tsum[lc[k1]] > kdup)
-		ret = min(ret, (unsigned int)querykdup(lc[k1], lc[k2], kdup, tl, mid));
-	if (tsum[rc[k2]]-tsum[rc[k1]] > kdup)
-		ret = min(ret, (unsigned int)querykdup(rc[k1], rc[k2], kdup, mid+1, tr));
-	return ret;
+	int mid = tl + (tr-tl>>1);
+	return min(querykdup(lc[k1], lc[k2], kdup, tl, mid),
+			   querykdup(rc[k1], rc[k2], kdup, mid+1, tr));
 }
 
 int main()
 {
-	scanf("%lld%lld", &N, &Q);
+	scanf("%d%d", &N, &Q);
 	D = log2(N)+1;
 	rt[0] = alc++;
-	for (ll i=1; i<1<<D; ++i)
+	for (int i=1; i<1<<D; ++i)
 	{
 		lc[i] = alc++;
 		rc[i] = alc++;
 	}
 
-	for (ll i=1; i<=N; ++i)
+	for (int i=1; i<=N; ++i)
 	{
-		ll d; scanf("%lld", &d);
+		int d; scanf("%d", &d);
 		rt[i] = rt[i-1];
 		update(d, 1, rt[i]);
 	}
 
-	//for (ll i=1; i<=N; ++i) { printf("\ntree at %d:", i); dump(rt[i]); }
+	//for (int i=1; i<=N; ++i) { printf("\ntree at %d:", i); dump(rt[i]); }
 
-	for (ll i=0; i<Q; ++i)
+	for (int i=0; i<Q; ++i)
 	{
-		ll l, r, k;
-		scanf("%lld%lld%lld", &l, &r, &k);
+		int l, r, k;
+		scanf("%d%d%d", &l, &r, &k);
 		printf("%d\n", querykdup(rt[l-1], rt[r], (r-l+1)/k)); // FIX: r-l+1 not l-r+1 smah
 	}
 
