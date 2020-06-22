1acb7fde18fde450a55b08d7ffd6ebd68b9545fc
Exr0n <spotyie@gmail.com>
2020-06-22 11:40:49 -0700

    submitting rewrite of luogu p3919

--- problems/luogu/P3919/main_P3919.cpp
+++ problems/luogu/P3919/main_P3919.cpp
@@ -1,6 +1,6 @@
 /*
  * Problem P3919 (luogu/P3919)
- * Create time: Thu 18 Jun 2020 @ 09:25 (PDT)
+ * Create time: Mon 22 Jun 2020 @ 11:22 (PDT)
  * Accept time: [!meta:end!]
  *
  */
@@ -39,72 +39,50 @@
 #define s second
 
 using namespace std;
-const ll MX = 1000111;
-ll N, D, M, segt[MX<<4], sett[MX<<4];
-ll alloc=1, lc[MX<<4], rc[MX<<4], rt[MX];
+const ll MX = 100111;
+ll N, D, Q, tsum[MX<<6], sett[MX<<6];
+ll alc=1, lc[MX<<6], rc[MX<<6], rt[MX];
 
 void dump(ll k)
 {
-	return;
 	ll d = D+1;
-	queue<int> bfs;
-	bfs.push(k);
+	queue<ll> bfs; bfs.push(k);
 	for (ll i=1; i<1<<1+D; ++i)
 	{
 		k = bfs.front(); bfs.pop();
-		bfs.push(lc[k]);
-		bfs.push(rc[k]);
-		if (__builtin_popcount(i) == 1) { --d; printf("\n"); }
-		printf("%-2d: %2d =%-2d(%-2d %2d)  ", k, segt[k], sett[k], lc[k], rc[k]);
-		for (int i=1; i<1<<d; ++i) printf("                   ");
+		bfs.push(lc[k]); bfs.push(rc[k]);
+		if (__builtin_popcount(i) == 1) { printf("\n"); --d; }
+		printf("%3d: %-2d+%-2d  ", k, tsum[k], sett[k]);
+		for (ll i=1; i<1<<d; ++i) printf("            ");
 	}
-	printf("\n");
 }
 void dupe(ll &k)
 {
-	segt[alloc] = segt[k];
-	sett[alloc] = sett[k];
-	lc[alloc] = lc[k];
-	rc[alloc] = rc[k];
-	k = alloc++;
+	tsum[alc] = tsum[k];
+	sett[alc] = sett[k];
+	lc[alc] = lc[k];
+	rc[alc] = rc[k];
+	k = alc++;
 }
-
 void apply(ll setv, ll &k, ll tl, ll tr)
 {
+	if (setv < 0) return;
 	dupe(k);
 	sett[k] = setv;
-	segt[k] = setv * (tr-tl+1);
+	tsum[k] = setv * (tr-tl+1);
 }
 void push(ll &k, ll tl, ll tr)
 {
 	dupe(k);
-	if (sett[k] < 0) return;
 	ll mid = tl + (tr-tl>>1);
 	apply(sett[k], lc[k], tl, mid);
 	apply(sett[k], rc[k], mid+1, tr);
 	sett[k] = -1;
 }
-//void combo(ll k)
-//{
-//    segt[k] = segt[lc[k]] + segt[rc[k]];
-//}
-//
-//void update(ll ql, ll qr, ll setv, ll &k, ll tl=1, ll tr=1<<D)
-//{
-//    if (qr < tl || tr < ql) return;
-//    if (ql <= tl && tr <= qr) return apply(setv, k, tl, tr);
-//    push(k, tl, tr); ll mid = tl + (tr-tl>>1);
-//    update(ql, qr, setv, lc[k], tl, mid);
-//    update(ql, qr, setv, rc[k], mid+1, tr);
-//    combo(k);
-//}
-//ll query(ll ql, ll qr, ll k, ll tl=1, ll tr=1<<D)	// TODO: take k as refrence?
-//{
-//    if (qr < tl || tr < ql) return 0;
-//    if (ql <= tl && tr <= qr) return segt[k];
-//    push(k, tl, tr); ll mid = tl + (tr-tl>>1);
-//    return query(ql, qr, lc[k], tl, mid) + query(ql, qr, rc[k], mid+1, tr);
-//}
+void comb(ll k)
+{
+	tsum[k] = tsum[lc[k]] + tsum[rc[k]];
+}
 
 void update(ll q, ll setv, ll &k, ll tl=1, ll tr=1<<D)
 {
@@ -112,49 +90,44 @@ void update(ll q, ll setv, ll &k, ll tl=1, ll tr=1<<D)
 	if (tl == tr) return apply(setv, k, tl, tr);
 	push(k, tl, tr); ll mid = tl + (tr-tl>>1);
 	if (q <= mid) update(q, setv, lc[k], tl, mid);
-	else          update(q, setv, rc[k], mid+1, tr);
-	segt[k] = segt[lc[k]] + segt[rc[k]];
+	else update(q, setv, rc[k], mid+1, tr);
 }
-ll query(ll q, ll k, ll tl=1, ll tr=1<<D)
+ll query(ll q, ll &k, ll tl=1, ll tr=1<<D)
 {
 	if (q < tl || q > tr) return 0;
-	if (tl == tr) return segt[k];
+	if (tl == tr) return tsum[k];
 	push(k, tl, tr); ll mid = tl + (tr-tl>>1);
 	if (q <= mid) return query(q, lc[k], tl, mid);
-	else          return query(q, rc[k], mid+1, tr);
+	else return query(q, rc[k], mid+1, tr);
 }
 
 int main()
 {
-	scanf("%lld%lld", &N, &M);
-	D = log2(N) +1;
-	memset(sett, -1, sizeof sett);
-
-	rt[0] = alloc++;
-	for (ll i=1; i<1<<D; ++i)
+	scanf("%lld%lld", &N, &Q);
+	D = log2(N)+1;
+	rt[0] = alc++;
+	for (ll i=1; i<=N; ++i)
 	{
-		lc[i] = alloc++;
-		rc[i] = alloc++;
+		lc[i] = alc++;
+		rc[i] = alc++;
 	}
+
 	for (ll i=1; i<=N; ++i)
 	{
 		ll d; scanf("%lld", &d);
 		update(i, d, rt[0]);
 	}
-	for (ll i=1; i<=M; ++i)
+
+	for (ll q=1; q<=Q; ++q)
 	{
-		//dump(rt[i-1]); printf("\n\n$ ");
-		ll t, c, p;	// QUIC noises
-		scanf("%lld%lld%lld", &t, &c, &p);
-		rt[i] = rt[t];
+		//dump(rt[q-1]); printf("\n$ ");
+		ll t, c, p; scanf("%lld%lld%lld", &t, &c, &p);
+		rt[q] = rt[t];
+		if (c == 2) printf("%lld\n", query(p, rt[q]));
 		if (c == 1)
 		{
-			ll v; scanf("%lld", &v);
-			update(p, v, rt[i]);	// FIX: typo--update at p not i (i is loop iter, p was inputted)
-		}
-		if (c == 2)
-		{
-			printf("%lld\n", query(p, rt[t]));
+			ll d; scanf("%lld", &d);
+			update(p, d, rt[q]);
 		}
 	}
 
