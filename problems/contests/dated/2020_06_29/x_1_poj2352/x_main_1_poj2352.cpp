39c71597266349cadfde86e64dfa73091fa6128e
Exr0n <spotyie@gmail.com>
2020-06-30 13:35:23 -0700

    AC contests/dated/2020_06_29/1_poj2352: (script)

---
+++ problems/contests/dated/2020_06_29/x_1_poj2352/x_main_1_poj2352.cpp
@@ -0,0 +1,54 @@
+/*
+ * Problem 1_poj2352 (contests/dated/2020_06_29/1_poj2352)
+ * Create time: Tue 30 Jun 2020 @ 13:23 (PDT)
+ * Accept time: Tue 30 Jun 2020 @ 13:35 (PDT)
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const ll MX = 15111;
+const ll MXN = 32111;
+ll N, bidx[MXN], ans[MX];
+
+void update(ll n, ll v)
+{
+    for (; n<=MXN; n+=n&-n)
+        bidx[n] += v;
+}
+ll query(ll n)
+{
+    ll tot = 0;
+    for (; n; n-=n&-n)
+        tot += bidx[n];
+    return tot;
+}
+
+int main()
+{
+    scanf("%lld", &N);
+    for (ll i=0; i<N; ++i)
+    {
+        ll x, y;
+        scanf("%lld%lld", &x, &y);
+        //printf("query %d got %d\n", x, query(x+1));
+        ++ans[query(x+1)];
+        update(x+1, 1);
+    }
+    for (ll i=0; i<N; ++i)
+        printf("%lld\n", ans[i]);
+
+	return 0;
+}
+
