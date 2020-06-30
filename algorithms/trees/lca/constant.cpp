422d8fb9648be1a973684278276bf0c276bf4880
Exr0n <spotyie@gmail.com>
2020-06-30 14:35:43 -0700

    i think it works now

--- algorithms/trees/lca/constant.cpp
+++ algorithms/trees/lca/constant.cpp
@@ -35,12 +35,16 @@ void dfs(ll cur, ll pre=0, ll dep=1)
             printf("sparse_idx = %d\n", sparse_idx);
         }
 }
-ll sparse_query(ll l, ll r)  // inc l exc r
+ll sparse_query(ll l, ll r)  // inc l inc r
 {
-    ll range = 64-__builtin_clzll(r-l);
+    if (r < l) swap(l, r);
+    ++r; // FIX: fencepost goes here, not outside since we dunno which will end up being bigger
+    ll range = 63-__builtin_clzll(r-l); // FIX: 63- not 64- cuz 1 is 1<<0 not 1<<1
+    printf("range = %d-%d -> %d\n", r, l, range);
     printf("querying min(%d..%d and %d..%d)\n", l, l+(1<<range), r-range, r-(1<<range)+(1<<range));
-    printf("    so min(%d, %d by %d, %d)\n", sparse[range][l].f, sparse[range][l].s, sparse[range][r].f, sparse[range][r].s);
-    return min(sparse[range][l], sparse[range][r-(1<<range)]).f;
+    printf("    so min(%d, %d by %d, %d)\n", sparse[range][l].f, sparse[range][l].s,
+            sparse[range][r-(1<<range)].f, sparse[range][r-(1<<range)].s);
+    return min(sparse[range][l], sparse[range][r-(1<<range)]).s;
 }
 
 void init_lca()
@@ -61,6 +65,8 @@ void init_lca()
         }
         printf("\n");
     }
+    for (ll i=1; i<=N; ++i)
+        printf("%d -> %d\n", i, occ[i]);
 }
 
 int main()
@@ -74,11 +80,25 @@ int main()
         addEdge(v, u);
     }
     init_lca();
-    ll Q; scanf("%lld", &Q);
-    for (ll i=0; i<Q; ++i)
+    printf("n is %d\n", N);
+    for (ll i=1; i<=N; ++i)
     {
-        ll u, v; scanf("%lld%lld", &u, &v);
-        printf("lca of %d and %d = %d\n", u, v, sparse_query(occ[u], occ[v]+1));
+        for (ll j=1; j<=N; ++j)
+            printf("%3d x %3d: %3d\n", i, j, sparse_query(occ[i], occ[j]));
+        printf("\n");
     }
+    //ll Q; scanf("%lld", &Q);
+    //for (ll i=0; i<Q; ++i)
+    //{
+    //    ll u, v; scanf("%lld%lld", &u, &v);
+    //    printf("lca of %d and %d = %d\n", u, v, sparse_query(occ[u], occ[v]+1));
+    //}
 }
 
+/*
+5
+1 2
+1 3
+2 4
+3 5
+*/
