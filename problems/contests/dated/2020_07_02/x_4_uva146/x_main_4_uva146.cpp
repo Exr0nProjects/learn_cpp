e2ba1182ddd8494dfa1a16098a6342aba9aad555
Exr0n <spotyie@gmail.com>
2020-07-06 13:53:34 -0700

    AC contests/dated/2020_07_02/4_uva146: (script)

--- problems/contests/dated/2020_07_02/x_4_uva146/x_main_4_uva146.cpp
+++ problems/contests/dated/2020_07_02/x_4_uva146/x_main_4_uva146.cpp
@@ -1,11 +1,12 @@
 /*
  * Problem 4_uva146 (contests/dated/2020_07_02/4_uva146)
  * Create time: Mon 06 Jul 2020 @ 13:32 (PDT)
- * Accept time: [!meta:end!]
+ * Accept time: Mon 06 Jul 2020 @ 13:53 (PDT)
  *
  */
 
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 using namespace std;
 char buf[60];
@@ -18,18 +19,12 @@ int main()
         N = strlen(buf);
         int i=N-2, j=N-1;
         for (; i>=0 && buf[i]>=buf[i+1]; --i);
-        //printf("%s\n", buf); for (int j=0; j<i; ++j) printf(" "); printf("^\n");
-        //printf("i = %d\n", i);
         if (i<0 && buf[0] >= buf[1])
             printf("No Successor\n");
         else
         {
-            //for (; buf[j] <= buf[i]; --j);
-            //for (int k=0; k<j; ++k) printf(" "); printf("^\n");
+            for (; buf[j] <= buf[i]; --j);
             swap(buf[i], buf[j]);
-            //printf("after swap:\n%s\n", buf);
-            //for (int k=0; k<=i; ++k) printf(" "); printf("^\n");
-            //for (int k=0; k<N; ++k) printf(" "); printf("^\n");
             sort(buf+i+1, buf+N);
             printf("%s\n", buf);
         }
