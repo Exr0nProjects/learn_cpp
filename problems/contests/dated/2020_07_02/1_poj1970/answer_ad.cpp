78c12b00e4df5653dca621fab449ace5df781e34
Exr0n <spotyie@gmail.com>
2020-07-06 20:17:32 -0700

    answer_ad breaks if you don't step backwards

--- problems/contests/dated/2020_07_02/1_poj1970/answer_ad.cpp
+++ problems/contests/dated/2020_07_02/1_poj1970/answer_ad.cpp
@@ -9,6 +9,7 @@
 #include <list>
 using namespace std;
 typedef pair <int, int> pii;
+#define mp make_pair
 #define f first
 #define s second
 const int maxn = 20;
@@ -17,10 +18,11 @@ int a[maxn][maxn];
 
 pair <int, pii> check_vert(int x, int y, int c) {
     pair <int, pii> ans;
-    while (x > 0) {
-        if (a[x - 1][y] == c) x--;
-        else break;
-    }
+    //while (x > 0) {
+    //    if (a[x - 1][y] == c) x--;
+    //    else break;
+    //}
+    if (a[x-1][y] != c) return mp(0, mp(0, 0));
     ans.f = 0;
     ans.s.f = x;
     ans.s.s = y;
@@ -33,10 +35,11 @@ pair <int, pii> check_vert(int x, int y, int c) {
 
 pair <int, pii> check_hori(int x, int y, int c) {
     pair <int, pii> ans;
-    while (y > 0) {
-        if (a[x][y - 1] == c) y--;
-        else break;
-    }
+    //while (y > 0) {
+    //    if (a[x][y - 1] == c) y--;
+    //    else break;
+    //}
+    if (a[x][y-1] != c) return mp(0, mp(0, 0));
     ans.f = 0;
     ans.s.f = x;
     ans.s.s = y;
@@ -49,13 +52,14 @@ pair <int, pii> check_hori(int x, int y, int c) {
 
 pair <int, pii> check_d1(int x, int y, int c) {
     pair <int, pii> ans;
-    while (x > 0 && y > 0) {
-        if (a[x - 1][y - 1] == c) {
-            x--;
-            y--;
-        }
-        else break;
-    }
+    //while (x > 0 && y > 0) {
+    //    if (a[x - 1][y - 1] == c) {
+    //        x--;
+    //        y--;
+    //    }
+    //    else break;
+    //}
+    if (a[x-1][y-1] != c) return mp(0, mp(0, 0));
     ans.f = 0;
     ans.s.f = x;
     ans.s.s = y;
@@ -69,13 +73,14 @@ pair <int, pii> check_d1(int x, int y, int c) {
 
 pair <int, pii> check_d2(int x, int y, int c) {
     pair <int, pii> ans;
-    while (x < n - 1 && y > 0) {
-        if (a[x + 1][y - 1] == c) {
-            x++;
-            y--;
-        }
-        else break;
-    }
+    //while (x < n - 1 && y > 0) {
+    //    if (a[x + 1][y - 1] == c) {
+    //        x++;
+    //        y--;
+    //    }
+    //    else break;
+    //}
+    if (a[x+1][y-1] != c) return mp(0, mp(0, 0));
     ans.f = 0;
     ans.s.f = x;
     ans.s.s = y;
@@ -102,24 +107,24 @@ int main() {
             for (int j = 0; j < 19; j++) {
                 if (!a[i][j]) continue;
                 pair <int, pii> v = check_vert(i, j, a[i][j]);
+                pair <int, pii> h = check_hori(i, j, a[i][j]);
+                pair <int, pii> d1 = check_d1(i, j, a[i][j]);
+                pair <int, pii> d2 = check_d2(i, j, a[i][j]);
                 if (v.f == 5) {
                     is_ans = true;
                     printf("%d\n%d %d\n", a[i][j], v.s.f + 1, v.s.s + 1);
                     break;
                 }
-                pair <int, pii> h = check_hori(i, j, a[i][j]);
                 if (h.f == 5) {
                     is_ans = true;
                     printf("%d\n%d %d\n", a[i][j], h.s.f + 1, h.s.s + 1);
                     break;
                 }
-                pair <int, pii> d1 = check_d1(i, j, a[i][j]);
                 if (d1.f == 5) {
                     is_ans = true;
                     printf("%d\n%d %d\n", a[i][j], d1.s.f + 1, d1.s.s + 1);
                     break;
                 }
-                pair <int, pii> d2 = check_d2(i, j, a[i][j]);
                 if (d2.f == 5) {
                     is_ans = true;
                     printf("%d\n%d %d\n", a[i][j], d2.s.f + 1, d2.s.s + 1);
