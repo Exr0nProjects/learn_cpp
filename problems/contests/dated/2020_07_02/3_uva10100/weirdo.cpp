9457ffdc08dfd20960d5db37d3b0dbf4e217ad73
Exr0n <spotyie@gmail.com>
2020-07-07 10:23:43 -0700

    wait so now the solve function works???

--- problems/contests/dated/2020_07_02/3_uva10100/weirdo.cpp
+++ problems/contests/dated/2020_07_02/3_uva10100/weirdo.cpp
@@ -74,38 +74,38 @@ int tab[MX][MX], alen, blen;
 //    return tot;
 //}
 
-//int solve()
-//{
-//    //printf("alen %d blen %d\n", alen, blen);
-//    for (int i=1; i<alen; ++i)
-//    {
-//        for (int j=1; j<blen; ++j)
-//        {
-//            //printf("%d %d: %s %s\n", i, j, a[i].c_str(), b[j].c_str());
-//            //if (a[i] == 0) printf("a[i] = 0! i=%d j=%d\n", i, j);
-//            //if (i == 1 && j == 25) printf("a[1] %d b[25] %d\n", a[i], b[j]);
-//            //if (a[i] == b[j] && a[i] >= 0)  // FIX: >= 0 not > 0 cuz first element gets descretized to 0 not 1
-//            if (a[i] == b[j])
-//            {
-//                //printf("%3d %3d same!\n\n", i, j);
-//                tab[i][j] = tab[i-1][j-1] + 1;
-//            }
-//            //tab[i][j] = max(tab[i][j], max(tab[i-1][j], tab[i][j-1]));
-//            else
-//                tab[i][j] = max(tab[i-1][j], tab[i][j-1]);
-//            //printf("%3d", tab[i][j]);
-//        }
-//        //printf("\n");
-//    }
-//    //printf("\n");
-//    //for (int i=1; i<=alen; ++i)
-//    //{
-//    //    for (int j=1; j<=blen; ++j)
-//    //        printf("%3d", tab[i][j]);
-//    //    printf("\n");
-//    //}
-//    return tab[alen-1][blen-1];
-//}
+int solve()
+{
+    //printf("alen %d blen %d\n", alen, blen);
+    for (int i=1; i<alen; ++i)
+    {
+        for (int j=1; j<blen; ++j)
+        {
+            //printf("%d %d: %s %s\n", i, j, a[i].c_str(), b[j].c_str());
+            //if (a[i] == 0) printf("a[i] = 0! i=%d j=%d\n", i, j);
+            //if (i == 1 && j == 25) printf("a[1] %d b[25] %d\n", a[i], b[j]);
+            //if (a[i] == b[j] && a[i] >= 0)  // FIX: >= 0 not > 0 cuz first element gets descretized to 0 not 1
+            if (a[i] == b[j])
+            {
+                //printf("%3d %3d same!\n\n", i, j);
+                tab[i][j] = tab[i-1][j-1] + 1;
+            }
+            //tab[i][j] = max(tab[i][j], max(tab[i-1][j], tab[i][j-1]));
+            else
+                tab[i][j] = max(tab[i-1][j], tab[i][j-1]);
+            //printf("%3d", tab[i][j]);
+        }
+        //printf("\n");
+    }
+    //printf("\n");
+    //for (int i=1; i<=alen; ++i)
+    //{
+    //    for (int j=1; j<=blen; ++j)
+    //        printf("%3d", tab[i][j]);
+    //    printf("\n");
+    //}
+    return tab[alen-1][blen-1];
+}
 
 //void process(string arr[], int &len)
 //{   // TODO: yoinked from answer_ad
@@ -159,23 +159,11 @@ int main()
             if (b[blen][0] == '!') break;
         }
 
-        for (int i=1; i<alen; ++i)
-        {
-            for (int j=1; j<blen; ++j)
-            {
-                if (a[i] == b[j])
-                {
-                    tab[i][j] = tab[i-1][j-1] + 1;
-                }
-                else
-                    tab[i][j] = max(tab[i-1][j], tab[i][j-1]);
-                //printf("%3d", tab[i][j]);
-            }
-            //printf("\n");
-        }
-        printf("%2d. Length of longest match: %d\n", ++T, tab[alen-1][blen-1]);
+        printf("%2d. Length of longest match: %d\n", ++T, solve());
     }
 
 	return 0;
 }
+
+
 
