4600cf133152b8dbc9b3406605ae21708bed2b49
Exr0n <spotyie@gmail.com>
2020-07-09 12:46:55 -0700

    so, maybe this works?

--- algorithms/kmp/attempt2.cpp
+++ algorithms/kmp/attempt2.cpp
@@ -12,11 +12,11 @@ void set_nxt()
     memset(nxt, -1, sizeof pat);
     for (int i=1; i<P; ++i)
     {
-        printf("i = %d, nxt[i-1] = %d\n", i, nxt[i-1]);
+        //printf("i = %d, nxt[i-1] = %d\n", i, nxt[i-1]);
         int j = i-1;
         do {
             j = nxt[j];
-            printf("j = %d\n", j);
+            //printf("j = %d\n", j);
             if (pat[j+1] == pat[i])
             {
                 nxt[i] = j+1;
@@ -26,11 +26,41 @@ void set_nxt()
     }
 }
 
+int count()
+{
+    int tot=0, j=0;
+    //for (int i=0; i<N; ++i, ++j)
+    for (int i=0; i<N; ++i)
+    {
+        printf("i %d j %d", i, j);
+        if (str[i] != pat[j])
+        {
+            printf(" x");
+            j = nxt[j-1], --i;
+        }
+        if (j+1 == P)
+        {
+            printf(" !");
+            ++tot, j=nxt[j];
+        }
+        printf("\n");
+        ++j;
+    }
+    return tot;
+}
+
 int main()
 {
-    scanf("%s", pat);
+    //scanf("%s", pat);
+    //P = strlen(pat);
+    //set_nxt();
+    //for (int i=0; i<P; ++i) printf("%3d", nxt[i]); printf("\n");
+
+    scanf("%s%s", pat, str);
     P = strlen(pat);
+    N = strlen(str);
     set_nxt();
-    for (int i=0; i<P; ++i) printf("%3d", nxt[i]); printf("\n");
+    //for (int i=0; i<P; ++i) printf("%3d", nxt[i]); printf("\n");
+    printf("%d\n", count());
 }
 
