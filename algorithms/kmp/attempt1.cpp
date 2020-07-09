5ab3c894575a96ca1b62990c0c47a257a76e8331
Exr0n <spotyie@gmail.com>
2020-07-09 11:42:11 -0700

    uh oh, do i need to https://gist.github.com/gravitylow/fb595186ce6068537a6e9da6d8b5b96d

--- algorithms/kmp/attempt1.cpp
+++ algorithms/kmp/attempt1.cpp
@@ -35,17 +35,47 @@ void set_nxt()
             }
             j = nxt[j];
         } while (j >= 0);
+        //for (; ; j=nxt[j])
+        //{
+        //    printf("    j = %d\n", j);
+        //    if (pat[j+1] == pat[i])
+        //    {
+        //        nxt[i] = j+1;
+        //        break;
+        //    }
+        //}
     }
 }
 
+int count()
+{
+    int tot=0, j=0;
+    for (int i=0; i<N; ++i)
+    {
+        printf("i %d j %d\n", i, j);
+        if (j+1 == P) ++tot, j=nxt[j];
+        if (pat[j] == str[i])
+            ++j;
+        else    // TODO: autofail trying to match null byte when end of pattern reached
+            //j = nxt[j] +1;
+            j = nxt[j] +1, --i;
+    }
+    return tot;
+}
 
 int main()
 {
-    scanf("%s", pat);
-    P = strlen(pat);
-    set_nxt();
-    for (int i=0; i<P; ++i)
-        printf("%d ", nxt[i]);
-    printf("\n");
+    int cs; scanf("%d", &cs);
+    while (cs--)
+    {
+        scanf("%s%s", pat, str);
+        P = strlen(pat);
+        N = strlen(str);
+        set_nxt();
+        for (int i=0; i<P; ++i)
+            printf("%d ", nxt[i]);
+        printf("\n");
+        printf("%d\n", count());
+    }
 }
 
