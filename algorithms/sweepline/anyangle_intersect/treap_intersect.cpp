6f813d9404c22760fc3aa798260f41573fd23d14
Exr0n <spotyie@gmail.com>
2020-07-13 11:57:05 -0700

    treap dynamic sorting seems to work with insert..
    3
    2 1 6 5
    8 1 4 5
    8 4 5 2

--- algorithms/sweepline/anyangle_intersect/treap_intersect.cpp
+++ algorithms/sweepline/anyangle_intersect/treap_intersect.cpp
@@ -2,21 +2,27 @@
 // don't allow concurrent lines or vertical lines
 // optimiztion: use next/prev pointers instead of bound function to check neighbors in constant time
 
+#include <queue>
 #include <cstdio>
 #include <cstdlib>
 #include <utility>
 #include <algorithm>
 #define x first
 #define y second
+#define id1 y.x
+#define id2 y.y
+#define mp make_pair
 #define dl double
 using namespace std;
 
 typedef pair<pair<dl, dl>, pair<dl, dl> > Seg;
+typedef pair<pair<dl, int>, pair<int, int> > Event; // x-pos, type{0: new, 1: cross, 2: remove}, {id, 0 for event 0,2; id, id}
 const int MX = 1000111;
 int N;
 Seg segs[MX];
 dl slopes[MX];
-dl sweep;   // x pos of the sweep line
+dl sweep = 0;   // x pos of the sweep line
+priority_queue<Event, deque<Event>, greater<Event> > events;
 
 bool cmp(int lhs, int rhs)
 {
@@ -35,7 +41,6 @@ struct Node
 #define BLACK "\x1b[38;5;239m"
 void dump(Node *cur, int lay=1, long long lbar=0, long long rbar=0)
 {
-	return;
 	if (lay == 1) printf("dump:\n");
 	if (!cur) return;
 	dump(cur->c[1], lay+1);
@@ -112,12 +117,31 @@ int main()
     {
         scanf("%lf%lf%lf%lf", &segs[i].x.x, &segs[i].x.y, &segs[i].y.x, &segs[i].y.y);
         if (segs[i].y < segs[i].x) swap(segs[i].x, segs[i].y);
-        slopes[i] = (segs[i].x.y-segs[i].y.y)/(segs[i].x.x-segs[i].y.x);
     }
     sort(segs, segs+N);
     for (int i=0; i<N; ++i)
     {
+        slopes[i] = (segs[i].x.y-segs[i].y.y)/(segs[i].x.x-segs[i].y.x);
         printf("line %d: %lf,%lf .. %lf,%lf with slope %lf\n", i, segs[i].x.x, segs[i].x.y, segs[i].y.x, segs[i].y.y, slopes[i]);
+        events.push(mp(mp(segs[i].x.x, 0), mp(i, i)));
+    }
+    while (!events.empty())
+    {
+        Event cur = events.top(); events.pop();
+        sweep = cur.x.x;
+        if (cur.x.y == 0)
+        {
+            insert(root, cur.id1);
+        }
+        if (cur.x.y == 1)
+        {
+            // TODO: swap and check neighbors
+        }
+        if (cur.x.y == 2)
+        {
+            // TODO: remove and check neighbors
+        }
+        dump(root);
     }
 }
 
