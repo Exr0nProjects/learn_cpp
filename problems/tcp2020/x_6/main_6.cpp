f115151ec7a268915d1ac2c79056f4819f0aed88
Exr0n <spotyie@gmail.com>
2020-07-19 09:27:48 -0700

    AC tcp2020/6: (script)

---
+++ problems/tcp2020/x_6/main_6.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem 6 (tcp2020/6)
+ * Create time: Sun 19 Jul 2020 @ 09:22 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+#define lr tl, (tl+((tr-tl)>>1))
+#define rr (tl+((tr-tl)>>1)+1), tr
+
+using namespace std;
+const ll MX = -1;
+
+int main()
+{
+    int head, feet;
+    scanf("%d%d", &head, &feet);
+    int rabbit = feet/2 - head;
+    printf("%d\n%d\n", head-rabbit, rabbit);
+
+	return 0;
+}
+
