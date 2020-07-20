0ac364d19077959b385a0d8239bc02cf8d8cca2b
Exr0n <spotyie@gmail.com>
2020-07-20 11:21:29 -0700

    memory optimization

--- algorithms/linear_sieve/attempt1.cpp
+++ algorithms/linear_sieve/attempt1.cpp
@@ -3,20 +3,20 @@
 
 #include <cstdio>
 const int MX = 10e6 + 100;
-int prime[MX], pcnt=0, nums[MX], totient[MX];
+int prime[MX], pcnt=0, nums[MX];
 
 void sieve()
 {
     for (int i=2; i<MX; ++i) {
         if (!nums[i]) {
-            totient[i] = i-1;
+            nums[i] = i-1;
             prime[pcnt++] = i;
         }
         for (int j=0; i*prime[j]<MX; ++j) {
-            nums[i*prime[j]] = prime[j];
-            totient[i*prime[j]] = totient[i] * totient[prime[j]];
+            //nums[i*prime[j]] = prime[j];
+            nums[i*prime[j]] = nums[i] * nums[prime[j]];
             if (i%prime[j] == 0) {
-                totient[i*prime[j]] = prime[j]*totient[i];
+                nums[i*prime[j]] = prime[j]*nums[i];
                 break;
             }
         }
@@ -26,6 +26,6 @@ void sieve()
 int main()
 {
     sieve();
-    for (int i=0; i<100; ++i) printf("%3d: %d\n", i, totient[i]);
+    for (int i=0; i<100; ++i) printf("%3d: %d\n", i, nums[i]);
 }
 
