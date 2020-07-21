185577bb31365c7549843253808c8d173142f072
Exr0n <spotyie@gmail.com>
2020-07-20 20:14:30 -0700

    dp[i][j] = less than or equal to j doesn't work, maybe eq to j works?

---
+++ problems/zju-icpc-summer2020c6/f/main_f.cpp
@@ -0,0 +1,109 @@
+/*
+ * Problem f (zju-icpc-summer2020c6/f)
+ * Create time: Mon 20 Jul 2020 @ 19:33 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <iterator>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+#define lr tl, (tl+((tr-tl)>>1))
+#define rr (tl+((tr-tl)>>1)+1), tr
+
+using namespace std;
+const int MX = 1e5+100;
+const int MXK = 20;
+
+int N, K, gift[MX], fav[MX], val[MXK], dps[MXK][MX];
+
+int main()
+{
+    scanf("%d%d", &N, &K);
+    for (int i=0; i<N*K; ++i)
+    {
+        int d; scanf("%d", &d);
+        ++gift[d];
+    }
+    int maxfav = 0;
+    for (int i=0; i<N; ++i)
+    {
+        int d; scanf("%d", &d);
+        ++fav[d];
+        maxfav = max(maxfav, fav[d]);
+    }
+    for (int i=0; i<K; ++i)
+        scanf("%d", &val[i]);
+
+    for (int j=1; j<=maxfav; ++j)
+    {
+        multiset<int> at = {0};
+        dps[1][j] = val[0];
+        for (int i=2; i<=N*K; ++i)
+        {
+            printf("i %d j %d: {", i, j); for (auto pos : at) printf("%3d", pos); printf(" }\n");
+            dps[i][j] = dps[i-1][j];
+            int delta = 0, rem=-1;
+            for (auto pos : at)
+                if (pos+1 < K && val[pos+1]-val[pos] > delta)
+                {
+                    delta = val[pos+1]-val[pos];
+                    rem = pos;
+                }
+            if (i > at.size() && j > at.size() && val[0] > delta)
+            {   // start giving to a new npc
+                at.insert(0);
+                dps[i][j] += val[0];
+            }
+            else
+            {   // give another to the most effective
+                auto it = at.find(rem);
+                if (it == at.end()) continue;
+                at.erase(it);
+                at.insert(rem+1);
+                dps[i][j] += delta;
+            }
+            printf("=> %+3d:  {", delta); for (auto pos : at) printf("%3d", pos); printf(" }\n\n");
+        }
+    }
+
+    for (int i=0; i<20; ++i)
+    {
+        for (int j=0; j<4; ++j)
+            printf("%3d", dps[i][j]);
+        printf("\n");
+    }
+
+	return 0;
+}
+
