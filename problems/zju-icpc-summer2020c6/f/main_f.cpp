7a5e7ad2a3c0654c27c7e60247498f68f9aa066a
Exr0n <spotyie@gmail.com>
2020-07-20 21:48:20 -0700

    swapped array semantics around and now its wa

--- problems/zju-icpc-summer2020c6/f/main_f.cpp
+++ problems/zju-icpc-summer2020c6/f/main_f.cpp
@@ -14,7 +14,7 @@ using namespace std;
 const int MX = 1e5+100;
 const int MXK = 20;
 
-int N, K, gift[MX], fav[MX], val[MXK], dps[MXK][5100];
+int N, K, gift[MX], fav[MX], val[MXK], dps[5100][MXK];
 
 int main()
 {
@@ -32,16 +32,16 @@ int main()
     for (int i=1; i<=K; ++i)
         scanf("%d", &val[i]);
 
-    for (int j=1; j<=N; ++j)
-        for (int i=0; i<=N*K; ++i)
-            for (int k=0; k<=K && k<=i; ++k)
-                dps[i][j] = max(dps[i][j], val[k] + dps[i-k][j-1]);
+    for (int i=1; i<=N; ++i)
+        for (int j=0; j<=N*K; ++j)
+            for (int k=0; k<=K && k<=j; ++k)
+                dps[i][j] = max(dps[i][j], val[k] + dps[i-1][j-k]);
 
     int sum=0;
     for (int f=0; f<=1e5+10; ++f)
     {
         //printf("fav %d: %d, %d => %d\n", f, gift[f], fav[f], dps[gift[f]][fav[f]]);
-        sum += dps[gift[f]][fav[f]];
+        sum += dps[fav[f]][gift[f]];
     }
     printf("%d\n", sum);
 
