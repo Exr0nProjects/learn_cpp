ce8338ea292765a8b02d5fcf6f035f48e9b66889
Exr0n <spotyie@gmail.com>
2020-07-20 20:35:57 -0700

    works for samples..

--- problems/zju-icpc-summer2020c6/f/main_f.cpp
+++ problems/zju-icpc-summer2020c6/f/main_f.cpp
@@ -46,6 +46,7 @@ const int MX = 1e5+100;
 const int MXK = 20;
 
 int N, K, gift[MX], fav[MX], val[MXK], dps[MXK][MX];
+set<int> favs;
 
 int main()
 {
@@ -60,6 +61,7 @@ int main()
     {
         int d; scanf("%d", &d);
         ++fav[d];
+        favs.insert(d);
         maxfav = max(maxfav, fav[d]);
     }
     for (int i=0; i<K; ++i)
@@ -67,11 +69,12 @@ int main()
 
     for (int j=1; j<=maxfav; ++j)
     {
-        multiset<int> at = {0};
-        dps[1][j] = val[0];
-        for (int i=2; i<=N*K; ++i)
+        multiset<int> at;
+        for (int i=0; i<j; ++i) at.insert(0);
+        dps[j][j] = val[0]*j;
+        for (int i=j+1; i<=N*K; ++i)
         {
-            printf("i %d j %d: {", i, j); for (auto pos : at) printf("%3d", pos); printf(" }\n");
+            //printf("i %d j %d: {", i, j); for (auto pos : at) printf("%3d", pos); printf(" }\n");
             dps[i][j] = dps[i-1][j];
             int delta = 0, rem=-1;
             for (auto pos : at)
@@ -93,16 +96,29 @@ int main()
                 at.insert(rem+1);
                 dps[i][j] += delta;
             }
-            printf("=> %+3d:  {", delta); for (auto pos : at) printf("%3d", pos); printf(" }\n\n");
+            //printf("=> %+3d:  {", delta); for (auto pos : at) printf("%3d", pos); printf(" }\n\n");
         }
     }
 
-    for (int i=0; i<20; ++i)
+    // don't have to give something to everyone
+    for (int j=1; j<=maxfav; ++j)
+        for (int i=1; i<=N*K; ++i)
+            dps[i][j] = max(dps[i][j], dps[i][j-1]);
+
+    int sum=0;
+    for (auto f : favs)
     {
-        for (int j=0; j<4; ++j)
-            printf("%3d", dps[i][j]);
-        printf("\n");
+        //printf("fav %d: %d, %d => %d\n", f, gift[f], fav[f], dps[gift[f]][fav[f]]);
+        sum += dps[gift[f]][fav[f]];
     }
+    printf("%d\n", sum);
+
+    //for (int i=0; i<20; ++i)
+    //{
+    //    for (int j=0; j<4; ++j)
+    //        printf("%3d", dps[i][j]);
+    //    printf("\n");
+    //}
 
 	return 0;
 }
