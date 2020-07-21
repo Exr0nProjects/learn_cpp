46ce999495851d883ee7a1754bd4cfc46a90f8d4
Exr0n <spotyie@gmail.com>
2020-07-21 12:20:15 -0700

    now its actually just wrong

--- problems/uva/1626_down/main_1626_down.cpp
+++ problems/uva/1626_down/main_1626_down.cpp
@@ -24,39 +24,8 @@ bool match(int i, int j)
 }
 
 //map<pair<int, int>, int> dps;
-int dps[maxn][maxn];
 int dp[maxn][maxn];
 int from[maxn][maxn];
-int dpp(int l, int r, int lay=1)
-{
-    //for (int i=0; i<lay; ++i) printf("|   "); printf("%d..%d: ", l, r); for (int i=l; i<=r; ++i) printf("%c", S[i]); printf("\n");
-    if (l > r) return 0;
-    if (l == r) return 1;
-    if (dps[l][r]) return dps[l][r];
-
-    int ret = dpp(l, l, lay+1) + dpp(l+1, r, lay+1);
-    int split = l;
-    //for (int i=0; i<lay; ++i) printf("|   "); printf("start %d\n", ret);
-    for (int k=l+1; k<r; ++k)
-    {
-        //for (int i=0; i<lay; ++i) printf("|   "); printf("--\n");
-        int cost = dpp(l, k, lay+1) + dpp(k+1, r, lay+1);
-        if (cost < ret)
-        {
-            //for (int i=0; i<lay; ++i) printf("|   "); printf("min ^^^\n");
-            ret = cost;
-            split = k;
-        }
-    }
-
-    //for (int i=0; i<lay; ++i) printf("|   "); printf("%c == %c ?\n", S[l], S[r]);
-
-    // FIX: need to check if S[l] < S[r] for across bracket match, else it will match )( as a pair
-    if (match(l, r) && dpp(l+1, r-1, lay+1) < ret) ret = dpp(l+1, r-1, lay+1), split=-1;
-    //for (int i=0; i<lay; ++i) printf("|   "); printf("=> %d\n", ret);
-    from[l][r] = split;
-    return dps[l][r] = ret;
-}
 
 void print(int l, int r, int lay=1)
 {
@@ -95,16 +64,28 @@ int main()
     while (cs--)
     {
         memset(from, 0, sizeof from);
-        memset(dps, 0, sizeof dps); // fix: typo--sizeof dps not sizeof from
+        memset(dp, 0, sizeof dp); // FIX: typo--sizeof dp not sizeof from
         //memset(s, 0, sizeof s);
-        //S.clear();
-        cin >> S;
-        //dpp(0, S.size()-1);
-        int N = S.size();
+
+        scanf("\n");
+        int N;
+        for (N=0;; ++N)
+        {
+            S[N] = getchar();
+            if (S[N] == '\n') {
+                S[N] = 0;
+                break;
+            }
+            //printf("%d : %c\n", len, s[len]);
+        }
+        //printf("len %d\n", N);
+        //cin >> S;
+        //int N = S.size();
 
         for (int i=0; i<=maxn; ++i) dp[i][i] = 1;
-        for (int len=1; len<N; ++len)
-            for (int l=0, r=l+len; r<N; ++l, ++r)
+
+        for (int ln=1; ln<N; ++ln)
+            for (int l=0, r=l+ln; r<N; ++l, ++r)
             {
                 int ret=dp[l][l]+dp[l+1][r], split = l;
                 for (int k=l+1; k<r; ++k)
@@ -119,24 +100,11 @@ int main()
         //for (int i=0; i<20; ++i) { for (int j=0; j<20; ++j) dps[i][j] ? printf("%3d", dps[i][j]) : printf("  ."); printf("\n"); } printf("\n");
         //for (int i=0; i<20; ++i) { for (int j=0; j<20; ++j) dp[i][j] ? printf("%3d", dp[i][j]) : printf("  ."); printf("\n"); }
 
-        //scanf("\n");
-        //int len;
-        //for (len=0;; ++len)
-        //{
-        //    s[len] = getchar();
-        //    if (s[len] == '\n') {
-        //        s[len] = 0;
-        //        break;
-        //    }
-        //    //printf("%d : %c\n", len, s[len]);
-        //}
-        ////printf("len %d\n", len);
-        //dp(0, len-1);
 
         //cout << "min len = " << dp(0, s.size()-1) << endl;
         //if (g) printf("\n"); g=1;
-        print(0, S.size()-1);
-        //print(0, len-1);
+        //print(0, S.size()-1);
+        print(0, N-1);
         printf("\n");
         if (cs) printf("\n");
     }
