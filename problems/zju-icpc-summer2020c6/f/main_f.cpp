674e2d3a948d803680589ff1a0d6a12af2293017
Exr0n <spotyie@gmail.com>
2020-07-20 21:23:19 -0700

    wa :/

--- problems/zju-icpc-summer2020c6/f/main_f.cpp
+++ problems/zju-icpc-summer2020c6/f/main_f.cpp
@@ -37,35 +37,40 @@ int main()
     for (int i=0; i<K; ++i)
         scanf("%d", &val[i]);
 
-    for (int j=1; j<=maxfav; ++j)
-    {
-        at.clear();
-        for (int i=0; i<j; ++i) at.insert(0);
-        dps[j][j] = val[0]*j;
-        for (int i=j+1; i<=N*K; ++i)
-        {
-            //printf("i %d j %d: {", i, j); for (auto pos : at) printf("%3d", pos); printf(" }\n");
-            dps[i][j] = dps[i-1][j];
-            int delta = 0, rem=-1;
-            for (auto pos : at)
-                if (pos+1 < K && val[pos+1]-val[pos] > delta)
-                {
-                    delta = val[pos+1]-val[pos];
-                    rem = pos;
-                }
-            auto it = at.find(rem);
-            if (it == at.end()) continue;
-            at.erase(it);
-            at.insert(rem+1);
-            dps[i][j] += delta;
-            //printf("=> %+3d:  {", delta); for (auto pos : at) printf("%3d", pos); printf(" }\n\n");
-        }
-    }
+    for (int j=1; j<=N; ++j)
+        for (int i=0; i<=N*K; ++i)
+            for (int k=0; k<=K && k<=i; ++k)
+                dps[i][j] = max(dps[i][j], val[k] + dps[i-1][j-k]);
+
+    //for (int j=1; j<=maxfav; ++j)
+    //{
+    //    at.clear();
+    //    for (int i=0; i<j; ++i) at.insert(0);
+    //    dps[j][j] = val[0]*j;
+    //    for (int i=j+1; i<=N*K; ++i)
+    //    {
+    //        //printf("i %d j %d: {", i, j); for (auto pos : at) printf("%3d", pos); printf(" }\n");
+    //        dps[i][j] = dps[i-1][j];
+    //        int delta = 0, rem=-1;
+    //        for (auto pos : at)
+    //            if (pos+1 < K && val[pos+1]-val[pos] > delta)
+    //            {
+    //                delta = val[pos+1]-val[pos];
+    //                rem = pos;
+    //            }
+    //        auto it = at.find(rem);
+    //        if (it == at.end()) continue;
+    //        at.erase(it);
+    //        at.insert(rem+1);
+    //        dps[i][j] += delta;
+    //        //printf("=> %+3d:  {", delta); for (auto pos : at) printf("%3d", pos); printf(" }\n\n");
+    //    }
+    //}
 
     // don't have to give something to everyone
-    for (int j=1; j<=maxfav; ++j)
-        for (int i=1; i<=N*K; ++i)
-            dps[i][j] = max(dps[i][j], dps[i][j-1]);
+    //for (int j=1; j<=maxfav; ++j)
+    //    for (int i=1; i<=N*K; ++i)
+    //        dps[i][j] = max(dps[i][j], dps[i][j-1]);
 
     int sum=0;
     //for (auto f : favs)
