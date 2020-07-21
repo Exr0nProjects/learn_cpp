6d009a5097f24c63af9c0d662e52637acae4d2e8
Exr0n <spotyie@gmail.com>
2020-07-20 21:43:37 -0700

    still re on 3

--- problems/zju-icpc-summer2020c6/f/main_f.cpp
+++ problems/zju-icpc-summer2020c6/f/main_f.cpp
@@ -11,12 +11,10 @@
 #include <set>
 #include <algorithm>
 using namespace std;
-const int MX = 1e4+100;
+const int MX = 1e5+100;
 const int MXK = 20;
 
-int N, K, gift[MX], fav[MX], val[MXK], dps[MXK][MX];
-set<int> favs;
-multiset<int> at;
+int N, K, gift[MX], fav[MX], val[MXK], dps[MXK][5100];
 
 int main()
 {
@@ -26,58 +24,24 @@ int main()
         int d; scanf("%d", &d);
         ++gift[d];
     }
-    int maxfav = 0;
     for (int i=0; i<N; ++i)
     {
         int d; scanf("%d", &d);
         ++fav[d];
-        favs.insert(d);
-        maxfav = max(maxfav, fav[d]);
     }
-    for (int i=0; i<K; ++i)
+    for (int i=1; i<=K; ++i)
         scanf("%d", &val[i]);
 
     for (int j=1; j<=N; ++j)
         for (int i=0; i<=N*K; ++i)
             for (int k=0; k<=K && k<=i; ++k)
-                dps[i][j] = max(dps[i][j], val[k] + dps[i-1][j-k]);
-
-    //for (int j=1; j<=maxfav; ++j)
-    //{
-    //    at.clear();
-    //    for (int i=0; i<j; ++i) at.insert(0);
-    //    dps[j][j] = val[0]*j;
-    //    for (int i=j+1; i<=N*K; ++i)
-    //    {
-    //        //printf("i %d j %d: {", i, j); for (auto pos : at) printf("%3d", pos); printf(" }\n");
-    //        dps[i][j] = dps[i-1][j];
-    //        int delta = 0, rem=-1;
-    //        for (auto pos : at)
-    //            if (pos+1 < K && val[pos+1]-val[pos] > delta)
-    //            {
-    //                delta = val[pos+1]-val[pos];
-    //                rem = pos;
-    //            }
-    //        auto it = at.find(rem);
-    //        if (it == at.end()) continue;
-    //        at.erase(it);
-    //        at.insert(rem+1);
-    //        dps[i][j] += delta;
-    //        //printf("=> %+3d:  {", delta); for (auto pos : at) printf("%3d", pos); printf(" }\n\n");
-    //    }
-    //}
-
-    // don't have to give something to everyone
-    //for (int j=1; j<=maxfav; ++j)
-    //    for (int i=1; i<=N*K; ++i)
-    //        dps[i][j] = max(dps[i][j], dps[i][j-1]);
+                dps[i][j] = max(dps[i][j], val[k] + dps[i-k][j-1]);
 
     int sum=0;
-    //for (auto f : favs)
-    for (multiset<int>::iterator f=favs.begin(); f!=favs.end(); ++f)
+    for (int f=0; f<=1e5+10; ++f)
     {
         //printf("fav %d: %d, %d => %d\n", f, gift[f], fav[f], dps[gift[f]][fav[f]]);
-        sum += dps[gift[*f]][fav[*f]];
+        sum += dps[gift[f]][fav[f]];
     }
     printf("%d\n", sum);
 
