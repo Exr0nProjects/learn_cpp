138abf3a40198c623d80ddd5e80b542845cc85fd
Exr0n <spotyie@gmail.com>
2020-07-22 13:27:55 -0700

    swap equ, still wa

--- problems/uva/pid/1218down/main_1218down.cpp
+++ problems/uva/pid/1218down/main_1218down.cpp
@@ -39,18 +39,21 @@ int N, dp[MX][5];   // 0 = server, 1 = served, 2 = not served
 void op(int cur, int pre=0, int lay=1)
 {
     //for (int i=0; i<lay; ++i) printf("|   "); printf("%d from %d\n", cur, pre);
-    int sum1=0, max0=-1e6, sum2=1;  // FIX: max0 = 0 doesn't work, max might be negative
+    //int sum1=0, max0=-1e6, sum2=1;  // FIX: max0 = 0 doesn't work, max might be negative
+    int sum1=0, max0=1e6, sum2=1;  // FIX: max0 = 0 doesn't work, max might be negative
     for (int nxt : head[cur]) if (nxt != pre)
     {
         op(nxt, cur, lay+1);
         sum1 += dp[nxt][1];
-        max0 = max(max0, dp[nxt][1]-dp[nxt][0]);
+        //max0 = max(max0, dp[nxt][1]-dp[nxt][0]);
+        max0 = min(max0, dp[nxt][0]-dp[nxt][1]);
         //sum2 += min(dp[nxt][2], dp[nxt][0]);
         sum2 += dp[nxt][2];
     }
     dp[cur][0] = min((int)2e6, sum2);
     //for (int i=0; i<lay; ++i) printf("|   "); printf("sum %d sub %d\n", sum1, max0);
-    dp[cur][1] = sum1-max0;
+    //dp[cur][1] = sum1-max0;
+    dp[cur][1] = sum1+max0;
     if (head[cur].size() == 1 && pre) dp[cur][1] = 2e6;
     dp[cur][2] = min((int)2e6, sum1);
     //for (int i=0; i<lay; ++i) printf("|   "); printf("=> %d %d %d\n", dp[cur][0], dp[cur][1], dp[cur][2]);
@@ -69,8 +72,8 @@ int main()
             {
                 int u, v; scanf("%d%d", &u, &v);
                 //printf("connecting %d and %d\n", u, v);
-                head[u].pb(v);
-                head[v].pb(u);
+                head[u].push_back(v);
+                head[v].push_back(u);
             }
             op(1);
             //printf("dp 1,0 %d  ;  1,1 %d\n", dp[1][0], dp[1][1]);
