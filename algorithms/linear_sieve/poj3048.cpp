d70ebe58c7195fc56e7a95694aa9d2838aaeeac6
Exr0n <spotyie@gmail.com>
2020-07-23 10:48:23 -0700

    still wa, i guess its diffing time

--- algorithms/linear_sieve/poj3048.cpp
+++ algorithms/linear_sieve/poj3048.cpp
@@ -1,19 +1,29 @@
 // 16 July 2020
 
 #include <cstdio>
+#include <algorithm>
+using namespace std;
 const int MX = 2e4 + 100;
-int N, sieve[MX];
+int N, sieve[MX], pri[MX], pcnt=0;
 int main()
 {
+    //for (int i=2; i<MX; ++i)
+    //    if (!sieve[i])
+    //        for (int j=1; i*j<MX; ++j)
+    //            sieve[i*j] = i;
     for (int i=2; i<MX; ++i)
     {
         if (!sieve[i])
-            for (int j=1; i*j<MX; ++j)
-                sieve[i*j] = i;
+            pri[pcnt++] = sieve[i] = i;
+        for (int j=0; j<pcnt && i*pri[j]<MX; ++j)
+        {
+            //printf("%d: max(%d %d) = %d\n", i*pri[j], pri[j], sieve[i], max(pri[j], sieve[i]));
+            sieve[i*pri[j]] = max(pri[j], sieve[i]);
+            if (i%pri[j] == 0) break;
+        }
         //printf("%3d: %3d\n", i, sieve[i]);
     }
-    //scanf("%d", &N);
-    //for (int i=0; i<N; ++i) printf("%3d\n", sieve[i]); return 0;
+    //scanf("%d", &N); for (int i=0; i<N; ++i) printf("%3d\n", sieve[i]); return 0;
     ////printf("n = %d\n", N);
     //int ret=0;  // FIX: can't assume 2 is the smallest, maybe its just one cow labeled 1
     //for (int i=0; i<N; ++i)
