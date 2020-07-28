2b782ff53126603ec79517497d4d4747b8a23f4d
Exr0n <spotyie@gmail.com>
2020-07-28 11:31:43 -0700

    fine i'll use a bfs

---
+++ problems/contests/teamscode/2020summer/tc2020/13/main_13.cpp
@@ -0,0 +1,87 @@
+/*
+ * Problem 13 (contests/teamscode/2020summer/tc2020/13)
+ * Create time: Tue 28 Jul 2020 @ 11:12 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <iterator>
+#include <exception>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+#define lr tl, (tl+((tr-tl)>>1))
+#define rr (tl+((tr-tl)>>1)+1), tr
+
+using namespace std;
+const ll MX = 40;
+
+int N, T, si, sj, tot=0;
+char grid[MX][MX];
+
+void dfs(int i, int j, int r=T)
+{
+    printf("    i %d j %d -> '%c'  r %d\n", i, j, grid[i][j], r);
+    if (grid[i][j] != '*') return;
+    grid[i][j] = 'X';
+    if (!~r) { printf("got %d %d\n", i, j); ++tot; }    // FIX: move until -1 not move until 0
+    else
+    {
+        int dx[] = {1, 0, -1, 0};
+        int dy[] = {0, 1, 0, -1};
+        for (int d=0; d<4; ++d)
+            dfs(i+dy[d], j+dx[d], r-1);
+    }
+}
+
+int main()
+{
+    memset(grid, '#', sizeof grid);
+    scanf("%d%d", &N, &T);
+    for (int i=1; i<=N; ++i)
+    {
+        getchar();
+        for (int j=1; j<=N; ++j)
+        {
+            grid[i][j] = getchar();
+            if (grid[i][j] == 'Z')
+                si = i, sj = j, grid[i][j] = '*';   // FIX: set start pos to legit
+        }
+    }
+
+    for (int i=0; i<=1+N; ++i) { for (int j=0; j<=1+N; ++j) printf("%c", grid[i][j]); printf("\n"); }
+    dfs(si, sj);
+    for (int i=0; i<=1+N; ++i) { for (int j=0; j<=1+N; ++j) printf("%c", grid[i][j]); printf("\n"); }
+    printf("%d\n", tot);
+
+	return 0;
+}
+
