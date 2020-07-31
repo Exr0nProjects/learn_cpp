998e2df502b4ef691c262d4bdec8d5f47fe5cd18
Exr0n <spotyie@gmail.com>
2020-07-31 15:34:17 -0700

    clean up fibtree

--- problems/contests/zju-icpc/2020summer3/x_A-fibtree/x_main_A-fibtree.cpp
+++ problems/contests/zju-icpc/2020summer3/x_A-fibtree/x_main_A-fibtree.cpp
@@ -8,38 +8,13 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <sstream>
-#include <iostream>
-#include <tuple>
-#include <vector>
-#include <string>
-#include <list>
-#include <array>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <cmath>
-#include <random>
-#include <chrono>
 #include <utility>
-#include <iterator>
-#include <exception>
-#include <algorithm>
-#include <functional>
+#include <vector>
 
-#define ll long long
-#define dl double
-
-#define pb push_back
-#define eb emplace_back
 #define mp make_pair
+#define pb push_back
 #define f first
 #define s second
-#define lr tl, (tl+((tr-tl)>>1))
-#define rr (tl+((tr-tl)>>1)+1), tr
 
 using namespace std;
 const int MX = 100111;
