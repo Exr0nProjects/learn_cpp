46d7094660a38f0dc67085bd76738d25ddc7bab6
Exr0n <spotyie@gmail.com>
2020-07-31 11:45:41 -0700

    ok run error on 24? wa on 1?

--- problems/contests/zju-icpc/2020summer3/C-more-and-more/main_C-more-and-more.cpp
+++ problems/contests/zju-icpc/2020summer3/C-more-and-more/main_C-more-and-more.cpp
@@ -38,23 +38,26 @@
 
 using namespace std;
 const ll MX = 1e5+1;
-ll N, M, K, val[MX], dp[MX][(int)1e4+1][2];    // dp[i][j][k] = max sum from 1..i but you must take i, j muliplies used, k = whether we are taking this? or not.
+const ll MXM = 50;
+ll N, M, K, val[MX], dp[MXM][4];    // dp[i][j][k] = max sum from 1..i but you must take i, j muliplies used, k = whether we are taking this? or not.. rolling array on i
+// FIX: hold up, M <= 30? not 1e4??
 
 int main()
 {
     scanf("%lld%lld%lld", &N, &M, &K);
     for (int i=1; i<=N; ++i) scanf("%lld", &val[i]);
 
-    for (int i=0; i<=M; ++i) dp[0][i][1] = -1e8;
+    for (int i=0; i<=MXM; ++i) dp[i][1] = -1e8;
 
     ll ret = -1e8;
     for (int i=1; i<=N; ++i)
     {
         for (int j=M; ~j; --j)
         {
-            dp[i][j][0] = max(max(dp[i-1][j][0], dp[i-1][j][1]), 0ll) + val[i];
-            if (j) dp[i][j][1] = max(max(dp[i-1][j][1], dp[i-1][j-1][0]), 0ll) + val[i]*K;
-            ret = max(ret, max(dp[i][j][0], dp[i][j][1]));
+            dp[j][0] = max(max(dp[j][0], dp[j][1]), 0ll) + val[i];
+            if (j) dp[j][1] = max(max(dp[j][1], dp[j-1][0]), 0ll) + val[i]*K;
+            else dp[j][1] = -1e8;
+            ret = max(ret, max(dp[j][0], dp[j][1]));
             //printf("(%-2d %2d)  ", dp[i][j][0], dp[i][j][1]);
         }
         //printf("\n");
