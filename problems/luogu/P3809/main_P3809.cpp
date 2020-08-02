9f76febf37e383112f22904a8e54585b7183f57d
Exr0n <spotyie@gmail.com>
2020-08-02 15:08:28 -0700

    smah

--- problems/luogu/P3809/main_P3809.cpp
+++ problems/luogu/P3809/main_P3809.cpp
@@ -20,7 +20,7 @@
 
 using namespace std;
 const ll MX = 1000111;
-int N, sa[MX], rk[MX];
+int N, sa[MX], rk[MX], tmp[MX], pos[MX];
 char inp[MX];
 
 void suffsort()
@@ -28,9 +28,9 @@ void suffsort()
     for (int i=1; i<=N; ++i)
         rk[i] = inp[i], sa[i] = i;
 
-    int tmp[MX], pos[MX], mx=128;
+    int mx=255;
 
-    for (int kk=1; kk<N; kk<<=1)
+    for (int kk=1; kk>>1<N; kk<<=1)
     {
         int k = kk>>1, p = k;    // r = current rerank num and it gets auto-incremented cuz pair[0] != pair[N]
         // copy by second keyword
@@ -39,7 +39,8 @@ void suffsort()
         for (int i=1; i<=N; ++i) if (sa[i] > k) tmp[++p] = sa[i]-k;   // put other elements into tmp but shifted (since they alre already sorted by second keyword from previous step); FIX: ++p not p++; FIX: sa[i]-k not sa[i], cuz u need to shift it over
         //printf("tmp_%d: ", k); for (int i=1; i<=N; ++i) printf("%3d", tmp[i]); printf("\n");
         // radix sort by first keyword
-        memset(pos, 0, sizeof(int)*mx);
+        memset(pos, 0, sizeof(int)*(mx+1)); // FIX: indexing--memset N+1 not mx again!!!
+        //memset(pos, 0, sizeof(pos));
         for (int i=1; i<=N; ++i) ++pos[rk[i]];          // radix sort by rk[tmp[i]] is equiv to sorting by rk[i]
         for (int i=1; i<=mx; ++i) pos[i] += pos[i-1];    // prefix sum on pos to get end positions of each group; FIX: up to mx not N because mx can be larger, on first iteration when its characters up to 127
         for (int i=N; i; --i) sa[pos[ rk[tmp[i]] ]--] = tmp[i];  // stable sort by first keyword, write tmp
@@ -52,6 +53,7 @@ void suffsort()
         //printf("rk_%d:  ", k); for (int i=1; i<=N; ++i) printf("%3d", rk[i]); printf("\n");
         //printf("\n");
         mx = rk[sa[N]];
+        if (mx == N) break;
     }
 }
 
