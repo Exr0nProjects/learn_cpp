18817ff365c005f8faaad171914a5f39a8d4e0c7
Exr0n <spotyie@gmail.com>
2020-08-03 12:26:15 -0700

    altho lcp calc still doesn't work

--- problems/poj/2774/main_2774.cpp
+++ problems/poj/2774/main_2774.cpp
@@ -54,67 +54,19 @@ void ksa()
         for (int i=1; i<=k; ++i) tmp[i] = N-i +1;
         for (int i=1; i<=N; ++i) if (sa[i] > k) tmp[++p] = sa[i]-k;
         // first keyword
-        //memset(pos, 0, 1+mx<<2);    // times 4 for int
         memset(pos, 0, 1+mx<<2);
         for (int i=1; i<=N; ++i) ++pos[rk[i]];
         for (int i=1; i<=mx; ++i) pos[i] += pos[i-1];    // FIX: mx not MX, altho both work; FIX: <=mx not <mx, causes whack bus error?
         for (int i=N; i; --i) sa[pos[ rk[tmp[i]] ]--] = tmp[i];
-        //for (int i=1; i<=N; ++i) printf("%3d", sa[i]); printf("\n");
         // re-rank
         for (int i=1; i<=N; ++i)
             tmp[sa[i]] = tmp[sa[i-1]] + (rk[sa[i]] != rk[sa[i-1]] || rk[sa[i]+k] != rk[sa[i-1]+k]);
-        //printf("%d %d\n", sizeof rk, sizeof tmp);
-        for (int i=1; i<=N; ++i) rk[i] = tmp[i];    // TODO: bus error
-        //for (int i=1; i<=3; ++i) rk[i] = tmp[i];    // works
-        for (int i=1; i<=N; ++i) printf("%3d", rk[i]); printf("\n\n");
+        memcpy(rk, tmp, 1+N<<2);
         // prep
-        printf(" n = %d\n", N);
         mx = rk[sa[N]];
-        printf("mx = %d, n = %d, eq? %d\n", mx, N, mx == N);
         if (mx == N) break;
-        printf("what the hell??\n");
     }
 }
-
-//void ksa()
-//{
-//    int mx = 128;
-//    for (int i=1; i<=N; ++i)
-//        rk[i] = inp[i], sa[i] = i;
-//    //for (int i=1; i<<1<N; i<<=1)
-//    int kk=1;
-//    while (kk*2<N)
-//    {
-//        printf("i = %d\n", kk);
-//        printf("%d\n", N);
-//        int k = kk>>1, p=k;
-//        // second keyword
-//        //for (int i=1; i<=k; ++i) tmp[i] = N-i+1;
-//        for (int i=1; i<=N; ++i) if (sa[i] > k) tmp[++p] = sa[i]-k;
-//        // first keyword
-//        //memset(pos, 0, 1+mx<<2);    // times 4 for int
-//        //for (int i=1; i<=N; ++i) ++pos[rk[i]];
-//        //for (int i=1; i<mx; ++i) pos[i] += pos[i-1];    // FIX: mx not MX, altho both work
-//        for (int i=N; i; --i) sa[pos[ rk[tmp[i]] ]--] = tmp[i];
-//        //for (int i=1; i<=N; ++i) printf("%3d", sa[i]); printf("\n");
-//        // re-rank
-//        for (int i=1; i<=N; ++i)
-//            tmp[sa[i]] = tmp[sa[i-1]] + (rk[sa[i]] != rk[sa[i-1]] || rk[sa[i]+k] != rk[sa[i-1]+k]);
-//        //printf("%d %d\n", sizeof rk, sizeof tmp);
-//        //for (int i=1; i<=N; ++i) rk[i] = tmp[i];    // TODO: bus error
-//        for (int i=1; i<=3; ++i) rk[i] = tmp[i];    // works
-//        for (int i=1; i<=N; ++i) printf("%3d", rk[i]); printf("\n\n");
-//        // prep
-//        printf(" n = %d\n", N);
-//        mx = rk[sa[N]];
-//        printf("mx = %d, n = %d, eq? %d\n", mx, N, mx == N);
-//        if (mx == N) break;
-//        printf("what the hell??\n");
-//        kk <<= 1;
-//        printf("wait but why %d\n", kk*2 < N);
-//        printf("kk %x tmp %x rk %x\n", &kk, tmp, rk);
-//    }
-//}
 
 void klcp()
 {
@@ -135,7 +87,7 @@ int main()
 
     for (int i=1; i<=N; ++i) printf("%3d", i); printf("\n");
     for (int i=1; i<=N; ++i) printf("%3c", inp[i]); printf("\n");
-    for (int i=1; i<=N; ++i) printf("%3d", sa[i]); printf("\n");
+    for (int i=1; i<=N; ++i) printf("%3d", rk[i]); printf("\n");
     for (int i=1; i<=N; ++i) printf("%3d", lcp[rk[i]]); printf("\n");
 
 	return 0;
