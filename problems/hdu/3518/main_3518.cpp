4b87e2a21717d028c5afd9eee16aa397f4401584
Exr0n <spotyie@gmail.com>
2020-08-04 12:21:03 -0700

    bus errrrr

---
+++ problems/hdu/3518/main_3518.cpp
@@ -0,0 +1,92 @@
+/*
+ * Problem 3518 (hdu/3518)
+ * Create time: Tue 04 Aug 2020 @ 12:11 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const ll MX = 1111;
+int N;
+char inp[MX];
+int sa[MX], rk[MX], tmp[MX], pos[MX], lcp[MX];
+
+void ksa()
+{
+    for (int i=1; i<=N; ++i)
+        rk[i] = inp[i], sa[i] = i;
+    int mx = 128;
+    for (int i=1; i>>1<N; i<<=1)
+    {
+        int k = i>>1, p = k;
+        for (int i=1; i<=k; ++i) tmp[i] = N-i+1;
+        for (int i=1; i<=N; ++i) if (sa[i] > k) tmp[++p] = sa[i];
+        memset(pos, 0, 1+mx<<2);
+        for (int i=1; i<=N; ++i) ++pos[rk[i]];
+        for (int i=1; i<=mx; ++i) pos[i] += pos[i-1];
+        for (int i=N; i; --i) sa[pos[ rk[tmp[i]] ]--] = tmp[i];
+        for (int i=1; i<=N; ++i)
+            tmp[sa[i]] = tmp[sa[i-1]] + (rk[sa[i]] == rk[sa[i-1]] && rk[sa[i]+k] == rk[sa[i-1]+k]);
+        memcpy(rk, tmp, 1+N<<2);
+        mx = rk[sa[N]];
+        if (mx==N) break;
+    }
+    for (int i=1; i<=N; ++i)
+    {
+        lcp[rk[i]] = max(lcp[rk[i-1]]-1, 0);
+        if (rk[i] > 1)
+            while (inp[i+lcp[rk[i]]] == inp[sa[rk[i]-1]+lcp[rk[i]]])
+                ++lcp[rk[i]];
+    }
+    for (int i=1; i<=N; ++i) printf("%3d", lcp[i]); printf("\n");
+}
+
+int main()
+{
+    while (true)
+    {
+        scanf("%s", inp+1);
+        if (inp[1] == '#') break;
+        N = strlen(inp+1);
+        ksa();
+        printf("\ni:         "); for (int i=1; i<=N; ++i) printf("%3d", sa[i]);
+        printf("\ninp[i]:    "); for (int i=1; i<=N; ++i) printf("%3d", inp[sa[i]]);
+        printf("\nrk[i]:     "); for (int i=1; i<=N; ++i) printf("%3d", i);
+        printf("\nlcp[rk[i]]:"); for (int i=1; i<=N; ++i) printf("%3d", lcp[i]);
+
+    }
+
+	return 0;
+}
+
