429dc2e3bdfe9aadb2eb844e6b6729c5c2d6b00f
Exr0n <spotyie@gmail.com>
2020-08-04 15:02:46 -0700

    why does short cause problems

--- problems/poj/x_1837/main_1837.cpp
+++ problems/poj/x_1837/main_1837.cpp
@@ -1,13 +1,14 @@
 #include <cstdio>
 #define S 4725
-short C, G, w[20], d[20], dp[2][S*2];
+short C, G; int w[20], d[20];
+int dp[2][S*2];
 int main()
 {
     scanf("%hd%hd", &C, &G);
-    for (short i=1; i<=C; ++i) scanf("%hd", &d[i]);
+    for (short i=1; i<=C; ++i) scanf("%d", &d[i]);
     dp[0][S] = 1;
     for (short i=1; i<=G; ++i) {
-        scanf("%hd", &w[i]);
+        scanf("%d", &w[i]);
         //w[i-1] = 0;   // TODO: why does uncommenting this break it?
         for (short j=-S; j<=S; ++j) {
             dp[i&1][j+S] = 0;
@@ -15,6 +16,6 @@ int main()
                 dp[i&1][j+S] += dp[~i&1][j-d[k]*w[i]+S];
         }
     }
-    printf("%hd\n", dp[G&1][S]);
+    printf("%d\n", dp[G&1][S]);
 }
 
