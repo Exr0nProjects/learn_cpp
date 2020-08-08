e2946adfd20f6a75f9595dc04c69970ba21eabc1
Exr0n <spotyie@gmail.com>
2020-08-08 12:13:17 -0700

    pst seems to work

--- problems/usaco/gold/2019dec/milkvisits/main_milkvisits.cpp
+++ problems/usaco/gold/2019dec/milkvisits/main_milkvisits.cpp
@@ -41,9 +41,54 @@ const ll MX = 131072;
 int N, M, type[MX];
 vector<int> head[MX];
 
-int st[18][MX<<2], occ[MX], stsz=1;  // TODO: might be MX<<1
+int D, rt[MX], alc=1, lc[MX<<6], rc[MX<<6], tsum[MX<<6];
+void dumpseg(int i)
+{
+    printf("seg %d:", i);
+    queue<int> q;
+    q.push(rt[i]);
+    int d = D-1;
+    for (int i=1; i<D<<1; ++i)
+    {
+        int k = q.front(); q.pop();
+        q.push(lc[k]); q.push(rc[k]);
+        if (__builtin_popcount(i) == 1) { d>>=1; printf("\n"); }
+        printf("%3d @ %-2d", tsum[k], k);
+        for (int i=1; i<d; ++i) printf("        ");
+    }
+    printf("\n");
+}
+void dupe(int &k)
+{
+    lc[alc] = lc[k];
+    rc[alc] = rc[k];
+    tsum[alc] = tsum[k];
+    k = alc++;
+}
+void su(int q, int v, int &k, int tl=1, int tr=D) // FIX: args--&k for pst
+{
+    //if (q < tl || q > tr) return;
+    dupe(k);
+    if (tl == tr) { tsum[k] += v; return; }
+    int mid = tl + (tr-tl>>1);
+    if (q <= mid) su(q, v, lc[k], tl, mid);
+    else su(q, v, rc[k], mid+1, tr);
+    //tsum[k] = tsum[lc[k]] + tsum[rc[k]]; // not needed
+}
+int sq(int q, int ku, int kv, int klca, int kslca, int tl=1, int tr=D)
+{
+    if (tl == tr) return tsum[ku] + tsum[kv] - tsum[klca] - tsum[kslca];
+    int mid = tl + (tr-tl>>1);
+    if (q <= mid) return sq(q, lc[ku], lc[kv], lc[klca], lc[kslca], tl, mid);
+    else return sq(q, rc[ku], rc[kv], rc[klca], rc[kslca], mid+1, tr);
+}
+
+int st[18][MX<<2], occ[MX], par[MX], stsz=1;  // TODO: might be MX<<1
 void dfs(int cur, int pre=0)
 {
+    par[cur] = pre;
+    rt[cur] = rt[pre];
+    su(cur, 1, rt[cur]);
     for (int nxt : head[cur])
         if (nxt != pre)
         {
@@ -73,34 +118,8 @@ int rmq(int l, int r)
     if (l > r) swap(l, r);
     ++r;
     int j; for (j=0; 2<<j <= r-l; ++j);
-    printf("j = %d, min(%d,%d;   %d,%d)\n", j, l, st[j][l], r-(1<<j), st[j][r-(1<<j)]);
+    //printf("j = %d, min(%d,%d;   %d,%d)\n", j, l, st[j][l], r-(1<<j), st[j][r-(1<<j)]);
     return min(st[j][l], st[j][r-(1<<j)]);
-}
-
-int D, rt[MX], alc=1, lc[MX<<6], rc[MX<<6], tsum[MX<<6];
-void dupe(int &k)
-{
-    lc[alc] = lc[k];
-    rc[alc] = rc[k];
-    tsum[alc] = tsum[k];
-    k = alc++;
-}
-void su(int q, int v, int k=1, int tl=1, int tr=D)
-{
-    //if (q < tl || q > tr) return;
-    dupe(k);
-    if (tl == tr) { tsum[k] += v; return; }
-    int mid = tl + (tr-tl>>1);
-    if (q <= mid) su(q, v, lc[k], tl, mid);
-    else su(q, v, rc[k], mid+1, tr);
-    //tsum[k] = tsum[lc[k]] + tsum[rc[k]]; // not needed
-}
-int sq(int q, int ku, int kv, int klca, int kslca, int tl=1, int tr=D)
-{
-    if (tl == tr) return tsum[ku] + tsum[kv] - tsum[klca] - tsum[kslca];
-    int mid = tl + (tr-tl>>1);
-    if (q <= mid) return sq(q, lc[ku], lc[kv], lc[klca], lc[kslca], tl, mid);
-    else return sq(q, rc[ku], rc[kv], rc[klca], rc[kslca], mid+1, tr);
 }
 
 int main()
@@ -117,6 +136,7 @@ int main()
     }
 
     klca();
+    for (int i=1; i<=N; ++i) dumpseg(i);
     for (int i=1; i<=M; ++i)
     {
         int u, v, t; scanf("%d%d%d", &u, &v, &t);
