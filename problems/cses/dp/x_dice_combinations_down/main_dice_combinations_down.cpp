a4bfa3cd3e9a0609c38cf11b46fcef2ac9827c76
Exr0n <spotyie@gmail.com>
2020-08-24 16:48:11 -0700

    AC cses/dp/dice_combinations_down: (script)

---
+++ problems/cses/dp/x_dice_combinations_down/main_dice_combinations_down.cpp
@@ -0,0 +1,60 @@
+/*
+ * Problem dice_combinations_down (cses/dp/dice_combinations_down)
+ * Create time: Mon 24 Aug 2020 @ 16:44 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#define ll long long
+#define dl double
+
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+const ll MX = 1e6+10;
+
+int N, dp[MX];
+int kdp(int i)
+{
+    if (!i) return 1;
+    int &ret = dp[i];
+    if (ret) return ret;
+    for (int v=1; v<=6 && v <= i; ++v)
+        ret = (ret + kdp(i-v)) % (int)(1e9+7);  // FIX: forgot mod
+    return ret;
+}
+
+int main()
+{
+    scanf("%d", &N);
+    printf("%d\n", kdp(N));
+
+	return 0;
+}
+
