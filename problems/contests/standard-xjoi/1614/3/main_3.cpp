8b2ca01920bf2d73c50eff88bd5a1f8ac922d7db
Exr0n <spotyie@gmail.com>
2020-08-27 17:53:24 -0700

    for the second iter, why is pos[1] = 1 not cleared??

--- problems/contests/standard-xjoi/1614/3/main_3.cpp
+++ problems/contests/standard-xjoi/1614/3/main_3.cpp
@@ -9,8 +9,9 @@
 #include <cstdlib>
 #include <cstring>
 #include <cmath>
-#include <utility>
 #include <algorithm>
+#include <utility>
+#include <queue>
 #include <list>
 #include <map>
 
@@ -26,31 +27,44 @@
 using namespace std;
 const ll MX = 1e5+11;
 
-int N;
+int N, pos[MX];
 list<int> h[MX];
 
 int remain(int c, int p, int k, int lay=1)
 {
-    //for (int i=0; i<lay; ++i) printf("|   "); printf("%d from %d k %d\n", c, p, k);
-    map<int, int> pos;
+    for (int i=0; i<lay; ++i) printf("|   "); printf("%d from %d k %d\n", c, p, k);
+    vector<int> q; q.reserve(h[c].size());
+    //memset(pos, 0, sizeof pos);
+    for (int i=0; i<MX; ++i) pos[i] = 0;
     for (int n : h[c]) if (n != p)
-        ++pos[remain(n, c, k, lay+1) % k];
+    {
+        int got = remain(n, c, k, lay+1) % k;
+        if (got < 0) return -1;
+        if (got > 0) {
+            if (!pos[got]) q.pb(got);
+            ++pos[got];
+            for (int i=1; i<=lay; ++i) printf("|   "); printf("INCREMENT %d\n", got);
+        }
+    }
 
     //for (auto p : pos) { for (int i=0; i<lay; ++i) printf("|   "); printf("%d...%d\n", p.f, p.s); }
+    printf("pos[1] = %d\n", pos[1]);
+    for (int i : q) { for (int i=0; i<lay; ++i) printf("|   "); printf("%d...%d\n", i, pos[i]); }
+    printf("pos[1] = %d\n", pos[1]);
 
     int ret=0;
-    if (pos.count(-1)) return -1;
     if (~k&1 && pos[k>>1]&1) ret = k>>1;    // if even k, center must match self
-    for (auto p : pos)
-        if (p.f && p.s && p.s > pos[k-p.f]) // FIX: don't doublecount by p.s > pos[k-p.f] instead of p.s != pos[k-p.f]
+    for (int i=1; i<=lay; ++i) printf("|   "); printf("pos[%d] = %d    ret = %d\n", k>>1, pos[k>>1], ret);
+
+    for (int i : q)
+        if (pos[i] && pos[i] > pos[k-i]) // FIX: don't doublecount by p.s > pos[k-p.f] instead of p.s != pos[k-p.f]
         {
-            //printf("mismatch! val=%d (%d vs %d) ret=%d\n", p.f, p.s, pos[k-p.f], ret);
+            printf("mismatch! val=%d (%d vs %d) ret=%d\n", i, pos[i], pos[k-i], ret);
             if (ret) ret = -2;
-            else ret = p.f;
+            else ret = i;
         }
-    ++ret;
-    //for (int i=0; i<lay; ++i) printf("|   "); printf("=> %d\n", ret%k);
-    return ret;
+    for (int i=0; i<lay; ++i) printf("|   "); printf("=> %d\n", (ret+1)%k);
+    return ret+1;
 }
 
 int main()
