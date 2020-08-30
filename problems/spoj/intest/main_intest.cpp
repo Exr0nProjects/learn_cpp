14a07e3ef140c953145ed29af2d4c2e1b5fb8f2a
Exr0n <spotyie@gmail.com>
2020-08-30 12:30:29 -0700

    make it a macro?

--- problems/spoj/intest/main_intest.cpp
+++ problems/spoj/intest/main_intest.cpp
@@ -18,28 +18,36 @@
 
 #define F(i,b) for (ll i=1; i<=(b); ++i)
 #define R(i,b) for (ll i=(b); i>=1; --i)
-inline bool sc(ll &n)
+inline ll sc(bool &e)
 {
-    n = 0;
     int neg = 1;
+    register ll n = 0;
     register char c = getchar_unlocked();
     while ((c <  '0' || c >  '9') && c != '-'
             && c != '+' && c!=EOF)
         c = getchar_unlocked();
     if (c == '-') neg = -1, c = getchar_unlocked();
-    //while ((c <  '0' || c >  '9') && c!=EOF)
-    //    c = getchar_unlocked();
+    while ((c <  '0' || c >  '9') && c!=EOF)
+        c = getchar_unlocked();
     while ( c >= '0' && c <= '9'  && c!=EOF)
         (n *= 10) += (c-'0'), c = getchar_unlocked();
-    n *= neg;
-    return c != EOF;
+    e = c != EOF;
+    return n * neg;
 }
-#define _ilb inline bool
-_ilb sc(int &a) { ll x; bool b=sc(x); a=x; return b; }
-_ilb sc(int &a, int &b) { return sc(a)&&sc(b); }
-_ilb sc(int &a, int &b, int &c) { return sc(a, b)&&sc(c); }
-_ilb sc(ll &a, ll &b) { return sc(a)&&sc(b); }
-_ilb sc(ll &a, ll &b, int &c) { return sc(a, b)&&sc(c); }
+inline ll sc()
+{
+    int neg = 1;
+    register ll n = 0;
+    register char c = getchar_unlocked();
+    while ((c <  '0' || c >  '9') && c != '-' && c != '+')
+        c = getchar_unlocked();
+    if (c == '-') neg = -1, c = getchar_unlocked();
+    while ((c <  '0' || c >  '9'))
+        c = getchar_unlocked();
+    while ((c >= '0' && c <= '9'))
+        (n *= 10) += (c-'0'), c = getchar_unlocked();
+    return n * neg;
+}
 //struct Edge { int f, t, n; } edges[-1]; int head[-1];
 
 using namespace std;
@@ -47,10 +55,10 @@ const int MX = -1;
 
 int main()
 {
-    int N, K, tot=0; sc(N, K);
-    //while (N--) { int x; sc(x); tot += !(x%K); }
-    while (N--) { int x; sc(x); if (!(x%K)) ++tot; }
-    //F(i,N) { int x; sc(x); tot += !(x%K); }
+    int N=sc(), K=sc(), tot=0;
+    //while (N--) { int x; sc(x); if (!(x%K)) ++tot; }
+    int a = ({int b = 2; b;});
+    while (N--) if (!(sc()%K)) ++tot;
     printf("%d\n", tot);
 
     return 0;
