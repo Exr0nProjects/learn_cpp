af88b59bcefc6630631cd419c24a4e41a0d75879
Exr0n <spotyie@gmail.com>
2020-08-31 23:51:38 -0700

    good enough defines

--- problems/create.sh
+++ problems/create.sh
@@ -20,6 +20,6 @@ cat ./.template_state/Makefile\
     > "$1/Makefile"
 
 #tmux new-window -n "$(basename $1)" "cd $1 && tmux split-window -v && PROB=$(basename $1) && tmux resizep -y 30 && tmux selectp -U && nvim 'main_$(basename $1).cpp' -c 'execute \"normal /-1;/\" | normal \"ggnn\" | noh'"
-tmux new-window -n "$(basename $1)" "cd $1 && tmux split-window -h && PROB=$(basename $1) && tmux selectp -L && nvim 'main_$(basename $1).cpp' -c 'execute \"normal /-1;/\" | normal \"ggnn\" | noh'"
+tmux new-window -n "$(basename $1)" "cd $1 && tmux split-window -h && PROB=$(basename $1) && tmux selectp -L && nvim 'main_$(basename $1).cpp' -c 'normal 8Gzf49Gjf-'"
 
 #[[ -s "$1/main_$(basename $1).cpp" ]] && printf "File exists!\n" | exit 1
