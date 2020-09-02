9d34b355585abae4837d15a832b81c97206e9b33
Exr0n <spotyie@gmail.com>
2020-09-01 19:10:31 -0700

    why re

--- problems/contests/standard-xjoi/1620/3/main_3.cpp
+++ problems/contests/standard-xjoi/1620/3/main_3.cpp
@@ -15,7 +15,7 @@
 #define mp make_pair
 #define f first
 #define s second
-#define lb(x) ((x)-=(x)&-(x))
+#define lb(x) ((x)&-(x))
 
 #define F(i,b) for (ll i=1; i<=(b); ++i)
 #define R(i,b) for (ll i=(b); i>=1; --i)
@@ -44,48 +44,72 @@ _ilb sc(ll &a, ll &b, int &c) { return sc(a, b)&&sc(c); }
 using namespace std;
 const int MX = 1e5+11;
 
-int N, P, d[MX], bit[MX<<2][MX<<2];
-vector<pair<pii, pii> > boards;
+int N, P, v[MX], d[MX], bit[MX<<1];
+vector<pii> y(1);
 set<int> desk;
 unordered_map<int, int> dk;
 
-int bq(pii x)
+typedef pair<int, pair<bool, int> > Event; // x pos, {0: start, 1: end}, line id
+priority_queue<Event, deque<Event>, greater<Event> > pq;
+
+int bq(int x)
 {
     int mx = 0;
-    for (; x.f; lb(x.f))
-        for (int y=x.s; y; lb(y))
-            mx = max(mx, bit[x.f][y]);
+    for (; x; x-=lb(x))
+        mx = max(mx, bit[x]);
     return mx;
+}
+void bu(int x, int v)
+{
+    for (; x<=MX; x+=lb(x))
+        bit[x] = max(bit[x], v);
 }
 
 int main()
 {
     sc(N, P);
     desk.insert(N);
-    for (int i=0; i<P; ++i)
+    for (int i=1; i<=P; ++i)
     {
         int x1, y1, x2, y2;
         sc(x1, y1, x2, y2);
         d[i] = abs(x1-x2) + abs(y1-y2);
-        boards.pb(mp(mp(x1, y1), mp(x2, y2)));
-        desk.insert(x1), desk.insert(x2),
-        desk.insert(y1), desk.insert(y2);
+        //printf("d[%d] = %d\n", i, d[i]);
+        y.pb(mp(y1+1, y2+1));
+        pq.push(mp(x1, mp(0, i)));
+        pq.push(mp(x2, mp(1, i)));
+        desk.insert(y1+1), desk.insert(y2+1);
     }
     int i=0; for (auto v : desk) dk[v] = ++i;
     //i = 0;
-    for (auto p : boards)
-        p.f.f = dk[p.f.f], p.f.s = dk[p.f.s],
-        p.s.f = dk[p.s.f], p.s.s = dk[p.s.s];
-        //printf("%d,%d -> %d,%d for %d\n", p.f.f, p.f.s, p.s.f, p.s.s, d[i++]);
-    sort(boards.begin(), bords.end());
-    int mx = 0;
-    i = 0;
-    for (auto p : boards)
+    for (auto p : y) p.f = dk[p.f], p.s = dk[p.s];
+
+    while (!pq.empty())
     {
-        int v = bq(p.f) + d[++i];
-        mx = max(mx, v);
-        bu(p.s, v);
+        Event cur = pq.top(); pq.pop();
+        //printf("event %d %d at %d\n", cur.s.f, cur.s.s, cur.f);
+        if (cur.s.f)
+        {
+            bu(y[cur.s.s].s, v[cur.s.s]);
+        }
+        else
+        {
+            v[cur.s.s] = bq(y[cur.s.s].f) + d[cur.s.s];
+        }
     }
-    printf("%d\n", N*N-mx);
+    for (int i=1; i<=N; ++i) v[i] = max(v[i], v[i-1]);
+    printf("%d\n", N+N-v[N]);
+
+        //printf("%d,%d -> %d,%d for %d\n", p.f.f, p.f.s, p.s.f, p.s.s, d[i++]);
+    //sort(boards.begin(), bords.end());
+    //int mx = 0;
+    //i = 0;
+    //for (auto p : boards)
+    //{
+    //    int v = bq(p.f) + d[++i];
+    //    mx = max(mx, v);
+    //    bu(p.s, v);
+    //}
+    //printf("%d\n", N*N-mx);
 }
 
