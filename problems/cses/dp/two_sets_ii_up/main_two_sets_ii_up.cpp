dcbac44a36102224603a43c0c357679f8f11474a
Exr0n <spotyie@gmail.com>
2020-09-13 16:12:58 -0700

    why wa

---
+++ problems/cses/dp/two_sets_ii_up/main_two_sets_ii_up.cpp
@@ -0,0 +1,77 @@
+/*
+ * Problem two_sets_ii_up (cses/dp/two_sets_ii_up)
+ * Create time: Sun 13 Sep 2020 @ 10:59 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+
+#define ll long long
+#define dl double
+
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define lb(x) ((x)&-(x))
+
+#define F(i,b) for (ll i=1; i<=(b); ++i)
+#define R(i,b) for (ll i=(b); i>=1; --i)
+//struct Edge { int u, v, n; } edges[MX]; int head[MX], ecnt=0;
+
+void setIO(const std::string &name = "two_sets_ii_up")
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+    if (fopen((name + ".in").c_str(), "r") != 0)
+        freopen((name + ".in").c_str(), "r", stdin),
+        freopen((name + ".out").c_str(), "w+", stdout);
+}
+#define _gc getchar_unlocked
+inline bool sc(ll &n)
+{
+    int neg = 1;
+    register char c;
+    do c = _gc(); while (isspace(c));
+    if (c == '-') neg = -1, c = _gc();
+    for (n=0; c >= '0' && c <= '9'; c=_gc())
+        (n *= 10) += (c-'0');
+    n *= neg;
+    return c != EOF;
+}
+#define _ilb inline bool
+_ilb sc(int&a){ll x;bool b=sc(x);a=x;return b;}
+_ilb sc(int&a,int&b){return sc(a)&&sc(b);}
+_ilb sc(int&a,int&b,int&c){return sc(a,b)&&sc(c);}
+_ilb sc(int&a,int&b,int&c,int&d){return sc(a,b)&&sc(c,d);}
+_ilb sc(ll&a,ll&b){return sc(a)&&sc(b);}
+_ilb sc(ll&a,ll&b,ll&c){return sc(a,b)&&sc(c);}
+_ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
+
+using namespace std;
+const int MX = 511;
+const int MXV = 500*501/2 + 10;
+const int MOD = 1e9 + 7;
+
+int N, dp[MXV];
+
+int main()
+{
+    int N; sc(N);
+
+    dp[0] = 1;
+    for (int i=1; i<=N; ++i)
+        for (int j=MXV; j>=i; --j)
+            (dp[j] += dp[j-i]) %= MOD;
+            //dp[j] = (dp[j] + dp[j-i]) % MOD;
+
+    //printf("%d ", N*(N+1)/4);
+    if ((N*(N+1)/2)&1) printf("0\n");
+    else printf("%d\n", dp[N*(N+1)/4]/2);
+
+}
+
