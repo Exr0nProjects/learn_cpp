3149377d5868a7fbac89b0d793996870e6f0c73b
Exr0n <spotyie@gmail.com>
2020-09-18 11:57:59 -0700

    AC contests/standard-xjoi/1625/2: (script)

---
+++ problems/contests/standard-xjoi/1625/x_2/x_main_2.cpp
@@ -0,0 +1,90 @@
+/*
+ * Problem 2 (contests/standard-xjoi/1625/2)
+ * Create time: Fri 18 Sep 2020 @ 11:39 (PDT)
+ * Accept time: Fri 18 Sep 2020 @ 11:57 (PDT)
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+
+#define ll long long
+#define dl double
+
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define lb(x) ((x)&-(x))
+
+#define F(i,b) for (ll i=1; i<=(b); ++i)
+#define R(i,b) for (ll i=(b); i>=1; --i)
+//struct Edge { int u, v, n; } edges[MX]; int head[MX], ecnt=0;
+
+void setIO(const std::string &name = "2")
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+    if (fopen((name + ".in").c_str(), "r") != 0)
+        freopen((name + ".in").c_str(), "r", stdin),
+        freopen((name + ".out").c_str(), "w+", stdout);
+}
+#define _gc getchar_unlocked
+inline bool sc(ll &n)
+{
+    int neg = 1;
+    register char c;
+    do c = _gc(); while (isspace(c));
+    if (c == '-') neg = -1, c = _gc();
+    for (n=0; c >= '0' && c <= '9'; c=_gc())
+        (n *= 10) += (c-'0');
+    n *= neg;
+    return c != EOF;
+}
+inline ll sc() { ll x; sc(x); return x; }
+#define _ilb inline bool
+_ilb sc(int&a){ll x;bool b=sc(x);a=x;return b;}
+_ilb sc(int&a,int&b){return sc(a)&&sc(b);}
+_ilb sc(int&a,int&b,int&c){return sc(a,b)&&sc(c);}
+_ilb sc(int&a,int&b,int&c,int&d){return sc(a,b)&&sc(c,d);}
+_ilb sc(ll&a,ll&b){return sc(a)&&sc(b);}
+_ilb sc(ll&a,ll&b,ll&c){return sc(a,b)&&sc(c);}
+_ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
+
+using namespace std;
+const int MX = 1e5+11;
+
+int N, a[MX], bit[MX], stop;
+
+void bu(int x)
+{
+    //printf("update %d\n", x);
+    for (; x<=N; x+=lb(x))
+        ++bit[x];
+}
+int bq(int x)
+{
+    int ret=0;
+    for (; x; x-=lb(x))
+        ret += bit[x];
+    return ret;
+}
+
+int main()
+{
+    sc(N);
+    for (int i=1; i<=N; ++i) sc(a[i]);
+    a[N+1] = 1e30;  // FIX: fencepost--start at N+1 to bitupdate N in for loop
+    for (stop=N; stop && a[stop] < a[stop+1]; --stop) bu(a[stop]);
+    //printf("stop = %d\n", stop);
+
+    printf("%d\n", stop);
+    for (int i=1; i<=stop; ++i)
+    {
+        printf("%d ", stop-i+bq(a[i]));
+        bu(a[i]);
+    }
+}
+
