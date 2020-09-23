87b8ec41220900a3a1dad9d9976aa47f11950848
Exr0n <spotyie@gmail.com>
2020-09-22 17:33:29 -0700

    modulo for template

--- problems/create.sh
+++ problems/create.sh
@@ -6,7 +6,8 @@ dateformat="+%a %d %b %Y @ %R (%Z)";
 mkdir -p $1
 
 [[ -s "$1/main_$(basename $1).cpp" ]] ||\
-    cat ./.template_state/template.cpp\
+    #cat ./.template_state/template.cpp\
+    cat ./.template_state/defines_everywhere.cpp\
     | sed "s/\[\!meta\:pid\!\]/$(basename $1)/g"\
     | sed "s/\[\!meta\:srcpath\!\]/${1//\//\\/}/g"\
     | sed "s/\[\!meta\:beg\!\]/$(date "$dateformat")/g"\
@@ -19,6 +20,6 @@ cat ./.template_state/Makefile\
     > "$1/Makefile"
 
 #tmux new-window -n "$(basename $1)" "cd $1 && tmux split-window -v && PROB=$(basename $1) && tmux resizep -y 30 && tmux selectp -U && nvim 'main_$(basename $1).cpp' -c 'execute \"normal /-1;/\" | normal \"ggnn\" | noh'"
-tmux new-window -n "$(basename $1)" "cd $1 && tmux split-window -h && PROB=$(basename $1) && tmux selectp -L && nvim 'main_$(basename $1).cpp' -c 'normal 8Gzf54Gjf-'"
+tmux new-window -n "$(basename $1)" "cd $1 && tmux split-window -h && PROB=$(basename $1) && tmux selectp -L && nvim 'main_$(basename $1).cpp' -c 'normal 8Gzf56Gjf-'"
 
 #[[ -s "$1/main_$(basename $1).cpp" ]] && printf "File exists!\n" | exit 1
