3b3c3f0891dc8fa20ae4dbcabb6a1e056ae33c5b
Exr0n <spotyie@gmail.com>
2020-09-23 20:09:59 -0700

    wa tle

--- problems/contests/standard-xjoi/1630/1/main_1.cpp
+++ problems/contests/standard-xjoi/1630/1/main_1.cpp
@@ -61,11 +61,12 @@ const int MX = 1e5+1;
 
 int N, a[MX], bit[MX];
 map<int, int> dsc;
+pair<int, int> c[MX];
 
-void bu(int x)
+void bu(int x, int v)
 {
     for (; x<=N; x+=lb(x))
-        ++bit[x];
+        bit[x] += v;
 }
 int bq(int x)
 {
@@ -84,13 +85,59 @@ int main()
     int i=1; for (auto &p : dsc) p.s = i++;
     F(i, N) a[i] = dsc[a[i]];
     //F(i, N) printf("%3d", a[i]); printf("\n");
-    int ans=0;
-    for (int i=N; i; --i)   // FIX: inversions back to front
+    //int ans=0;
+    //for (int i=N; i; --i)   // FIX: inversions back to front
+    //{
+    //    //F(i, N) printf("%3d", bit[i]); printf("\n");
+    //    if (bq(a[i]-1) > 1) ++ans;
+    //    bu(a[i]);
+    //}
+    //printf("%d\n", ans);
+
+    for (int i=N; i; --i)
     {
-        //F(i, N) printf("%3d", bit[i]); printf("\n");
-        if (bq(a[i]-1) > 1) ++ans;
-        bu(a[i]);
+        bu(a[i], 1);   // normally after but no colision
+        a[i] = bq(a[i]-1);
     }
+
+
+    int ans=0, cnt;
+    do {
+        ++ans;
+        //printf("a: "); F(i, N) printf("%3d", a[i]); printf("\n");
+        cnt = 0;
+        for (int i=N; i; --i)
+        {
+            int g = lower_bound(c, c+cnt, mp(a[i], 0)) - c;
+            //printf("    a[%d] = %d -> %d\n", i, a[i], g);
+            if (g == cnt) c[cnt++] = mp(a[i], i);
+            else c[g] = min(c[g], mp(a[i], i));
+        }
+        for (int i=0; i<cnt; ++i)
+        {
+            //printf("a[c[%d]] = a[%d] = %d\n", i, c[i].s, a[c[i].s]);
+            if (a[c[i].s]) --a[c[i].s];
+            c[i] = mp(0, 0);
+        }
+        //printf("b: "); F(i, N) printf("%3d", a[i]); printf("\n");
+        for (int i=0; i<=N; ++i)
+        {
+            int g = lower_bound(c, c+cnt, mp(a[i], 0)) - c;
+            //printf("    a[%d] = %d -> %d\n", i, a[i], g);
+            if (g == cnt) c[cnt++] = mp(a[i], i);
+            else c[g] = min(c[g], mp(a[i], i));
+        }
+        for (int i=0; i<cnt; ++i)
+        {
+            //printf("a[c[%d]] = a[%d] = %d\n", i, c[i].s, a[c[i].s]);
+            if (a[c[i].s]) --a[c[i].s];
+            c[i] = mp(0, 0);
+        }
+        //printf("c: "); F(i, N) printf("%3d", a[i]); printf("\n");
+        bool flag = 0;
+        F(i, N) if (a[i]) flag=1;
+        if (!flag) break;
+    } while (true);
     printf("%d\n", ans);
 }
 
