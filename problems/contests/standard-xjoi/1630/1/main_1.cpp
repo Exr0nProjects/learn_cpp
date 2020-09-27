a750b3f1badf69273cc2be2e73d88ba9ce8173a1
Exr0n <spotyie@gmail.com>
2020-09-27 12:35:40 -0700

    set thing actually doesn't work
    because it's not monotonically increasing, and you can't just add like
    that and expect it to work (probably doublecounts)

--- problems/contests/standard-xjoi/1630/1/main_1.cpp
+++ problems/contests/standard-xjoi/1630/1/main_1.cpp
@@ -9,7 +9,7 @@
 #include <cstring>
 #include <algorithm>
 #include <utility>
-#include <map>
+#include <set>
 
 #define ll long long
 #define dl double
@@ -59,101 +59,20 @@ _ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
 using namespace std;
 const int MX = 1e5+1;
 
-int N, a[MX], bit[MX];
-map<int, int> dsc;
-pair<int, int> c[MX];
-
-void bu(int x, int v)
-{
-    for (; x<=N; x+=lb(x))
-        bit[x] += v;
-}
-int bq(int x)
-{
-    int ret=0;
-    //printf("bq(%d) ", x);
-    for (; x; x-=lb(x))
-        ret += bit[x];
-    //printf("=> %d\n", ret);
-    return ret;
-}
+int N, a[MX], srt[MX];
+set<int> seen;
 
 int main()
 {
-    sc(N); F(i, N) sc(a[i]);
-    //F(i, N) dsc[a[i]];
-    //int i=1; for (auto &p : dsc) p.s = i++;
-    //F(i, N) a[i] = dsc[a[i]];
-
-    //F(i, N) printf("%3d", a[i]); printf("\n");
-    //int ans=0;
-    //for (int i=N; i; --i)   // FIX: inversions back to front
-    //{
-    //    //F(i, N) printf("%3d", bit[i]); printf("\n");
-    //    if (bq(a[i]-1) > 1) ++ans;
-    //    bu(a[i]);
-    //}
-    //printf("%d\n", ans);
-
-    //for (int i=N; i; --i)
-    //{
-    //    bu(a[i], 1);   // normally after but no colision
-    //    a[i] = bq(a[i]-1);
-    //}
-    //
-    //
-    //int ans=0, cnt;
-    //do {
-    //    ++ans;
-    //    //printf("a: "); F(i, N) printf("%3d", a[i]); printf("\n");
-    //    cnt = 0;
-    //    for (int i=N; i; --i)
-    //    {
-    //        int g = lower_bound(c, c+cnt, mp(a[i], 0)) - c;
-    //        //printf("    a[%d] = %d -> %d\n", i, a[i], g);
-    //        if (g == cnt) c[cnt++] = mp(a[i], i);
-    //        else c[g] = min(c[g], mp(a[i], i));
-    //    }
-    //    for (int i=0; i<cnt; ++i)
-    //    {
-    //        //printf("a[c[%d]] = a[%d] = %d\n", i, c[i].s, a[c[i].s]);
-    //        if (a[c[i].s]) --a[c[i].s];
-    //        c[i] = mp(0, 0);
-    //    }
-    //    //printf("b: "); F(i, N) printf("%3d", a[i]); printf("\n");
-    //    for (int i=0; i<=N; ++i)
-    //    {
-    //        int g = lower_bound(c, c+cnt, mp(a[i], 0)) - c;
-    //        //printf("    a[%d] = %d -> %d\n", i, a[i], g);
-    //        if (g == cnt) c[cnt++] = mp(a[i], i);
-    //        else c[g] = min(c[g], mp(a[i], i));
-    //    }
-    //    for (int i=0; i<cnt; ++i)
-    //    {
-    //        //printf("a[c[%d]] = a[%d] = %d\n", i, c[i].s, a[c[i].s]);
-    //        if (a[c[i].s]) --a[c[i].s];
-    //        c[i] = mp(0, 0);
-    //    }
-    //    //printf("c: "); F(i, N) printf("%3d", a[i]); printf("\n");
-    //    bool flag = 0;
-    //    F(i, N) if (a[i]) flag=1;
-    //    if (!flag) break;
-    //} while (true);
-    //printf("%d\n", ans);
-
-
-    int ans=0, cnt;
-    do {
-        ++ans;
-        //printf("a: "); F(i, N) printf("%3d", a[i]); printf("\n");
-        for (int i=1; i<N; ++i)
-            if (a[i] > a[i+1]) swap(a[i], a[i+1]);
-        for (int i=N-1; i; --i)
-            if (a[i] > a[i+1]) swap(a[i], a[i+1]);
-        bool flag = 0;
-        F(i, N-1) if (a[i] > a[i+1]) flag=1;
-        if (!flag) break;
-    } while (true);
-    printf("%d\n", ans);
+    sc(N); F(i, N) a[i] = srt[i] = sc();
+    sort(srt+1, srt+N+1);
+    int M = 0;
+    F(i, N)
+    {
+        seen.insert(a[i]);
+        M += !seen.count(srt[i]);
+        //printf("%d: %d %d %d\n", i, a[i], srt[i], M);
+    }
+    printf("%d\n", M);
 }
 
