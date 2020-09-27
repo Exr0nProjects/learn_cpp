925b769317747bacd019bd0f989fa723f50e42cc
Exr0n <spotyie@gmail.com>
2020-09-26 20:36:19 -0700

    acutally need seperate dist and dep arrays
    because dist won't always increase by one

--- problems/zoj/2760_netflo/main_2760_netflo.cpp
+++ problems/zoj/2760_netflo/main_2760_netflo.cpp
@@ -15,8 +15,9 @@
 #define ll long long
 #define dl double
 
-#define pii pair<int, int>
 #define gpq(T) priority_queue<T, deque<T>, greater<T> >
+#define CMA , // https://stackoverflow.com/q/13842468/10372825
+#define pii pair<int, int>
 #define pb push_back
 #define mp make_pair
 #define f first
@@ -61,45 +62,50 @@ _ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
 using namespace std;
 const int MX = 111;
 
-int N, U, V, dep[MX], dist[MX][MX];
+int N, U, V, dep[MX], dist[MX], cap[MX][MX];
 list<pii> hd[MX];
 
 void kdep()
 {
-    memset(dep, 0x3f, sizeof dep);
+    memset(dist, 0x3f, sizeof dist);
+    //memset(dep, 0, sizeof dep);
+    //dep[U] = 1;
     //priority_queue<pii, deque<pii>, greater<pii> > pq;
-    gpq(pii) pq;
-    pq.push(mp(1, U));
+    gpq(pair<int CMA pii>) pq;
+    pq.push(mp(1, mp(U, 0)));
     while (!pq.empty())
     {
-        pii c = pq.top(); pq.pop();
-        if (dep[c.s] < c.f) continue;
-        dep[c.s] = c.f;
-        if (c.s == V) break;
-        for (auto p : hd[c.s])
-            if (dep[p.s] > c.f + p.f)
-                pq.push(mp(c.f+p.f, p.s));
+        pair<int, pii> c = pq.top(); pq.pop();
+        if (dist[c.s.f] < c.f) continue;
+        dist[c.s.f] = c.f;
+        dep[c.s.f] = dep[c.s.s] +1;
+        if (c.s.f == V) break;
+        for (auto p : hd[c.s.f])
+            if (dist[p.s] > c.f + p.f)
+                pq.push(mp(c.f+p.f, mp(p.s, c.s.f)));
     }
 }
 
 int aug(int c, int p, int mn, int lay=1)
 {
     F(i, lay) printf("|   "); printf("aug %2d %2d for %2d\n", c, p, mn);
-    if (!mn || c == V) return dist[p][c] -= mn, mn;
+    if (!mn || c == V) return cap[p][c] -= mn, mn;
     int flo=0;
     for (int n=1; n<=N; ++n)
+    {
+            F(i, lay) printf("|   "); printf("could go to %d -> %d for %d, %d\n", c, n, mn, cap[c][n]);
         if (dep[n] -1 == dep[c])
         {
-            F(i, lay) printf("|   "); printf("could go to %d -> %d for %d\n", c, n, dist[c][n]);
-            if (int g = aug(n, c, min(mn, dist[c][n]), lay+1))
+            if (int g = aug(n, c, min(mn, cap[c][n]), lay+1))
             {
-                F(i, lay) printf("|   "); printf("g = %d\n", g);
+                F(i, lay) printf("|   "); printf("g = %d, cap = %d\n", g, cap[p][c]);
                 flo += g;
-                dist[p][c] -= g;
-                dist[c][p] += g;
-                mn = min(mn, dist[p][c]);
+                cap[p][c] -= g;
+                cap[c][p] += g;
+                mn = min(mn, cap[p][c]);
             }
         }
+    }
     F(i, lay) printf("|   "); printf("=> %d\n", flo);
     return flo;
 }
@@ -132,13 +138,13 @@ int main()
         int w=sc();
         if (w > 0)
             //printf("%d %d = %d\n", i, j, w),
-            hd[i].pb(mp(w, j)), dist[i][j] = w;
+            hd[i].pb(mp(w, j)), cap[i][j] = w;
     }
-    dist[0][U] = 1e9;
+    cap[0][U] = 1e9;
     sc(U, V);
     ++U, ++V;
 
-    F(i, N) { F(j, N) printf("%3d", dist[i][j]); printf("\n"); }
+    F(i, N) { F(j, N) printf("%3d", cap[i][j]); printf("\n"); }
 
     kdep();
     F(i, N) printf("%3d", i); printf("\n");
