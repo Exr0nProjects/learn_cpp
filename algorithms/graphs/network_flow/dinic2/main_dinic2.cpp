cf31c8d08e0f85285b9876ade0ad9fb9e8dfdab1
Exr0n <spotyie@gmail.com>
2020-10-15 21:48:49 -0700

    edgelist dinic should work

--- algorithms/graphs/network_flow/dinic2/main_dinic2.cpp
+++ algorithms/graphs/network_flow/dinic2/main_dinic2.cpp
@@ -64,11 +64,12 @@ const int MX = 1e5+11;
 int N, M, S, T, dep[MX];
 // cap[MX][MX];
 
-struct edge { int t, n, w; } edges[MX]; int thead[MX], head[MX], ecnt=0;
+struct Edge { int t, n, w; } edges[MX]; int thead[MX], head[MX], ecnt=2;
 void addEdge(int a, int b, int w)
 {
     //cap[a][b] = w;
     edges[ecnt].w = w;
+    printf("%d -> %d = %d\n", a, b, edges[ecnt].w);
     //edges[ecnt].f = a;
     edges[ecnt].t = b;
     edges[ecnt].n = head[a];
@@ -82,24 +83,29 @@ bool kdep()
     q.push(S);
     dep[S] = 1; // FIX: don't return to source
     //bool flag=0;
+    //memcpy(thead, head, N+1<<2);   // FIX: args--memcpy last arg is length, not end pointer
+    copy(head, head+N+1, thead);
     while (!q.empty())
     {
         int c = q.front(); q.pop();
+        printf("c = %d\n", c);
         //if (c == T) flag = 1;
-        if (c == T) break;   // FIX: no need to wait
+        if (c == T) return 1;   // FIX: no need to wait
 
         //for (int n=1; n<=N; ++n)
         for (int e=head[c]; e; e=edges[e].n)
             if (!dep[edges[e].t] && edges[e].w)
                 dep[edges[e].t] = dep[c]+1, q.push(edges[e].t);
     }
-    memcpy(thead, head, N+1);   // FIX: args--memcpy last arg is length, not end pointer
-    return !q.empty();
+    //for (int i=1; i<=N; ++i) printf("%3d", thead[i]); printf("\n");
+    //return !q.empty();
+    return 0;
 }
 
 
 int aug(int c, int mn)
 {
+    printf("%d %d\n", c, mn);
     if (!mn || c == T) return mn;
     int flo=0;
     //for (int n=1; n<=N; ++n) if (dep[n] == dep[c]+1)
@@ -109,14 +115,14 @@ int aug(int c, int mn)
             if (int g = aug(edges[e].t, min(mn, edges[e].w)))
             {
                 flo += g;
-                edges[e].w += g;
-                edges[e^1].w -= g;
+                edges[e].w -= g;    // FIX: typo--subtract from and add reverse smah
+                edges[e^1].w += g;
                 //cap[c][edges[e].t] -= g;
                 //cap[edges[e].t][c] += g;
                 mn -= g;
             }
-        if (mn) thead[c] = edges[thead[c].n];
-        else break;
+        //if (mn) thead[c] = edges[thead[c]].n;
+        //else break;
     }
     return flo;
 }
