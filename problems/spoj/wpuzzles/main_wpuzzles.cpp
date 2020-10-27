28bfa6525d432bf607119cce48af82a567833378
Exr0n <spotyie@gmail.com>
2020-10-26 22:14:19 -0700

    wrong answer..

--- problems/spoj/wpuzzles/main_wpuzzles.cpp
+++ problems/spoj/wpuzzles/main_wpuzzles.cpp
@@ -78,18 +78,18 @@ pair<pair<int, int>, char> ans[MX];
 
 vector<int> useChar(int &cur, int c)
 {
-    printf("    using char '%c' on %d\n", c+'A', cur);
+    //printf("    using char '%c' on %d\n", c+'A', cur);
     for (; cur && !trie[cur][c]; cur=fail[cur]);    // KMP flashbacks
     if (trie[cur][c]) cur = trie[cur][c];
-    printf("        went to %d\n", cur);
+    //printf("        went to %d\n", cur);
     // follow failtrail for skipped words
     vector<int> tans;
     for (int ftc=cur; ftc && !failvis[ftc] && hasw[ftc]; failvis[ftc]=1, ftc=fail[ftc])
     {
-        printf("           visiting %-3d on failtrail\n", ftc);
+        //printf("           visiting %-3d on failtrail\n", ftc);
         if (isw[ftc]) tans.pb(isw[ftc]);
     }
-    printf("        got %d answers\n", tans.size());
+    //printf("        got %d answers\n", tans.size());
     return tans;
 }
 
@@ -110,9 +110,8 @@ int main()
         lcnt=1;
         for (int i=1; i<=W; ++i) ans[i] = {};
 
-        for (int i=0; i<L; ++i) { scanf("%s", grid[i]); printf("'%s'\n", grid[i]); }
+        for (int i=0; i<L; ++i) scanf("%s", grid[i]);
         scanf("%*c");   // FIX: input--consume newline
-        printf("w %d\n", W);
         // construct trie
         for (int i=1; i<=W; ++i)
         {
@@ -121,7 +120,6 @@ int main()
             {
                 if (c < 0)
                 {
-                    printf("\n");
                     wlen[i] = dep[cur]-1;
                     hasw[cur] = 1, isw[cur] = i;
                     break;
@@ -130,7 +128,7 @@ int main()
                     //par[lcnt] = cur,
                     dep[lcnt] = dep[cur]+1,
                     trie[cur][c] = lcnt++;
-                printf("got char '%c' -> %d\n", c+'A', trie[cur][c]);
+                //printf("got char '%c' -> %d\n", c+'A', trie[cur][c]);
                 cur = trie[cur][c], c = _gc() - 'A';
             }
         }
@@ -143,7 +141,7 @@ int main()
         while (!q.empty())
         {
             QState c = q.front(); q.pop();
-            printf("q cur %d <- %d (%c)\n", c.f.f, c.f.s, c.s+'A');
+            //printf("q cur %d <- %d (%c)\n", c.f.f, c.f.s, c.s+'A');
             // construct fail pointer
             for (fail[c.f.f] = fail[c.f.s]; fail[c.f.f] && !trie[fail[c.f.f]][c.s]; // FIX: typo--go while fail[c.f.f], not stop when it
                     fail[c.f.f] = fail[fail[c.f.f]]);   // kmp flashbacks
@@ -154,58 +152,62 @@ int main()
                 q.push(mp(mp(trie[c.f.f][i], c.f.f), i));
         }
 
-        // debug ac automaton
-        printf("    "); for (int i=0; i<26; ++i) printf("%3c", i+'A'); printf("\n");
-        for (int i=0; i<lcnt; ++i)
-        {
-            printf("%-3d ", i);
-            for (int j=0; j<26; ++j)
-                if (trie[i][j]) printf("%3d", trie[i][j]);
-                else printf("  .");
-            printf("   ->%3d     isw %-3d hasw %d dep %-3d\n", fail[i], isw[i], hasw[i], dep[i]);
-        }
+        //// debug ac automaton
+        //printf("    "); for (int i=0; i<26; ++i) printf("%3c", i+'A'); printf("\n");
+        //for (int i=0; i<lcnt; ++i)
+        //{
+        //    printf("%-3d ", i);
+        //    for (int j=0; j<26; ++j)
+        //        if (trie[i][j]) printf("%3d", trie[i][j]);
+        //        else printf("  .");
+        //    printf("   ->%3d     isw %-3d hasw %d dep %-3d\n", fail[i], isw[i], hasw[i], dep[i]);
+        //}
 
         // check the grid
-        for (int i=0; i<L; ++i) for (int d=0; d<3; ++d)
+        for (int i=0; i<L; ++i) for (int d=-1; d<2; ++d)
         {
             // start on left side
             memset(failvis, 0, sizeof failvis);
-            printf("left  side d=%d\n", d);
-            for (int y=i, x=0, cur=0; ok(y, x); ++x, y+=delta[d])
+            //printf("left  side d=%d\n", d);
+            for (int y=i, x=0, cur=0; ok(y, x); ++x, y+=d)
             {
                 vector<int> g = useChar(cur, grid[y][x]-'A');
-                printf("        cur = %d\n", cur);
-                for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y-wlen[a]*delta[d], x-wlen[a]), 'B'+d);
+                //printf("        cur = %d\n", cur);
+                //for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y-wlen[a]*d, x-wlen[a]), 'B'+d);
+                for (auto a : g) ans[a] = mp(mp(y-wlen[a]*d, x-wlen[a]), 'B'+d);
             }
             // start on right side
             memset(failvis, 0, sizeof failvis);
-            printf("right side d=%d\n", d);
-            for (int y=i, x=C-1, cur=0; ok(y, x); --x, y+=delta[d])
+            //printf("right side d=%d\n", d);
+            for (int y=i, x=C-1, cur=0; ok(y, x); --x, y+=d)
             {
                 vector<int> g = useChar(cur, grid[y][x]-'A');
-                printf("        cur = %d\n", cur);
-                for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y-wlen[a]*delta[d], x+wlen[a]), 'H'-d);
+                //printf("        cur = %d\n", cur);
+                //for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y-wlen[a]*d, x+wlen[a]), 'H'-d);
+                for (auto a : g) ans[a] = mp(mp(y-wlen[a]*d, x+wlen[a]), 'H'-d);
             }
         }
-        for (int i=0; i<C; ++i)
+        for (int i=0; i<C; ++i) for (int d=-1; d<2; ++d)
         {
             // start from the top edge (no diagonals)
             memset(failvis, 0, sizeof failvis);
-            printf("top   edge");
-            for (int x=i, y=0, cur=0; ok(y, x); ++y)
+            //printf("top   edge");
+            for (int x=i, y=0, cur=0; ok(y, x); ++y, x+=d)
             {
                 vector<int> g = useChar(cur, grid[y][x]-'A');
-                printf("        cur = %d\n", cur);
-                for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y-wlen[a], x), 'E');
+                //printf("        cur = %d\n", cur);
+                //for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y-wlen[a], x-wlen[a]*d), 'E');
+                for (auto a : g) ans[a] = mp(mp(y-wlen[a], x-wlen[a]*d), 'E');
             }
             // bottom edge (no diagonals)
-            printf("bot   edge");
+            //printf("bot   edge");
             memset(failvis, 0, sizeof failvis);
-            for (int x=i, y=L-1, cur=0; ok(y, x); --y)
+            for (int x=i, y=L-1, cur=0; ok(y, x); --y, x+=d)
             {
                 vector<int> g = useChar(cur, grid[y][x]-'A');
-                printf("        cur = %d\n", cur);
-                for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y+wlen[a], x), 'A');
+                //printf("        cur = %d\n", cur);
+                //for (auto a : g) printf("    setting ans %d\n", a), ans[a] = mp(mp(y+wlen[a], x-wlen[a]*d), 'A');
+                for (auto a : g) ans[a] = mp(mp(y+wlen[a], x-wlen[a]*d), 'A');
             }
         }
         for (int i=1; i<=W; ++i)
