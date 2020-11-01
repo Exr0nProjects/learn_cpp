dc67ffd43c7c1cf2dbbd204d6b46d6c3665e5598
Exr0n <spotyie@gmail.com>
2020-10-31 19:18:51 -0700

    too much mem (not sure what i expected)

---
+++ algorithms/linear_sieve/bitset_0/main_bitset_0.cpp
@@ -0,0 +1,102 @@
+/*
+ * Problem bitset_0 (../algorithms/linear_sieve/bitset_0)
+ * Create time: Sat 31 Oct 2020 @ 19:00 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <set>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <utility>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+
+#define ll long long
+#define dl double
+
+#define gpq(T) priority_queue<T, deque<T>, greater<T> >
+#define CMA , // https://stackoverflow.com/q/13842468/10372825
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define lb(x) ((x)&-(x))
+
+#define F(i,b) for (ll i=1; i<=(b); ++i)
+#define R(i,b) for (ll i=(b); i>=1; --i)
+//struct Edge { int u, v, n; } eg[MX]; int hd[MX], ecnt=0;
+
+inline ll pow(ll b, ll e, ll m)
+{
+    ll ret=1;
+    for (; e; e>>=1, (b*=b)%=m)
+        if (e&1) (ret *= b)%=m;
+    return ret;
+}
+
+void setIO(const std::string &name = "bitset_0")
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+    if (fopen((name + ".in").c_str(), "r") != 0)
+        freopen((name + ".in").c_str(), "r", stdin),
+        freopen((name + ".out").c_str(), "w+", stdout);
+}
+#define _gc getchar_unlocked
+inline bool sc(ll &n)
+{
+    int neg = 1;
+    register char c;
+    do c = _gc(); while (isspace(c));
+    if (c == '-') neg = -1, c = _gc();
+    for (n=0; c >= '0' && c <= '9'; c=_gc())
+        (n *= 10) += (c-'0');
+    n *= neg;
+    return c != EOF;
+}
+inline ll sc() { ll x; sc(x); return x; }
+#define _ilb inline bool
+_ilb sc(int&a){ll x;bool b=sc(x);a=x;return b;}
+_ilb sc(int&a,int&b){return sc(a)&&sc(b);}
+_ilb sc(int&a,int&b,int&c){return sc(a,b)&&sc(c);}
+_ilb sc(int&a,int&b,int&c,int&d){return sc(a,b)&&sc(c,d);}
+_ilb sc(ll&a,ll&b){return sc(a)&&sc(b);}
+_ilb sc(ll&a,ll&b,ll&c){return sc(a,b)&&sc(c);}
+_ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
+#define gcd(_a, _b) ({auto a=_a;auto\
+    b=_b;while(b)(a)%=(b),(a)^=(b)^=(a)^=(b);a;})
+
+using namespace std;
+const int MX = 1e8+11;
+int N = 1e9+6;
+
+//bitset<(unsigned) 1e9+10> *bs = new bitset<(unsigned) 1e9+10>;
+bitset<(unsigned) 1e9+10> bs;
+int pri[MX], pcnt=0;
+
+int *phi = new int[(int)1e9+10];
+
+int main()
+{
+    for (int i=2; i<=N; ++i)
+    {
+        if (!(i%(int)1)) printf("%f%%\r", (dl)i/(dl)N);
+        if (!bs[i]) phi[i] = i-1, pri[pcnt++] = i;
+        for (int j=0; j<pcnt; ++j)
+        {
+            bs[i*pri[j]] = 1;
+            phi[i*pri[j]] = phi[i] * (pri[j]-1);
+            if (!(i % pri[j]))
+            {
+                phi[i*pri[j]] = pri[j]*phi[i];
+                break;
+            }
+        }
+    }
+
+}
+
