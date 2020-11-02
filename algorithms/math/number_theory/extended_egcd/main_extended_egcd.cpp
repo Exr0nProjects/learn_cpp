84ef772c59976dfc519f982b59e56b19ee100428
Exr0n <spotyie@gmail.com>
2020-11-01 17:06:50 -0800

    wip egcd + maff test

---
+++ algorithms/math/number_theory/extended_egcd/main_extended_egcd.cpp
@@ -0,0 +1,89 @@
+/*
+ * Problem extended_egcd (../algorithms/math/number_theory/extended_egcd)
+ * Create time: Sat 31 Oct 2020 @ 19:19 (PDT)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <set>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <utility>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+
+#define ll long long
+#define dl double
+
+#define gpq(T) priority_queue<T, deque<T>, greater<T> >
+#define CMA , // https://stackoverflow.com/q/13842468/10372825
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define lb(x) ((x)&-(x))
+
+#define F(i,b) for (ll i=1; i<=(b); ++i)
+#define R(i,b) for (ll i=(b); i>=1; --i)
+//struct Edge { int u, v, n; } eg[MX]; int hd[MX], ecnt=0;
+
+inline ll pow(ll b, ll e, ll m)
+{
+    ll ret=1;
+    for (; e; e>>=1, (b*=b)%=m)
+        if (e&1) (ret *= b)%=m;
+    return ret;
+}
+
+void setIO(const std::string &name = "extended_egcd")
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+    if (fopen((name + ".in").c_str(), "r") != 0)
+        freopen((name + ".in").c_str(), "r", stdin),
+        freopen((name + ".out").c_str(), "w+", stdout);
+}
+#define _gc getchar_unlocked
+inline bool sc(ll &n)
+{
+    int neg = 1;
+    register char c;
+    do c = _gc(); while (isspace(c));
+    if (c == '-') neg = -1, c = _gc();
+    for (n=0; c >= '0' && c <= '9'; c=_gc())
+        (n *= 10) += (c-'0');
+    n *= neg;
+    return c != EOF;
+}
+inline ll sc() { ll x; sc(x); return x; }
+#define _ilb inline bool
+_ilb sc(int&a){ll x;bool b=sc(x);a=x;return b;}
+_ilb sc(int&a,int&b){return sc(a)&&sc(b);}
+_ilb sc(int&a,int&b,int&c){return sc(a,b)&&sc(c);}
+_ilb sc(int&a,int&b,int&c,int&d){return sc(a,b)&&sc(c,d);}
+_ilb sc(ll&a,ll&b){return sc(a)&&sc(b);}
+_ilb sc(ll&a,ll&b,ll&c){return sc(a,b)&&sc(c);}
+_ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
+#define gcd(_a, _b) ({auto a=_a;auto\
+    b=_b;while(b)(a)%=(b),(a)^=(b)^=(a)^=(b);a;})
+
+using namespace std;
+const int MX = -1;
+
+pii eegcd(int a, int b)
+{
+    for (; b; b%=a, a^=b^=a^=b)
+    {
+        int q = b/a;
+    }
+}
+
+int main()
+{
+    setIO();
+
+}
+
