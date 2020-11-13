a6a0da94f6aec1d13fdbdf3bab3fc04c49ea3bcd
Exr0n <mail@exr0n.com>
2020-11-12 17:51:23 -0800

    really close to tle but couldn't optimize

--- problems/duck/x_1001/main_1001.cpp
+++ problems/duck/x_1001/main_1001.cpp
@@ -10,14 +10,14 @@
 #include <cstring>
 void sort(unsigned *a, int n) {
     unsigned c[n];
-    unsigned *b = c, pos[300];
+    unsigned *b = c, pos[256];
     for (unsigned s=0; s<32; s+=8)
     {
         memset(pos, 0, sizeof pos); // FIX: clears-- pos
         for (int i=0; i<n; ++i) ++pos[a[i]>>s&0xff];
         for (int i=1; i<256; ++i) pos[i] += pos[i-1];
         for (int i=n-1; ~i; --i) b[--pos[a[i]>>s&0xff]] = a[i]; // FIX: bounds--start at n-1 not at n cuz zero index smah
-        std::swap(a, b);
+        unsigned *t = b; b = a, a = t;
     }
 }
 
