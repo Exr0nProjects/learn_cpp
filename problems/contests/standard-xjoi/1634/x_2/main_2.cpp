e17f7595083be8b7a17695b79560580b4b69c460
Exr0n <mail@exr0n.com>
2020-11-22 17:07:18 -0800

    AC contests/standard-xjoi/1634/2: (script)

---
+++ problems/contests/standard-xjoi/1634/x_2/main_2.cpp
@@ -0,0 +1,134 @@
+/*
+ * Problem 2 (contests/standard-xjoi/1634/2)
+ * Create time: Sun 22 Nov 2020 @ 15:08 (PST)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <set>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <utility>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+
+#define ll long long
+#define dl double
+
+#define gpq(T) priority_queue<T, deque<T>, greater<T> >
+#define CMA , // https://stackoverflow.com/q/13842468/10372825
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define lb(x) ((x)&-(x))
+
+#define F(i,b) for (ll i=1; i<=(b); ++i)
+#define R(i,b) for (ll i=(b); i>=1; --i)
+//struct Edge { int u, v, n; } eg[MX]; int hd[MX], ecnt=0;
+
+inline ll pow(ll b, ll e, ll m)
+{
+    ll ret=1;
+    for (; e; e>>=1, (b*=b)%=m)
+        if (e&1) (ret *= b)%=m;
+    return ret;
+}
+
+void setIO(const std::string &name = "2")
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+    if (fopen((name + ".in").c_str(), "r") != 0)
+        freopen((name + ".in").c_str(), "r", stdin),
+        freopen((name + ".out").c_str(), "w+", stdout);
+}
+#define _gc getchar_unlocked
+inline bool sc(ll &n)
+{
+    int neg = 1;
+    register char c;
+    do c = _gc(); while (isspace(c));
+    if (c == '-') neg = -1, c = _gc();
+    for (n=0; c >= '0' && c <= '9'; c=_gc())
+        (n *= 10) += (c-'0');
+    n *= neg;
+    return c != EOF;
+}
+inline ll sc() { ll x; sc(x); return x; }
+#define _ilb inline bool
+_ilb sc(int&a){ll x;bool b=sc(x);a=x;return b;}
+_ilb sc(int&a,int&b){return sc(a)&&sc(b);}
+_ilb sc(int&a,int&b,int&c){return sc(a,b)&&sc(c);}
+_ilb sc(int&a,int&b,int&c,int&d){return sc(a,b)&&sc(c,d);}
+_ilb sc(ll&a,ll&b){return sc(a)&&sc(b);}
+_ilb sc(ll&a,ll&b,ll&c){return sc(a,b)&&sc(c);}
+_ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
+#define gcd(_a, _b) ({auto a=_a;auto\
+    b=_b;while(b)(a)%=(b),(a)^=(b)^=(a)^=(b);a;})
+
+using namespace std;
+const int MX = 1e5+11;
+
+int N, K, near[MX], deg[MX], par[MX], ans=0;  // near[i] is distance to nearest leaf
+
+struct Edge { int f, t, n; } eg[MX<<1]; int hd[MX], ecnt=2;
+void addEdge(int u, int v, bool b=1)
+{
+    eg[ecnt].f = u;
+    eg[ecnt].t = v;
+    eg[ecnt].n = hd[u];
+    hd[u] = ecnt++;
+    if (b) addEdge(v, u, 0);
+    else ++deg[u], ++deg[v];
+}
+
+int knear(int c, int p=0)
+{
+    if (deg[c] == 1) return 0; // if leaf
+    near[c] = 1e9;
+    par[c] = p;
+    for (int e=hd[c]; e; e=eg[e].n)
+        if (eg[e].t != p)
+            near[c] = min(near[c], knear(eg[e].t, c)+1);
+    return near[c];
+}
+
+//int best(int c, int p=0, int d=0)
+void best(int c, int p=0, int d=0)
+{
+    //if (d >= near[c]) return 1; // illegal hub, would be caught
+    if (d >= near[c]) return; // illegal hub, would be caught
+    ans += deg[c]-2;
+    //int ret = deg[c];
+    for (int e=hd[c]; e; e=eg[e].n)
+        if (eg[e].t != p)
+            //ret = max(ret, best(eg[e].t, c, d+1));
+            best(eg[e].t, c, d+1);
+    //return ret;
+}
+
+int main()
+{
+    sc(N, K);
+    for (int i=1; i<N; ++i)
+    {
+        int u, v; sc(u, v);
+        addEdge(u, v);
+    }
+
+    //knear(1); // get nearest leaf in each subtree // FIX: can't just start at one bc it might be a leaf
+    for (int i=1; i<=N; ++i) if (deg[i] > 1) { knear(i); break; }
+    // also consider that nearest might come from above (don't need to store second nearest cuz its ok if same)
+    for (int i=1; i<=N; ++i)
+        near[i] = min(near[i], near[par[i]]+1);
+    //printf("idx   "); for (int i=1; i<=N; ++i) printf("%3d", i); printf("\n");
+    //printf("deg   "); for (int i=1; i<=N; ++i) printf("%3d", deg[i]); printf("\n");
+    //printf("near  "); for (int i=1; i<=N; ++i) printf("%3d", near[i]); printf("\n");
+    best(K);
+    printf("%d\n", ans+2);
+}
+
