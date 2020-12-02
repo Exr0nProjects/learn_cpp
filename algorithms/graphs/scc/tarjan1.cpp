3e361e683dd6791b51772156d0c09d5cb02c5742
Exr0n <mail@exr0n.com>
2020-12-01 21:28:58 -0800

    manual stack and pruning opt didn't sped much

--- algorithms/graphs/scc/tarjan1.cpp
+++ algorithms/graphs/scc/tarjan1.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <utility>
-#include <stack>
+//#include <stack>
 
 using namespace std;
 const int MX = 1e5+10;
 
 int N, M, ans[MX];
-int pre[MX], post[MX], low[MX], ncnt=1;
+int pre[MX], post[MX], /*low[MX],*/ ncnt=1;
 struct Edge { int t, n; } eg[MX<<6]; int hd[MX], ecnt=2;
 void addEdge()
 {
@@ -18,27 +18,31 @@ void addEdge()
     hd[u] = ecnt++;
 }
 
-stack<int> tstack;
+//stack<int> tstack;
+int tstack[MX], sr=0;
 int tarj(int c)
 {
     //if (pre[c]) return low[c];
-    low[c] = ncnt; // FIX: init low
+    //low[c] = ncnt; // FIX: init low
+    int low = ncnt;
     pre[c] = ncnt++;
-    tstack.push(c);
-    for (int e=hd[c]; e; e=eg[e].n)
+    tstack[++sr] = c;
+    //tstack.push(c);
+    for (int e=hd[c]; e && low > 0; e=eg[e].n)
     {
         if (!pre[eg[e].t])      // child of this node (tree path)
-            low[c] = min(low[c], tarj(eg[e].t));
+            low = min(low, tarj(eg[e].t));
         else if (!ans[eg[e].t]) // existing node in same scc
-            low[c] = min(low[c], pre[eg[e].t]);  // set to pre[n] not low[n] because you can't move any more than that
+            low = min(low, pre[eg[e].t]);  // set to pre[n] not low[n] because you can't move any more than that
     }
-    if (low[c] == pre[c])   // FIX: equal to pre[c] not ncnt because everything is by pre
+    if (low == pre[c])   // FIX: equal to pre[c] not ncnt because everything is by pre
     {
-        while (tstack.top() != c)
-            ans[tstack.top()] = c, tstack.pop();
-        ans[c] = c; tstack.pop();
+        if (c > 1) return -c; // abort, we found another scc (problem specific)
+        while (tstack[sr] != c)
+            ans[tstack[sr--]] = c;
+        ans[c] = c; --sr;
     }
-    return low[c];
+    return low;
 }
 
 int main()
@@ -47,12 +51,13 @@ int main()
     int N, M; scanf("%d%d", &N, &M);
     for (int i=1; i<=M; ++i)
         addEdge();
-    tarj(1);
+    int g = tarj(1); if (g < 0) { printf("NO\n%d 1\n", -g); return 0; }
     for (int i=2; i<=N; ++i) if (ans[i] != ans[1])
         //{ printf("NO\n%d 1\n", i); return 0; }
     {
         if (pre[i]) printf("NO\n%d 1\n", i);
         else printf("NO\n1 %d", i); // FIX: print in order of impossible
+        return 0;
     }
     printf("YES\n");
 }
