e3446c356d10da94657faf3f2989adc2a3c2df34
Exr0n <mail@exr0n.com>
2020-12-06 15:56:38 -0800

    hode up, this is distinctively not a bfs

---
+++ problems/cses/graph/labyrinth/main_labyrinth.cpp
@@ -0,0 +1,109 @@
+/*
+ * Problem labyrinth (cses/graph/labyrinth)
+ * Create time: Sun 06 Dec 2020 @ 15:50 (PST)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <set>
+#include <map>
+#include <tuple>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <utility>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+#define ll long long
+#define dl double
+
+#define gpq(...) priority_queue<__VA_ARGS__, deque<__VA_ARGS__>, greater<__VA_ARGS__> >
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define f first
+#define s second
+#define mt make_tuple
+#define un(...) ll __VA_ARGS__; tie(__VA_ARGS__)
+#define lb(x) ((x)&-(x))
+
+#define F(i,b) for (ll i=1; i<=(b); ++i)
+#define R(i,b) for (ll i=(b); i>=1; --i)
+#define N(e,c) for (int e=hd[c]; e; e=eg[e].n)
+#define TN(e,c,p) N(e,c) if (eg[e].t != p)
+
+inline ll pow(ll b, ll e, ll m)
+{
+    ll ret=1;
+    for (; e; e>>=1, (b*=b)%=m)
+        if (e&1) (ret *= b)%=m;
+    return ret;
+}
+
+void setIO(const std::string &name = "labyrinth")
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+    if (fopen((name + ".in").c_str(), "r") != 0)
+        freopen((name + ".in").c_str(), "r", stdin),
+        freopen((name + ".out").c_str(), "w+", stdout);
+}
+#define _gc getchar_unlocked
+inline bool sc(ll &n)
+{
+    int neg = 1;
+    register char c;
+    do c = _gc(); while (isspace(c));
+    if (c == '-') neg = -1, c = _gc();
+    for (n=0; c >= '0' && c <= '9'; c=_gc())
+        (n *= 10) += (c-'0');
+    n *= neg;
+    return c != EOF;
+}
+inline ll sc() { ll x; sc(x); return x; }
+#define _ilb inline bool
+_ilb sc(int&a){ll x;bool b=sc(x);a=x;return b;}
+_ilb sc(int&a,int&b){return sc(a)&&sc(b);}
+_ilb sc(int&a,int&b,int&c){return sc(a,b)&&sc(c);}
+_ilb sc(int&a,int&b,int&c,int&d){return sc(a,b)&&sc(c,d);}
+_ilb sc(ll&a,ll&b){return sc(a)&&sc(b);}
+_ilb sc(ll&a,ll&b,ll&c){return sc(a,b)&&sc(c);}
+_ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
+ll gcd(ll a, ll b) { while (b^=a^=b^=a%=b); return a; }
+
+//struct Edge { int t, n; } eg[MX*MX]; int hd[MX], ecnt=2;
+using namespace std;
+const int MX = 1e3+11;
+int N, M;
+
+int _y[] = { 0, 1, 0, -1 };
+int _x[] = { 1, 0, -1, 0 };
+char _c[] = {};
+
+int dir[MX][MX];
+char buf[MX][MX];
+
+int endy, endx;
+
+void bfs(int y, int x, int d=-1)
+{
+    if (buf[y][x] == 'B') endy = y, endx = x;
+    if (buf[y][x] != '.' || dir[y][x]) return;
+    dir[y][x] = d;
+    for (int d=0; d<4; ++d)
+        bfs(y+_y[d], x+_x[d], d);
+}
+
+int main()
+{
+    sc(N, M);
+    F(i, N) scanf("%s", buf[i]+1);
+
+    F(i, N) F(j, N)
+        if (buf[i][j] == 'A')
+        { bfs(i, j); break; }
+
+
+}
+
