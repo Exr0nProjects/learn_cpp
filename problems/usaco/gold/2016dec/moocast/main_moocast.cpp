8b41f2d847bcad97f98fd4c9b22f1bd4130d09b7
Exr0n <mail@exr0n.com>
2020-12-06 11:07:47 -0800

    plm dense prim, wa for sample

---
+++ problems/usaco/gold/2016dec/moocast/main_moocast.cpp
@@ -0,0 +1,108 @@
+/*
+ * Problem moocast (usaco/gold/2016dec/moocast)
+ * Create time: Sun 06 Dec 2020 @ 11:02 (PST)
+ * Accept time: [!meta:end!]
+ *
+ */
+
+#include <set>
+#include <map>
+#include <tuple>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <utility>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+#define ll long long
+#define dl double
+
+#define gpq(T) priority_queue<T, deque<T>, greater<T> >
+#define CMA , // https://stackoverflow.com/q/13842468/10372825
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define lb(x) ((x)&-(x))
+
+#define F(i,b) for (ll i=1; i<=(b); ++i)
+#define R(i,b) for (ll i=(b); i>=1; --i)
+#define N(e,c) for (int e=hd[c]; e; e=eg[e].n)
+#define TN(e,c,p) N(e,c) if (eg[e].t != p)
+
+inline ll pow(ll b, ll e, ll m)
+{
+    ll ret=1;
+    for (; e; e>>=1, (b*=b)%=m)
+        if (e&1) (ret *= b)%=m;
+    return ret;
+}
+
+void setIO(const std::string &name = "moocast")
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+    if (fopen((name + ".in").c_str(), "r") != 0)
+        freopen((name + ".in").c_str(), "r", stdin),
+        freopen((name + ".out").c_str(), "w+", stdout);
+}
+#define _gc getchar_unlocked
+inline bool sc(ll &n)
+{
+    int neg = 1;
+    register char c;
+    do c = _gc(); while (isspace(c));
+    if (c == '-') neg = -1, c = _gc();
+    for (n=0; c >= '0' && c <= '9'; c=_gc())
+        (n *= 10) += (c-'0');
+    n *= neg;
+    return c != EOF;
+}
+inline ll sc() { ll x; sc(x); return x; }
+#define _ilb inline bool
+_ilb sc(int&a){ll x;bool b=sc(x);a=x;return b;}
+_ilb sc(int&a,int&b){return sc(a)&&sc(b);}
+_ilb sc(int&a,int&b,int&c){return sc(a,b)&&sc(c);}
+_ilb sc(int&a,int&b,int&c,int&d){return sc(a,b)&&sc(c,d);}
+_ilb sc(ll&a,ll&b){return sc(a)&&sc(b);}
+_ilb sc(ll&a,ll&b,ll&c){return sc(a,b)&&sc(c);}
+_ilb sc(ll&a,ll&b,ll&c,ll&d){return sc(a,b)&&sc(c,d);}
+ll gcd(ll a, ll b) { while (b^=a^=b^=a%=b); return a; }
+
+//struct Edge { int t, n; } eg[MX*MX]; int hd[MX], ecnt=2;
+using namespace std;
+const int MX = 1e3+11;
+
+int N, x[MX], y[MX], dist[MX];
+
+inline int kdst(int i, int j)
+{
+    return (x[i]-x[j])*(x[i]-x[j]) + (y[i]-y[j])*(y[i]-y[j]);
+}
+
+int main()
+{
+    setIO();
+    sc(N);
+    F(i, N) sc(x[i], y[i]);
+
+    memset(dist, 0x3f, sizeof dist);
+    int mni = 1, ans = 0;
+    F(i, N)
+    {
+        printf("best was %d with %d\n", mni, dist[mni]);
+        ans = dist[mni];
+        dist[mni] = -1;
+        int n = 0;
+        F(j, N)
+        {
+            dist[j] = min(dist[j], kdst(mni, j));
+            if (!n || dist[j] < dist[n]) n = j;
+        }
+        mni = n;
+    }
+    printf("%d\n", ans);
+}
+
