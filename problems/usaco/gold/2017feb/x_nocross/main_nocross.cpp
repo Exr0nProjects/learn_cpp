07d0d3649107cf6ca9170acfa4deb753cbcdd5d6
Exr0n <mail@exr0n.com>
2020-12-12 17:24:44 -0800

    AC usaco/gold/2017feb/nocross: (script)

---
+++ problems/usaco/gold/2017feb/x_nocross/main_nocross.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem nocross (usaco/gold/2017feb/nocross)
+ * Create time: Sat 12 Dec 2020 @ 17:21 (PST)
+ * Accept time: [!meta:end!]
+ *
+ */
+#include <bits/stdc++.h>
+using namespace std;
+const int MX = 1e3+10;
+
+int N, a[MX], b[MX], dp[MX][MX];
+
+int main()
+{
+    freopen("nocross.in", "r", stdin);
+    freopen("nocross.out", "w+", stdout);
+    scanf("%d", &N);
+    for (int i=1; i<=N; ++i) scanf("%d", a+i);
+    for (int i=1; i<=N; ++i) scanf("%d", b+i);
+
+    // basically lcs
+    for (int i=1; i<=N; ++i)
+        for (int j=1; j<=N; ++j)
+        {
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            if (abs(a[i] - b[j]) <= 4)
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
+        }
+    printf("%d\n", dp[N][N]);
+}
+
