3af7bfbfe639127300b2c444c4e3b38c0e23238f
Exr0n <mail@exr0n.com>
2020-12-15 22:03:19 -0800

    plm dinic segfault on spoj fastflow sample

---
+++ problems/spoj/fastflow_1/main_fastflow_1.cpp
@@ -0,0 +1,66 @@
+/*
+ * Problem fastflow_1 (spoj/fastflow_1)
+ * Create time: Tue 15 Dec 2020 @ 21:50 (PST)
+ * Accept time: [!meta:end!]
+ *
+ */
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const ll MX = 5010;
+const ll MXE = 30010;
+
+ll N, M;
+ll q[MX], ql=1, qr=0;
+ll dep[MX];
+
+struct Edge { ll t, w, n; } eg[MXE<<1]; ll hd[MX], ecnt=2;
+void addEdge(ll u=0, ll v=0, ll w=0)
+{
+    if (!u) scanf("%d%d%d", &u, &v, &w);
+    else addEdge(v, u, w);
+    eg[ecnt] = { v, w, hd[u] };
+    hd[u] = ecnt++;
+}
+
+bool bfs()
+{
+    memset(dep, 0, sizeof dep);
+    memset(q, 0, sizeof q);
+    ql = 1, qr = 0;
+    for (q[++qr] = 1; ql <= qr; ++ql)
+    {
+        if (q[ql] == N) return 1;
+        for (int e=hd[q[ql]]; e; e=eg[e].n)
+            if (!dep[eg[e].t] && eg[e].w > 0)
+                dep[eg[e].t] = dep[q[ql]]+1,
+                q[++qr] = eg[e].t;  // could be compressed
+    }
+    return 0;
+}
+
+ll aug(ll c, ll mn)
+{
+    if (!mn || c == N) return mn;
+    ll flo = 0;
+    for (int e=hd[c]; e && mn; e=eg[e].n)
+    {
+        ll g = aug(eg[e].t, min(mn, eg[e].w));
+        eg[e].w -= g, eg[e^1].w += g;
+        mn -= g, flo += g;
+    }
+    if (!flo) dep[c] = 0;
+    return flo;
+}
+
+int main()
+{
+    scanf("%d%d", &N, &M);
+    for (int i=1; i<=M; ++i) addEdge();
+
+    ll ans = 0;
+    while (bfs())
+        ans += aug(1, 1e16);
+    printf("%lld\n", ans);
+}
+
