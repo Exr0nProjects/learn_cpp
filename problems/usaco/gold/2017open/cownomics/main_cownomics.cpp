2626985db22fdd06619414040fba0499a5829907
Exr0n <mail@exr0n.com>
2020-12-16 18:21:34 -0800

    bins scuffed

--- problems/usaco/gold/2017open/cownomics/main_cownomics.cpp
+++ problems/usaco/gold/2017open/cownomics/main_cownomics.cpp
@@ -24,6 +24,25 @@ ll compress(char &c)
     }
 }
 
+bool check(ll len)
+{
+    printf("check %d\n", len);
+    set<ll> vis;
+    for (int j=1; j+len <= M; ++j)
+    {
+        vis.clear();
+        bool good = 1;
+        for (int i=1; i<=N; ++i)
+            vis.insert(hs[0][i][j][j+len]);
+        for (int i=1; i<=N; ++i)
+            if (vis.count(hs[1][i][j][j+len]))
+            { good = 0; break; }
+        if (good) return 1;
+    }
+    printf("no \n");
+    return 0;
+}
+
 int main()
 {
     scanf("%d%d", &N, &M);
@@ -36,10 +55,17 @@ int main()
 
     for (int i=1; i<=N; ++i) { for (int j=1; j<=M; ++j) printf("%3d", hs[0][i][j][j]); printf("\n"); }
 
-    for (int s=0; s<2; ++s) for (int i=1; i<=N; ++i)
-        for (int n=1; n<N; ++n) for (int j=1; j+n<=N; ++j)
+    for (int s=0; s<2; ++s) for (int n=1; n<M; ++n)
+    {
+        for (int i=1; i<=N; ++i) for (int j=1; j+n<=M; ++j) // FIX: typo-- some should be <= M not <= N
+            //printf("%d << 2 + %d = %d\n", hs[s][i][j][j+n-1], hs[s][i][j+n][j+n], (hs[s][i][j][j+n-1] << 2)%mod + hs[s][i][j+n][j+n]),
             hs[s][i][j][j+n] =
                 (hs[s][i][j][j+n-1] << 2)%mod + hs[s][i][j+n][j+n];
-    for (int i=1; i<=N; ++i) { for (int j=1; j<=M; ++j) printf("%20lld", hs[0][i][j][j+1]); printf("\n"); }
+        //for (int i=1; i<=N; ++i) { for (int j=1; j<=M; ++j) printf("%20lld", hs[0][i][j][j+n]); printf("\n"); } printf("\n\n");
+    }
+    ll l=0, r=M-1, m;
+    for (; l+1 <= r; (check(m)?r:l) = m)
+        m = l + (r-l>>1);
+    printf("%d\n", r);
 }
 
