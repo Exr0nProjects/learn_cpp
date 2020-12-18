dacee92f849777fdf22b4dcdfc8b536702afd667
Exr0n <mail@exr0n.com>
2020-12-17 22:27:28 -0800

    wap spoj najpf (kmp)

--- problems/spoj/najpf2/main_najpf2.cpp
+++ problems/spoj/najpf2/main_najpf2.cpp
@@ -10,6 +10,7 @@ using namespace std;
 const ll MX = 1e6;
 
 ll T, N, P, pi[MX];
+ll ans[MX], acnt=0;
 char str[MX], pat[MX];
 
 void kpi()
@@ -28,11 +29,26 @@ int main()
     scanf("%lld", &T);
     while (T--)
     {
+        acnt=0; // FIX: clears
         scanf("%s%s", str, pat);
         N = strlen(str);
         P = strlen(pat);
+        //printf("N %d P %d\n", N, P);
         kpi();
-        for (int i=0; i<P; ++i) printf("%3d", pi[i]);
+        //for (int i=0; i<P; ++i) printf("%3d", pi[i]);
+        ll c = 0;   // c = next char to match
+        for (int i=0; i<N; ++i)
+        {
+            for (; ~c && pat[c] != str[i];
+                    c = pi[c]);
+            if (pat[c] == str[i]) ++c;
+            if (c == P)
+                //printf("end match at %d\n", i),
+                    ans[++acnt] = i-P+2;
+        }
+        if (!acnt) { printf("Not Found\n"); continue; }
+        //printf("%d\n", acnt);
+        for (int i=1; i<=acnt; ++i) printf("%d ", ans[i]); printf("\n");
     }
 }
 
