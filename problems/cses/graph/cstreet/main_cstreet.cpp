23c2944492f580b5b0791faf27f0ce82ae4d94c0
Exr0n <mail@exr0n.com>
2020-12-17 22:11:04 -0800

    plm prim

---
+++ problems/cses/graph/cstreet/main_cstreet.cpp
@@ -0,0 +1,51 @@
+/*
+ * Problem cstreet (cses/graph/cstreet)
+ * Create time: Thu 17 Dec 2020 @ 22:06 (PST)
+ * Accept time: [!meta:end!]
+ *
+ */
+#include <bits/stdc++.h>
+#define ll long long
+#define mp make_pair
+using namespace std;
+
+const ll MX = 1e3+10;
+const ll MXE = 3e5+10;
+
+ll T, P, N, M;
+bool vis[MX];
+typedef pair<ll, ll> St;
+
+struct Edge { ll t, n; } eg[MXE]; ll hd[MX], ecnt=2;
+void addEdge(ll u=0, ll v=0)
+{
+    if (!u) scanf("%lld%lld", &u, &v), addEdge(v, u);
+    eg[ecnt] = { v, hd[u] };
+    hd[u] = ecnt++;
+}
+
+int main()
+{
+    scanf("%lld", &T);
+    while (T--)
+    {
+        scanf("%lld%lld%lld", &P, &N, &M);
+        for (int i=1; i<=M; ++i) addEdge();
+
+        priority_queue<St, deque<St>, greater<St> > pq;
+        pq.push(mp(0, 1));
+
+        ll ans = 0;
+        while (pq.size())
+        {
+            ll d, c; tie(d, c) = pq.top(); pq.pop();
+            if (vis[c]) continue;
+            vis[c] = 1;
+            ans += d;
+            for (int e=hd[c]; e; e=eg[e].n)
+                pq.push(mp(eg[e].w, eg[e].t));
+        }
+        printf("%lld\n", ans);
+    }
+}
+
