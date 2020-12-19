8f80205f17ca2dd1fa8fad45ef489d43e27e870a
Exr0n <mail@exr0n.com>
2020-12-19 14:08:26 -0800

    struggling to dfs

--- problems/usaco/gold/2020dec/1053/main_1053.cpp
+++ problems/usaco/gold/2020dec/1053/main_1053.cpp
@@ -17,13 +17,22 @@ using namespace std;
 const ll MX = 1e3+10;
 const ll _x[] = { 1, 0, -1, 0 };
 const ll _y[] = { 0, 1, 0, -1 };
-ll N, D;
+ll N, D, ans=0;
 char grid[MX][MX];
 
 ll smx[MX][MX], near[MX][MX];
 bool vis[MX][MX][4];
 
-void flood(ll x, ll y, ll c) {}
+bitset<MX*MX*(MX+1)> fvis;
+void flood(ll x, ll y, ll c)
+{
+    if (!c) return;
+    if (fvis.test((x-1)*MX*MX+(y-1)*MX+c)) return;
+    if (!smx[x][y]) ++ans, grid[x][y] = 'x', smx[x][y] = c;
+    fvis.set((x-1)*MX*MX+(y-1)*MX+c);
+    for (int n=0; n<4; ++n)
+        flood(x+_x[n], y+_y[n], c-1);
+}
 
 int main()
 {
@@ -51,33 +60,38 @@ int main()
                 hq.push(mp(d+1, mp(nx, ny)));
         }
     }
-    for (int i=1; i<=N; ++i) { for (int j=1; j<=N; ++j) db("%3d", near[i][j]); db("\n"); }
+    //for (int i=1; i<=N; ++i) { for (int j=1; j<=N; ++j) db("%3d", near[i][j]); db("\n"); }
     while (sq2.size())
     {
         ll d, x, y, n; tie(d, x, y, n) = sq2.front(); sq2.pop();
         if (vis[x][y][n] > d) continue; vis[x][y][n] = d;
-        db("at %d %d %d %d\n", d, x, y, n);
+        //db("at %d %d %d %d\n", d, x, y, n);
         smx[x][y] = max(smx[x][y], d/D+1);
         if (d % D == 0) if (near[x][y] < d/D+1) { --smx[x][y]; continue; } // replication would hit wall
-        db("to %d,%d gets %d >= %d and %d < %d\n", x+_x[n], y+_y[n], near[x+_x[n]][y+_y[n]], d/D+1, vis[x+_x[n]][y+_y[n]][n], d+1);
+        //db("to %d,%d gets %d >= %d and %d < %d\n", x+_x[n], y+_y[n], near[x+_x[n]][y+_y[n]], d/D+1, vis[x+_x[n]][y+_y[n]][n], d+1);
         if (near[x+_x[n]][y+_y[n]] >= d/D+1 && vis[x+_x[n]][y+_y[n]][n] < d+1)
             sq2.push(mt(d+1, x+_x[n], y+_y[n], n));
     }
     for (int i=1; i<=N; ++i) { for (int j=1; j<=N; ++j) db("%3d", smx[i][j]); db("\n"); }
-    typedef tuple<ll, ll, ll> St;
-    priority_queue<St, deque<St>, greater<St> > pq;
+
+
     for (int i=1; i<=N; ++i) for (int j=1; j<=N; ++j)
-        if (smx[i][j]) pq.push(mt(smx[i][j], i, j));
-    ll ans = 0;
-    for (; pq.size();)
-    {
-        ll d, x, y; tie(d, x, y) = pq.top(); pq.pop();
-        db("at %d %d %d\n", d, x, y);
-        if (smx[x][y] > d) continue;
-        smx[x][y] = d; ++ans;
-        for (int n=0; n<4; ++n) if (smx[x+_x[n]][y+_y[n]] < d-1)
-            pq.push(mt(d-1, x+_x[n], y+_y[n]));
-    }
+        if (smx[i][j]) flood(i, j, smx[i][j]), ans += (bool)smx[i][j];
+    for (int i=1; i<=N; ++i) { for (int j=1; j<=N; ++j) db("%3c", grid[i][j]); db("\n"); }
+
+    //typedef tuple<ll, ll, ll> St;
+    //priority_queue<St, deque<St>, greater<St> > pq;
+    //for (int i=1; i<=N; ++i) for (int j=1; j<=N; ++j)
+    //    if (smx[i][j]) pq.push(mt(smx[i][j], i, j));
+    //for (; pq.size();)
+    //{
+    //    ll d, x, y; tie(d, x, y) = pq.top(); pq.pop();
+    //    //db("at %d %d %d\n", d, x, y);
+    //    if (smx[x][y] > d) continue;
+    //    smx[x][y] = d; ++ans;
+    //    for (int n=0; n<4; ++n) if (smx[x+_x[n]][y+_y[n]] < d-1)
+    //        pq.push(mt(d-1, x+_x[n], y+_y[n]));
+    //}
     printf("%lld\n", ans);
 }
 
