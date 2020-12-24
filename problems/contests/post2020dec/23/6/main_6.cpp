ef69af537443089e0f7419ada650f6e9efeebf1d
Exr0n <mail@exr0n.com>
2020-12-24 13:09:00 -0800

    tle 80 xcp23.6 :/

--- problems/contests/post2020dec/23/6/main_6.cpp
+++ problems/contests/post2020dec/23/6/main_6.cpp
@@ -7,7 +7,8 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define ord(c) (c - 'A' +1)
-#define db(...) fprintf(stdout, __VA_ARGS__)
+//#define db(...) fprintf(stdout, __VA_ARGS__)
+#define db(...)
 using namespace std;
 
 const ll MX = 110;
@@ -19,20 +20,33 @@ vector<ll> tail[MX];
 ll dp2[MX];
 char buf[MX];
 
+ll dbm(ll mask)
+{
+    for (int i=1; i<27; ++i)
+        db("%3c", mask>>i&1? i+'A'-1 : ' ');
+    db("\n");
+    return mask;
+}
+
 ll op(ll i, ll j) // inc exc
 {
-    if (i+1 == j) return ord(buf[i]);
-    if (dp[i][j]) return dp[i][j];
-    if (i+2 == j) return adj[i][j];
     ll &ret = dp[i][j];
+    //if (i+1 == j) ret = dbm(1<<ord(buf[i]));
+    //if (i+2 == j) ret = dbm(adj[ord(buf[i])][ord(buf[j-1])]);
+    if (i+1 == j) ret = 1<<ord(buf[i]);
+    if (i+2 == j) ret = adj[ord(buf[i])][ord(buf[j-1])];
+    if (!ret)
     for (int k=i+1; k<j; ++k)
     {
         ll l = op(i, k), r = op(k, j);
+        //db("l "); dbm(l); db("r "); dbm(r);
+        if (!l || !r) continue;
         for (int xl=1; xl<30; ++xl) if (l>>xl & 1)
         for (int xr=1; xr<30; ++xr) if (r>>xr & 1)
             ret |= adj[xl][xr];
     }
-    if (ret>>ord('S')&1) tail[j+1].push_back(i+1);
+    db("%d..%d got ", i, j); dbm(ret); db("(%d)\n", ret>>ord('S')&1);
+    if (ret>>ord('S')&1) tail[j].push_back(i);
     return ret;
 }
 
@@ -42,8 +56,14 @@ int main()
     for (int i=1; i<=M; ++i)
     {
         char u, a, b; scanf("\n%c%c%c", &u, &a, &b);
-        adj[ord(a)][ord(b)] |= ord(u);
+        //db("got '%c' '%c' '%c'\n", u, a, b);
+        adj[ord(a)][ord(b)] |= (1<<ord(u)); // FIX: |= to 1<< ord not just ord smah
     }
+    //db("adj[1][1] = %d\n", adj[1][1]);
+    //for (int i=1; i<=30; ++i) if (adj[1][1]>>i &1) db("%3d", i); else db("   ");
+    //for (int i=1; i<27; ++i) for (int j=1; j<27; ++j)
+    //    db("i %d j %d :   ", i, j), dbm(adj[i][j]);
+
     scanf("%d", &Q);
     for (int i=1; i<=Q; ++i)
     {
@@ -55,14 +75,16 @@ int main()
         for (int i=1; i<=N; ++i) tail[i].clear();
         op(0, N);
 
+        //for (int i=1; i<=N; ++i) db("tail %d has %d\n", i, tail[i].size());
+
         memset(dp2, 0x3f, sizeof dp2);
         dp2[0] = 0;
         for (int j=1; j<=N; ++j)
             for (ll i : tail[j])
-                dp2[j] = min(dp2[j], dp2[i-1]+1);
+                dp2[j] = min(dp2[j], dp2[i]+1);
 
-        if (ans >= 1e9) printf("NIE\n");
-        else printf("%lld\n", ans);
+        if (dp2[N] >= 1e9) printf("NIE\n");
+        else printf("%lld\n", dp2[N]);
     }
 }
 
