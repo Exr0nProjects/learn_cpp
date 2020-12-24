1f1a690cfbe8203763ea489f26e08eff6334f0db
Exr0n <mail@exr0n.com>
2020-12-24 14:35:31 -0800

    time to onebase everything?

--- problems/contests/post2020dec/23/6/main_6.cpp
+++ problems/contests/post2020dec/23/6/main_6.cpp
@@ -11,11 +11,24 @@
 //#define db(...)
 using namespace std;
 
+ll pow(ll b, ll e, ll m)
+{
+    ll ans = 1;
+    //if (e < 0) return 1; // TODO: pow getting passed -1?
+    assert(e >= 0);
+    for (; e; e>>=1, (b*=b)%=m)
+    {
+        //printf("pow %d %d %d\n", b, e, m);
+        if (e&1) (ans *= b) %= m;
+    }
+    return ans;
+}
+
 const ll MX = 110;
 const ll MXE = 10010;
-const ll P1 = 29;
-const ll P2 = 31;
-const ll MOD = 1e9+7;
+const ll P = 29;
+const ll MOD1 = 1e9+7;
+const ll MOD2 = 1e9+9;
 
 ll N, M, Q;
 ll adj[30][30], dp[MX][MX];
@@ -38,6 +51,10 @@ ll op(ll i, ll j) // inc exc
     ll &ret = dp[i][j];
     //if (i+1 == j) ret = dbm(1<<ord(buf[i]));
     //if (i+2 == j) ret = dbm(adj[ord(buf[i])][ord(buf[j-1])]);
+    ll hash = (((phash[j]>>32) - (phash[i]>>32)*pow(P, j-i, MOD1)%MOD1)<<32)
+        | ((phash[j]&((1ll<<32)-1ll))-(phash[i]&((1ll<<32)-1ll))*pow(P, j-i, MOD2)%MOD2);
+    db("hash %3d..%3d = %20llx %20llx -> %20llx\n", i, j, (((phash[j]>>32) - (phash[i]>>32)*pow(P, j-i, MOD1)%MOD1)), ((phash[j]&((1ll<<32)-1ll))-(phash[i]&((1ll<<32)-1ll))*pow(P, j-i, MOD2)%MOD1), hash);
+    if (mem.count(hash)) ret = hash;
     if (i+1 == j) ret = 1<<ord(buf[i]);
     if (i+2 == j) ret = adj[ord(buf[i])][ord(buf[j-1])];
     if (!ret)
@@ -52,12 +69,22 @@ ll op(ll i, ll j) // inc exc
     }
     //db("%d..%d got ", i, j); dbm(ret); db("(%d)\n", ret>>ord('S')&1);
     if (ret>>ord('S')&1) tail[j].push_back(i);
+    mem[hash] = ret;
     return ret;
 }
 
 int main()
 {
-    scanf("%d", &M);
+    //
+    //while (true)
+    //{
+    //    ll b, e, m;
+    //    scanf("%lld%lld%lld", &b, &e, &m);
+    //    printf("%lld\n", pow(b, e, m));
+    //}
+    //
+    //
+    scanf("%lld", &M);
     for (int i=1; i<=M; ++i)
     {
         char u, a, b; scanf("\n%c%c%c", &u, &a, &b);
@@ -69,18 +96,25 @@ int main()
     //for (int i=1; i<27; ++i) for (int j=1; j<27; ++j)
     //    db("i %d j %d :   ", i, j), dbm(adj[i][j]);
 
-    scanf("%d", &Q);
+    scanf("%lld", &Q);
     for (int i=1; i<=Q; ++i)
     {
         scanf("%s", buf);
         N = strlen(buf);
-        ll g = op(0, N);
+        //ll g = op(0, N);
 
         phash[0] = ord(buf[0]) | (ord(buf[0])<<32);
-        printf("phash0 = %llx\n", phash[0]);
+        //printf("phash0 = %llx\n", phash[0]);
         for (int i=1; i<N; ++i)
-            phash[i] = ((((phash[i-1]>>32)*P1 + ord(buf[i])) % MOD)<<32) | (((phash[i-1] & ((1<<32)-1))*P2 + ord(buf[i]))%MOD);
-        for (int i=0; i<N; ++i) db("%3d: %10llx %10llx\n", i, phash[i]>>32, phash[i] &((1<<32)-1));
+            phash[i] = ((((phash[i-1ll]>>32ll)*P + ord(buf[i])) % MOD1)<<32ll) | (((phash[i-1] & ((1ll<<32ll)-1ll))*P + ord(buf[i]))%MOD2);
+        for (int i=0; i<N; ++i) db("%3d: %20llx ( %10llx %10llx )\n", i, phash[i], phash[i]>>32, phash[i] &((1ll<<32ll)-1ll));
+
+        for (int i=0; i<N; ++i) { for (int j=i+1; j<N; ++j)
+        {
+            ll hash = (((phash[j]>>32) - (phash[i]>>32)*pow(P, j-i, MOD1)%MOD1)<<32) | ((phash[j]&((1ll<<32)-1ll))-(phash[i]&((1ll<<32)-1ll))*pow(P, j-i, MOD2)%MOD2);
+            db("hash %3d..%3d = %20llx %20llx -> %20llx\n", i, j, (((phash[j]>>32) - (phash[i]>>32)*pow(P, j-i, MOD1)%MOD1)), ((phash[j]&((1ll<<32)-1ll))-(phash[i]&((1ll<<32)-1ll))*pow(P, j-i, MOD2)%MOD1), hash);
+        } }
+
         continue;
 
         memset(dp, 0, sizeof dp);
