943ea0dacea047b080e00b6e4d3daba929d63100
Exr0n <mail@exr0n.com>
2020-12-26 10:18:23 -0800

    wuh woh (2d segt bins broke?)
    1 4 4
    -1 3 4  // gets 4 4, should get 3 4

--- problems/usaco/gold/2020jan/boards/gen.cpp
+++ problems/usaco/gold/2020jan/boards/gen.cpp
@@ -3,10 +3,16 @@
 #include <cstdlib>
 #include <random>
 #include <chrono>
+#include <tuple>
+#include <utility>
 #define ll long long
+#define f first
+#define s second
+#define mp make_pair
 #define db(...) fprintf(stderr, __VA_ARGS__)
 #define rand(l,h) (rng()%(h-l+1)+l)
-// sparse segtree binary search in a casegen :/
+using namespace std;
+// 2d sparse segtree binary search in a casegen :/
 
 const ll N = 2e1;
 const ll P = 1e1;
@@ -16,48 +22,54 @@ const ll P = 1e1;
 bool vis[N][N];
 
 ll tsum[P<<6], lc[P<<6], rc[P<<6], sub[P<<6], rt=0, tcnt=0;
-void update(ll qx, ll qy, ll &k=rt, ll tl=1, ll tr=N)
-{
-    if (!k) k = ++tcnt;
-    if (tl == tr)
 void updatesub(ll q, ll v, ll &k=rt, ll tl=1, ll tr=N)
 {
     if (!k) k = ++tcnt;
-    printf("update %d %d at %d (%d..%d)\n", q, v, k, tl, tr);
+    //printf("updatesub %d %d at %d (%d..%d)\n", q, v, k, tl, tr);
     if (tl == tr) return tsum[k] = v, void();
     ll mid = tl + (tr-tl>>1);
-    if (q <= mid) update(q, v, lc[k], tl, mid);
-    else update(q, v, rc[k], mid+1, tr);
+    if (q <= mid) updatesub(q, v, lc[k], tl, mid);
+    else updatesub(q, v, rc[k], mid+1, tr);
     tsum[k] = tsum[lc[k]] + tsum[rc[k]];
 }
-ll pquery(ll p, ll k=rt, ll tl=1, ll tr=N)
+void update(ll qx, ll qy, ll &k=rt, ll tl=1, ll tr=N)
 {
-    if (!k) return tl*N-N + p;
-    if (tl == tr) return pquerysub(p, sub[k], 1, N);
+    if (!k) k = ++tcnt;
+    //printf("update %d %d at %d (%d..%d)\n", qx, qy, rt, tl, tr);
+    if (tl == tr) return updatesub(qy, 1, sub[k]), tsum[k] = tsum[sub[k]], void();
     ll mid = tl + (tr-tl>>1);
-    if (p < (mid-tl+1)*N - tsum[lc[k]]) return pquery(p, lc[k], tl, mid);
-    else return pquery(p-(mid-tl+1)*N+tsum[lc[k]], rc[k], mid+1, tr);
+    if (qx <= mid) update(qx, qy, lc[k], tl, mid);
+    else update(qx, qy, rc[k], mid+1, tr);
+    tsum[k] = tsum[lc[k]] + tsum[rc[k]];
 }
 ll pquerysub(ll p, ll &k=rt, ll tl=1, ll tr=N)
 {
-    printf("pquery %d at %d (%d..%d)\n", p, k, tl, tr);
+    //printf("pquery %d at %d (%d..%d)\n", p, k, tl, tr);
     if (!k) return tl+p-1;  // FIX: tl + p -1 not just p bc need to account for left
     if (tl == tr) return tl;
     ll mid = tl + (tr-tl>>1);
     if (p <= mid-tl+1-tsum[lc[k]]) return pquerysub(p, lc[k], tl, mid);
     else return pquerysub(p-(mid-tl+1-tsum[lc[k]]), rc[k], mid+1, tr);
 }
+pair<ll, ll> pquery(ll px, ll py, ll k=rt, ll tl=1, ll tr=N)
+{
+    if (!k) return mp(tl-1 + px, py);
+    if (tl == tr) return mp(tl, pquerysub(py, sub[k]));
+    ll mid = tl + (tr-tl>>1);
+    if (px < mid-tl+1-tsum[lc[k]]) return pquery(px, py, lc[k], tl, mid);
+    else return pquery(px-(mid-tl+1-tsum[lc[k]]), py, rc[k], mid+1, tr);
+}
 
 int main()
 {
     std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-    //while (true)
-    //{
-    //    ll t, x;
-    //    scanf("%lld%lld", &t, &x);
-    //    if (~t) update(t, x);
-    //    else printf("%d\n", pquery(x));
-    //}
+    while (true)
+    {
+        ll t, x, y;
+        scanf("%lld%lld%lld", &t, &x, &y);
+        if (~t) update(x, y);
+        else { auto p = pquery(x, y); printf("%d, %d\n", p.f, p.s); }
+    }
     //while (true) printf("%d\n", rand(1, 20));
     printf("%d %d\n", N, P);
     for (int i=1; i<=P; ++i)
@@ -67,10 +79,10 @@ int main()
         y1 = rand(1, N-2*i), y2 = rand(1, N-2*i);
         if (x1 > x2) x1 ^= x2 ^= x1 ^= x2;
         if (y1 > y2) y1 ^= y2 ^= y1 ^= y2;
-        x1 = pquery(x1); y1 = pquery(y1);
-        x2 = pquery(x2); y2 = pquery(y2);
-        update(x1, 1); update(y1, 1);
-        update(x2, 1); update(y2, 1);
+
+        tie(x1, y1) = pquery(x1, y1);
+        tie(x2, y2) = pquery(x2, y2);
+        update(x1, y1); update(x2, y2);
         //do
         //    x1 = rand(0 , N), y1 = rand(0 , N),
         //    x2 = rand(x1, N), y2 = rand(y1, N),
