1362391851202a29fc668efd158dd8fb98458d1f
Exr0n <mail@exr0n.com>
2020-12-26 09:27:38 -0800

    how to casegen distinct locations?

--- problems/usaco/gold/2020jan/boards/gen.cpp
+++ problems/usaco/gold/2020jan/boards/gen.cpp
@@ -22,9 +22,14 @@ int main()
     for (int i=1; i<=P; ++i)
     {
         ll x1, y1, x2, y2;
-        do db("%d %d %d\n", i, x1, y1), x1 = rand(0, N), y1 = rand(0, N); while (vis[x1][y1]);
+        do
+            x1 = rand(0 , N), y1 = rand(0 , N),
+            x2 = rand(x1, N), y2 = rand(y1, N),
+            db("%d %d %d %d\n", x1, y1, x2, y2);
+        while (!vis[x1][y1] && !vis[x2][y2]);
+        //do db("%d %d %d\n", i, x1, y1), x1 = rand(0, N), y1 = rand(0, N); while (vis[x1][y1]);
         vis[x1][y1] = 1;
-        do db("%d %d %d\n", i, x2, y2), x2 = rand(x1, N), y2 = rand(y1, N); while (vis[x2][y2]);
+        //do db("%d %d %d\n", i, x2, y2), x2 = rand(x1, N), y2 = rand(y1, N); while (vis[x2][y2]);
         vis[x2][y2] = 1;
         printf("%d %d %d %d\n", x1, y1, x2, y2);
     }
