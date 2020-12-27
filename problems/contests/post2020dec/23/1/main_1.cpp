15b81e136d01dd3a4deb79dabe46eabf290295b8
Exr0n <mail@exr0n.com>
2020-12-27 11:23:38 -0800

    wa 10?

--- problems/contests/post2020dec/23/1/main_1.cpp
+++ problems/contests/post2020dec/23/1/main_1.cpp
@@ -8,24 +8,25 @@
 #define ll long long
 using namespace std;
 
-const ll MX = 1e4+1;
-const ll MXL = 50;
-const ll AL = 26;
-ll N, trie[MX*MXL][AL], isw[MX*MXL], tcnt=0;
+const int MX = 1e4+10;
+const int MXL = 60;
+const int AL = 32;
+int N, isw[MX*MXL], tcnt=0;
+map<char, int> trie[MX*MXL];
 char buf[MXL];
 
 int main()
 {
-    scanf("%lld", &N);
-    ll ans = 1;
-    for (ll i=1; i<=N; ++i)
+    scanf("%d", &N);
+    int ans = 1;
+    for (int i=1; i<=N; ++i)
     {
         scanf("%s", buf);
-        ll cur = 0, sum = 0;
+        int cur = 0, sum = 0;
         for (ll j=0; buf[j]; ++j)
         {
             sum += isw[cur];
-            if (!trie[cur][j]) trie[cur][j] = ++tcnt;
+            if (!trie[cur].count(j)) trie[cur][j] = ++tcnt;
             cur = trie[cur][j];
         }
         isw[cur] = 1;
