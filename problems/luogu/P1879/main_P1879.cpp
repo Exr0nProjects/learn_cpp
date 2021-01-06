4694808eb3e7bdb747b40b7ceb32e3bc6d15bd53
Exr0n <mail@exr0n.com>
2021-01-05 18:51:46 -0800

    works for small (1d) cases but not sample

--- problems/luogu/P1879/main_P1879.cpp
+++ problems/luogu/P1879/main_P1879.cpp
@@ -17,11 +17,6 @@ const ll MX = 13;
 const ll mod = 1e8;
 ll N, M, can[MX], dp[MX][MX][1<<MX];
 
-void print(ll x)
-{
-    for (int i=0; i<M; ++i) db("%2c", x>>i&1 ? '#' : '_');
-}
-
 int main()
 {
     scanf("%lld%lld", &N, &M);
@@ -35,16 +30,22 @@ int main()
     dp[0][0][0] = 1; dp[0][0][1] = can[0]&1;
     for (int i=0; i<N; ++i) for (int j=i?0:1; j<M; ++j)
         for (int x=0; x<(1<<(i*M+j+1)); ++x)
+            //if (!(x | (can[i]>>j | i?can[i-1]<<M-j:0)))
         {
-            db("%2d %2d: ", i, j); print(i?can[i-1]:0); db(","); print(can[i]); db("\n");
-            print(can[i]>>j | i?can[i-1]<<M-j:0); db("\n"); print(x); db("\n");
-            if (!(x | ~(can[i]>>j | i?can[i-1]<<M-j:0)))
+            bool bad = 0;
+            for (int k=0; k<(i?M:j) && !bad; ++k)
             {
-                db("%3d - %x\n", i*M+j, x);
-                dp[i][j][x] = pre(i, j)[x>>1] + pre(i, j)[x>>1 | 1<<(M-1)];
-                if (x&1 && ~x>>1&1 && can[i]>>j&1) (dp[i][j][x] += pre(i, j)[x>>1]) %= mod;
+                if (x>>k&1 && x>>(k+1)&1) bad = 1;
+                //db("%d %d %d : %d && !(can[%d-%d]->%d >> %d : %d)\n", i, j, k, x>>k&1, i, k>j, can[i-((bool)k>j)], j-k, (can[i-((bool)k>j)]>>(j-k)&1));
+                //printf("%d and %d?\n", x>>k&1, !(can[i-((bool)k>j)]>>(j-k)&1));
+                if (x>>k&1 && !(can[i-((bool)k>j)]>>(j-k)&1)) bad = 1; // illegal mask
             }
+            if (bad) continue;  // FIX: logic--continuing in nested loop smah, need flag var
+            //db("%3d - %x\n", i*M+j, x);
+            dp[i][j][x] = pre(i, j)[x>>1];
+            if (~x&1 && i && can[i-1]>>j&1) (dp[i][j][x] += pre(i, j)[x>>1 | 1<<(M-1)]) %= mod;
+            //if (x&1 && ~x>>1&1 && can[i]>>j&1) (dp[i][j][x] += pre(i, j)[x>>1]) %= mod;
         }
-    ll ans = 0; for (int x=0; x<1<<MX; ++x) (ans += dp[N-1][M-1][x]) %= mod;
+    ll ans = 0; for (int x=0; x<1<<M; ++x) (ans += dp[N-1][M-1][x]) %= mod;
     printf("%lld\n", ans);
 }
