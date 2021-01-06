0f79246b7d3555259f8f8455b1d75c77c366d6ee
Exr0n <mail@exr0n.com>
2021-01-05 18:13:33 -0800

    direct set scuffed bc bits numbered backwards

--- problems/luogu/P1879/main_P1879.cpp
+++ problems/luogu/P1879/main_P1879.cpp
@@ -17,6 +17,11 @@ const ll MX = 13;
 const ll mod = 1e8;
 ll N, M, can[MX], dp[MX][MX][1<<MX];
 
+void print(ll x)
+{
+    for (int i=0; i<M; ++i) db("%2c", x>>i&1 ? '#' : '_');
+}
+
 int main()
 {
     scanf("%lld%lld", &N, &M);
@@ -31,9 +36,14 @@ int main()
     for (int i=0; i<N; ++i) for (int j=i?0:1; j<M; ++j)
         for (int x=0; x<(1<<(i*M+j+1)); ++x)
         {
-            //db("%3d - %x\n", i*M+j, x);
-            dp[i][j][x] = pre(i, j)[x>>1] + pre(i, j)[x>>1 | 1<<(M-1)];
-            if (x&1 && ~x>>1&1 && can[i]>>j&1) db("ok %d\n", x), (dp[i][j][x] += pre(i, j)[x>>1]) %= mod;
+            db("%2d %2d: ", i, j); print(i?can[i-1]:0); db(","); print(can[i]); db("\n");
+            print(can[i]>>j | i?can[i-1]<<M-j:0); db("\n"); print(x); db("\n");
+            if (!(x | ~(can[i]>>j | i?can[i-1]<<M-j:0)))
+            {
+                db("%3d - %x\n", i*M+j, x);
+                dp[i][j][x] = pre(i, j)[x>>1] + pre(i, j)[x>>1 | 1<<(M-1)];
+                if (x&1 && ~x>>1&1 && can[i]>>j&1) (dp[i][j][x] += pre(i, j)[x>>1]) %= mod;
+            }
         }
     ll ans = 0; for (int x=0; x<1<<MX; ++x) (ans += dp[N-1][M-1][x]) %= mod;
     printf("%lld\n", ans);
