d1c3ce07bd892e46af42a1fe8aa045139fa11c32
Exr0n <mail@exr0n.com>
2021-01-07 18:39:27 -0800

    shift and take newest condition wrong (line 64) when wrapping

--- problems/luogu/P1896/main_P1896.cpp
+++ problems/luogu/P1896/main_P1896.cpp
@@ -7,7 +7,7 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define db(...) fprintf(stdout, __VA_ARGS__)
-#define pre(i, j) j?i:i-1][j?j-1:N-1
+#define pre(i, j) (j?i:i-1)][(j?j-1:N-1)
 using namespace std;
 
 const ll MX = 10;
@@ -22,8 +22,7 @@ void print(ll x, char e='\n')
 
 bool legal(ll x, ll j)
 {
-    if (__builtin_popcountll(x) > K) return 0;
-    ++j;
+    ++j; j%=N;
     for (int i=1; i<=N; ++i)
         if (i != (j?j:N) && x>>(i-1)&1 && x>>i&1)
             //return db("badnewsbears at i%d j%d\n", i, j), 0;
@@ -49,24 +48,28 @@ int main()
         dp[0][i][j][x] = 1;  // always one way to place zero kings
     dp[1][0][0][1] = 1;
 
-    for (int k=1; k<=K; ++k) for (int i=0; i<N; ++i)
+    for (int k=1; k<=K; ++k)
+    {
+        for (int i=0; i<N; ++i)
     {
         for (int j=i?0:1; j<N; ++j)
     {
         ll localans = 0;
-        for (int x=0; x<2<<(i?N:j); ++x) if (legal(x, j))
+        for (int x=0; x<2<<(i?N:j); ++x)
+            if (legal(x, j) && __builtin_popcountll(x) <= k)
         {
+            db("k%2d i%2d j%2d  (pre has %d)  ", k, i, j, debug[pre(i, j)]); print(x, ' ');
             if (x&1) dp[k][i][j][x] = dp[k-1][pre(i, j)][x>>1];
             else     dp[k][i][j][x] = dp[k  ][pre(i, j)][x>>1];
-            if (~x&1 && ~x>>1&1 && ~x>>N&1 && i) dp[k][i][j][x] += dp[k][pre(i, j)][x>>1 | 1<<N];
-            db("k%2d i%2d j%2d  ", k, i, j); print(x, ' '); db("-> %d\n", dp[k][i][j][x]);
+            if (~x&1 && ~x>>1&1 && (~x>>N&1) && i) db("(can link) "), dp[k][i][j][x] += dp[k][pre(i, j)][x>>1 | 1<<N];
+            db("-> %d\n", dp[k][i][j][x]);
             localans += dp[k][i][j][x];
         }
         debug[i][j] = localans;
         db("\n");
     }
         db("\n");}
-    for (int i=0; i<N; ++i) { for (int j=0; j<N; ++j) db("%3d", debug[i][j]); db("\n"); }
+    for (int i=0; i<N; ++i) { for (int j=0; j<N; ++j) db("%3d", debug[i][j]); db("\n"); } db("\n\n"); memset(debug, 0, sizeof debug); }
     ll ans = 0; for (ll x=0; x<2<<N; ++x) ans += dp[K][N-1][N-1][x];
     printf("%lld\n", ans);
 }
