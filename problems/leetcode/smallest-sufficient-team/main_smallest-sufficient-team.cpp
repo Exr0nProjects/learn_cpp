2c9f66ad72b3152cfb12d847ede7c66b6837b32b
Exr0n <mail@exr0n.com>
2021-01-10 20:24:32 -0800

    leetcode smallest sufficient team tle?

--- problems/leetcode/smallest-sufficient-team/main_smallest-sufficient-team.cpp
+++ problems/leetcode/smallest-sufficient-team/main_smallest-sufficient-team.cpp
@@ -9,9 +9,9 @@
 #define db(...) fprintf(stdout, __VA_ARGS__)
 using namespace std;
 
-const ll MX = 16;
+const ll MX = 17;
 const ll MXN = 64;
-ll dp[MX][1<<MX], pre[MX][1<<MX];
+ll dp[MXN][1<<MX], pre[MXN][1<<MX];
 
 class Solution {
     map<string, ll> sid;
@@ -27,30 +27,38 @@ class Solution {
             ret |= 1<<id(s);
         return ret;
     }
+    void names(ll x, ll M,  vector<string>& req_skills)
+    {
+        //db("(%d) ", x);
+        //for (int i=0; i<M; ++i) if (x>>i&1) cerr << req_skills[i] << " "; else for (int j=0; j<=req_skills[i].size(); ++j) db(" ");
+        //cerr << endl;
+    }
 public:
     vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
         ll N = people.size(), M = req_skills.size();
+        //db("n %d m %d\n", N, M);
 
         memset(dp, 0x3f, sizeof dp);
 
-        for (int i=0; i<N; ++i)
-            for (int x=0; x<1<<M; ++i)
+        dp[0][0] = 0; for (int x=1; x<1<<M; ++x) if (!(x & ~mask(people[0]))) dp[0][x] = 1, pre[0][x] = 1;    // FIX: base case
+        for (int i=1; i<N; ++i)
+            for (int x=0; x<1<<M; ++x)
             {
-                db("%d %d\n", i, x);
                 dp[i][x] = dp[i-1][x], pre[i][x] = 0; // don't take this person
                 if (dp[i][x] > dp[i-1][x&~mask(people[i])]+1)
                     dp[i][x] = dp[i-1][x&~mask(people[i])]+1, pre[i][x] = 1;
+                //db("i %d needs %10d for ", i, dp[i][x]); names(x, M, req_skills);
             }
 
-        ll aid = 0;
-        for (int i=1; i<N; ++i)
-            if (dp[i][0] < dp[aid][0])
-                aid = i;
-        db("seg\n");
-        ll cur = 0;
+        //ll aid = 0;
+        //for (int i=1; i<N; ++i)
+        //    if (dp[i][(1<<M)-1] < dp[aid][(1<<M)-1])
+        //        aid = i;
+        //db("answer id = %d\n", aid);
+        ll cur = (1<<M)-1;
         vector<int> ret;
-        for (int i=0; i<N; ++i) if (pre[i][cur])
-            cur |= mask(people[i]), ret.push_back(i);
+        for (int i=N-1; ~i; --i) if (pre[i][cur])
+            cur &= ~mask(people[i]), ret.push_back(i);
         return ret;
     }
 };
